7f16c7abc04ebd356495ec5d975930a124af5aab
Paul Maddox <paul.maddox@gmail.com>
2017-08-08 17:18:56 +0100

    Add a 3 second timeout to SAM Local version upgrade checks

--- version.go
+++ version.go
@@ -3,9 +3,15 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/google/go-github/github"
 )
+
+// githubTimeout defines how long to wait for a response from GitHub
+// when checking for new SAM Local versions.
+const githubTimeout = 3
 
 // checkVersionResult contains information on the current version of AWS SAM CLI, and
 // whether there are any newer versions available to upgrade to.
@@ -20,8 +26,13 @@ func checkVersion() (*checkVersionResult, error) {
 	const RepoOwner = "awslabs"
 	const RepoName = "aws-sam-local"
 
+	// Create a HTTP client with appropriate timeouts configured
+	client := &http.Client{
+		Timeout: time.Duration(githubTimeout * time.Second),
+	}
+
 	// Get the latest version details from Github release
-	gh := github.NewClient(nil)
+	gh := github.NewClient(client)
 	releases, _, err := gh.Repositories.ListReleases(context.Background(), RepoOwner, RepoName, nil)
 	if err != nil {
 		return &checkVersionResult{}, err
