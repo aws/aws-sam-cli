5d759618bea1b51298a0f379696eb65cd9376e1a
Sanghyeok Lee <shlee322@elab.kr>
2017-08-16 13:43:22 +0900

    Added to use ToSlash in getWorkingDir function.

--- runtime.go
+++ runtime.go
@@ -573,6 +573,9 @@ func getWorkingDir(basedir string, codeuri string) (string, error) {
 
 	dir := filepath.Join(abs, codeuri)
 
+        // Windows uses \ as the path delimiter, but Docker requires / as the path delimiter.
+        dir = filepath.ToSlash(dir)
+
 	return dir, nil
 
 }
