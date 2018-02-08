597dd74c7a6e450067055107a286a7e43624a44a
Tim Whitbeck <twhitbeck@gmail.com>
2018-02-08 11:21:43 -0500

    Use inspect debug protocol for node6 runtime

--- runtime.go
+++ runtime.go
@@ -467,7 +467,7 @@ func (r *Runtime) getDebugEntrypoint() (overrides []string) {
 	case runtimeName.nodejs610:
 		overrides = []string{
 			"/var/lang/bin/node",
-			"--debug-brk=" + r.DebugPort,
+			"--inspect-brk=" + r.DebugPort,
 			"--nolazy",
 			"--max-old-space-size=1229",
 			"--max-semi-space-size=76",
