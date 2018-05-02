dee54f44071be2e1de96e6713a640cd51cc3cca8
joaoasrosa <joaoasrosa@gmail.com>
2018-05-02 20:14:28 +0200

    Add dotnetcore2.0 hello world lambda

---
+++ samples/hello-world/dotnetcore2.0/src/HelloWorld/Function.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(JsonSerializer))]
+
+namespace HelloWorld
+{
+    public class Function
+    {
+        public void Handler(Event @event, ILambdaContext context)
+        {
+            context.Logger.Log($"The name is '{@event.Name}'.");
+            context.Logger.Log($"The age is '{@event.Age}'.");
+        }
+    }
+}
