2d7ccb9c37f953578131774bf0af1b7a38edb2bc
Heitor Lessa <heitor.lessa@hotmail.com>
2018-05-09 10:42:00 +0100

    Fix typo in github repo name

--- samcli/commands/init/__init__.py
+++ samcli/commands/init/__init__.py
@@ -46,9 +46,9 @@ def cli(ctx, location, runtime, output_dir, name, no_input):
         Initializes a new SAM project using custom template in a Git/Mercurial repository
         \b
         # gh being expanded to github url
-        $ sam init --location gh:aws-samples/cookiecutter-sam-python
+        $ sam init --location gh:aws-samples/cookiecutter-aws-sam-python
         \b
-        $ sam init --location git+ssh://git@github.com/aws-samples/cookiecutter-sam-python.git
+        $ sam init --location git+ssh://git@github.com/aws-samples/cookiecutter-aws-sam-python.git
         \b
         $ sam init --location hg+ssh://hg@bitbucket.org/repo/template-name
         \b
