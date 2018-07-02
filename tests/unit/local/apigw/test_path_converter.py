0678a69b166d841899970b5939814b40fa413cc1
Jacob Fuss <jfuss@amazon.com>
2018-07-02 07:23:12 -0700

    Update names of constants and added more tests

--- tests/unit/local/apigw/test_path_converter.py
+++ tests/unit/local/apigw/test_path_converter.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from parameterized import parameterized
+
 from samcli.local.apigw.path_converter import PathConverter
 
 
@@ -19,12 +21,16 @@ class TestPathConverter_toFlask(TestCase):
 
         self.assertEquals(flask_path, "/<path:proxy>")
 
-    def test_proxy_path_with_different_name(self):
-        path = "/{resource+}"
-
+    @parameterized.expand([("/{resource+}", "/<path:resource>"),
+                           ("/a/{id}/b/{resource+}", "/a/<id>/b/<path:resource>"),
+                           ("/a/b/{proxy}/{resource+}", "/a/b/<proxy>/<path:resource>"),
+                           ("/{id}/{something+}", "/<id>/<path:something>"),
+                           ("/{a}/{b}/{c}/{d+}", "/<a>/<b>/<c>/<path:d>")
+                           ])
+    def test_proxy_path_with_different_name(self, path, expected_result):
         flask_path = PathConverter.convert_path_to_flask(path)
 
-        self.assertEquals(flask_path, "/<path:resource>")
+        self.assertEquals(flask_path, expected_result)
 
     def test_proxy_with_path_param(self):
         path = "/id/{id}/user/{proxy+}"
@@ -64,12 +70,16 @@ class TestPathConverter_toApiGateway(TestCase):
 
         self.assertEquals(flask_path, "/{proxy+}")
 
-    def test_proxy_path_with_different_name(self):
-        path = "/<path:resource>"
-
+    @parameterized.expand([("/<path:resource>", "/{resource+}"),
+                           ("/a/<id>/b/<path:resource>", "/a/{id}/b/{resource+}"),
+                           ("/a/b/<proxy>/<path:resource>", "/a/b/{proxy}/{resource+}"),
+                           ("/<id>/<path:something>", "/{id}/{something+}"),
+                           ("/<a>/<b>/<c>/<path:d>", "/{a}/{b}/{c}/{d+}")
+                           ])
+    def test_proxy_path_with_different_name(self, path, expected_result):
         flask_path = PathConverter.convert_path_to_api_gateway(path)
 
-        self.assertEquals(flask_path, "/{resource+}")
+        self.assertEquals(flask_path, expected_result)
 
     def test_proxy_with_path_param(self):
         path = "/id/<id>/user/<path:proxy>"
