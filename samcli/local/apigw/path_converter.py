4e384055dd865e12ae5d6339f4006566271491e9
Jacob Fuss <32497805+jfuss@users.noreply.github.com>
2018-07-03 05:54:06 -0700

    fix: Allow Proxy Paths to have arbitrary names (#505)

--- samcli/local/apigw/path_converter.py
+++ samcli/local/apigw/path_converter.py
@@ -4,16 +4,31 @@ Converter class that handles the conversion of paths from Api Gateway to Flask a
 
 import re
 
-FLASK_PATH_PARAMS = "/<path:proxy>"
-APIGW_PATH_PARAMS_ESCAPED = r"/{proxy\+}"
-APIGW_PATH_PARAMS = "/{proxy+}"
+# The regex captures any path information before the {proxy+}. This is to support paths that have other params in
+# them. Otherwise the regex will match the first { with the last +} giving incorrect results.
+# Example: /id/{id}/user/{proxy+}. g<1> = '/id/{id}/user/' g<2> = 'proxy'
+PROXY_PATH_PARAMS_ESCAPED = r"(.*/){(.*)\+}"
+
+# The regex replaces what was captured with PROXY_PATH_PARAMS_ESCAPED to construct the full path with the {proxy+}
+# replaces. The first group is anything before {proxy+}, while the second group is the name given to the proxy.
+# Example: /id/{id}/user/{resource+}; g<1> = '/id/{id}/user/'; g<2> = 'resource'
+FLASK_CAPTURE_ALL_PATH = r"\g<1><path:\g<2>>"
+
+# The regex will replace the first group from FLASK_CAPTURE_ALL_PATH_REGEX into the proxy name part of the APIGW path.
+# Example: /<path:resource>; g<1> = 'resource'; output = /{resource+}
+PROXY_PATH_PARAMS = r"/{\g<1>+}"
+
+# The regex will capture the name of the path for the APIGW Proxy path.
+# Example: /<path:resource> is equivalent to the APIGW path /{resource+}
+FLASK_CAPTURE_ALL_PATH_REGEX = r"/<path:(.*)>"
+
 LEFT_BRACKET = "{"
 RIGHT_BRACKET = "}"
 LEFT_ANGLE_BRACKET = "<"
 RIGHT_ANGLE_BRACKET = ">"
 
-APIGW_TO_FLASK_REGEX = re.compile(APIGW_PATH_PARAMS_ESCAPED)
-FLASK_TO_APIGW_REGEX = re.compile(FLASK_PATH_PARAMS)
+APIGW_TO_FLASK_REGEX = re.compile(PROXY_PATH_PARAMS_ESCAPED)
+FLASK_TO_APIGW_REGEX = re.compile(FLASK_CAPTURE_ALL_PATH_REGEX)
 
 
 class PathConverter(object):
@@ -31,7 +46,7 @@ class PathConverter(object):
         :param str path: Path to convert to Flask defined path
         :return str: Path representing a Flask path
         """
-        proxy_sub_path = APIGW_TO_FLASK_REGEX.sub(FLASK_PATH_PARAMS, path)
+        proxy_sub_path = APIGW_TO_FLASK_REGEX.sub(FLASK_CAPTURE_ALL_PATH, path)
 
         # Replace the '{' and '}' with '<' and '>' respectively
         return proxy_sub_path.replace(LEFT_BRACKET, LEFT_ANGLE_BRACKET).replace(RIGHT_BRACKET, RIGHT_ANGLE_BRACKET)
@@ -49,7 +64,7 @@ class PathConverter(object):
         :param str path: Path to convert to Api Gateway defined path
         :return str: Path representing an Api Gateway path
         """
-        proxy_sub_path = FLASK_TO_APIGW_REGEX.sub(APIGW_PATH_PARAMS, path)
+        proxy_sub_path = FLASK_TO_APIGW_REGEX.sub(PROXY_PATH_PARAMS, path)
 
         # Replace the '<' and '>' with '{' and '}' respectively
         return proxy_sub_path.replace(LEFT_ANGLE_BRACKET, LEFT_BRACKET).replace(RIGHT_ANGLE_BRACKET, RIGHT_BRACKET)
