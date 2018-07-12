681d1903a315782211590430b4dfa1a827b6f2f8
Heitor Lessa <heitor.lessa@hotmail.com>
2018-07-12 14:05:35 +0100

    fix: Numpy docstring, includes API GW input/return interface

--- samcli/local/init/templates/cookiecutter-aws-sam-hello-python/{{cookiecutter.project_name}}/hello_world/app.py
+++ samcli/local/init/templates/cookiecutter-aws-sam-hello-python/{{cookiecutter.project_name}}/hello_world/app.py
@@ -6,12 +6,58 @@ import requests
 def lambda_handler(event, context):
     """Sample pure Lambda function
 
-    Arguments:
-        event LambdaEvent -- Lambda Event received from Invoke API
-        context LambdaContext -- Lambda Context runtime methods and attributes
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
 
-    Returns:
-        dict -- {'statusCode': int, 'body': dict}
+        {
+            "resource": "Resource path",
+            "path": "Path parameter",
+            "httpMethod": "Incoming request's method name"
+            "headers": {Incoming request headers}
+            "queryStringParameters": {query string parameters }
+            "pathParameters":  {path parameters}
+            "stageVariables": {Applicable stage variables}
+            "requestContext": {Request context, including authorizer-returned key-value pairs}
+            "body": "A JSON string of the request payload."
+            "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
+        }
+
+        https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: dict, required
+        Lambda Context runtime methods and attributes
+
+        {
+            "aws_request_id": "Lambda request ID",
+            "client_context": "Additional context",
+            "function_name": "Lambda function name",
+            "function_version": "Function version identifier",
+            "get_remaining_time_in_millis": "Time in milliseconds before function times out",
+            "identity": "Identity context from caller",
+            "invoked_function_arn": "Function ARN",
+            "log_group_name": "Cloudwatch Log group name",
+            "log_stream_name": "Cloudwatch Log stream name",
+            "memory_limit_in_mb: "Function memory"
+        }
+
+        # Lambda Python Context Object doc
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+        'statusCode' and 'body' are required
+
+        {
+            "isBase64Encoded": true | false,
+            "statusCode": httpStatusCode,
+            "headers": {"headerName": "headerValue", ...},
+            "body": "..."
+        }
+
+        # api-gateway-simple-proxy-for-lambda-output-format
+        https: // docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
     try:
