47d11c848e24c427961ba6bb483e222db83e662b
Heitor Lessa <heitor.lessa@hotmail.com>
2018-07-12 13:27:16 +0100

    fix: Return non-200 on Exceptions and send error to logs

--- samcli/local/init/templates/cookiecutter-aws-sam-hello-python/{{cookiecutter.project_name}}/hello_world/app.py
+++ samcli/local/init/templates/cookiecutter-aws-sam-hello-python/{{cookiecutter.project_name}}/hello_world/app.py
@@ -14,7 +14,27 @@ def lambda_handler(event, context):
         dict -- {'statusCode': int, 'body': dict}
     """
 
-    ip = requests.get('http://checkip.amazonaws.com/')
+    try:
+        ip = requests.get('http://checkip.amazonaws.com/')
+    except requests.RequestException as e:
+        error = {
+            "lambda_request_id": context.aws_request_id,
+            "lambda_log_group": context.log_group_name,
+            "lambda_log_stream": context.log_stream_name,
+            "apigw_request_id": event['requestContext']['requestId'],
+            "error_message": str(e.args)
+        }
+
+        # Send some context about this error to Lambda Logs
+        print(json.dumps(error, indent=4))
+
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": "Something went wrong :(",
+                "request_id": error['apigw_request_id']
+            })
+        }
 
     return {
         "statusCode": 200,
