79ed99ff5c649045f49fb546119cc6a2def442bb
Thulbourn <thulsimo@f01898280044.ant.amazon.com>
2018-09-21 22:47:08 +0100

    add new exception for invalid env var file

--- samcli/commands/local/lib/exceptions.py
+++ samcli/commands/local/lib/exceptions.py
@@ -8,3 +8,10 @@ class NoApisDefined(Exception):
     Raised when there are no APIs defined in the template
     """
     pass
+
+
+class OverridesNotWellDefined(Exception):
+    """
+    Raised when the overrides file is invalid
+    """
+    pass
