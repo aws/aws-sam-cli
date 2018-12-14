cc69a37f1e533b6321e588ec1f889eb2f9475c46
Vicky Wang <qwang@amazon.com>
2018-12-13 16:38:38 -0800

    Pin serverlessrepo version, refactor integration tests

--- tests/integration/publish/app/test_cli_integ.py
+++ tests/integration/publish/app/test_cli_integ.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 import time
 import json
 from subprocess import Popen, PIPE
@@ -13,11 +14,58 @@ except ImportError:
 
 class TestPublishApp(TestCase):
 
-    def setUp(self):
-        self.region_name = "us-east-1"
-        self.sar_client = boto3.client('serverlessrepo', region_name=self.region_name)
-        self.test_data_path = Path(__file__).resolve().parents[2].joinpath("testdata", "publish")
+    @classmethod
+    def setUpClass(cls):
+        cls.region_name = "us-east-1"
+        cls.bucket_name = str(uuid.uuid4())
+        cls.bucket_name_placeholder = "<bucket-name>"
+        cls.test_data_path = Path(__file__).resolve().parents[2].joinpath("testdata", "publish")
+        cls.sar_client = boto3.client('serverlessrepo', region_name=cls.region_name)
 
+        # Create S3 bucket
+        s3 = boto3.resource('s3')
+        cls.s3_bucket = s3.Bucket(cls.bucket_name)
+        cls.s3_bucket.create()
+
+        # Replace placeholder with the created S3 bucket name in test files
+        cls.replace_s3_bucket_in_file(cls.bucket_name_placeholder, cls.bucket_name)
+
+        # Grant serverlessrepo read access to the bucket
+        bucket_policy = cls.test_data_path.joinpath("s3_bucket_policy.json").read_text()
+        cls.s3_bucket.Policy().put(Policy=bucket_policy)
+
+        # Upload test files to S3
+        license_body = Path(__file__).resolve().parents[4].joinpath("LICENSE").read_text()
+        cls.s3_bucket.put_object(Key="LICENSE", Body=license_body)
+
+        readme_body = Path(__file__).resolve().parents[4].joinpath("README.rst").read_text()
+        cls.s3_bucket.put_object(Key="README.rst", Body=readme_body)
+        cls.s3_bucket.put_object(Key="README_UPDATE.rst", Body=readme_body)
+        cls.s3_bucket.put_object(Key="README_NO_PERMISSION.rst", Body=readme_body)
+
+        code_body = cls.test_data_path.joinpath("main.py").read_text()
+        cls.s3_bucket.put_object(Key="main.py", Body=code_body)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.s3_bucket.delete_objects(Delete={
+            'Objects': [
+                {'Key': 'LICENSE'}, {'Key': 'README.rst'}, {'Key': 'main.py'},
+                {'Key': 'README_UPDATE.rst'}, {'Key': 'README_NO_PERMISSION.rst'}
+            ]
+        })
+        cls.s3_bucket.delete()
+        # Replace the S3 bucket name with placeholder in test files
+        cls.replace_s3_bucket_in_file(cls.bucket_name, cls.bucket_name_placeholder)
+
+    @classmethod
+    def replace_s3_bucket_in_file(cls, bucket_name, replace_name):
+        for f in cls.test_data_path.iterdir():
+            if f.suffix == ".yaml" or f.suffix == ".json":
+                content = f.read_text()
+                f.write_text(content.replace(bucket_name, replace_name))
+
+    def setUp(self):
         # Create application for each test
         app_metadata_text = self.test_data_path.joinpath("metadata_create_app.json").read_text()
         app_metadata = json.loads(app_metadata_text)
@@ -25,6 +73,12 @@ class TestPublishApp(TestCase):
         response = self.sar_client.create_application(**app_metadata)
         self.application_id = response['ApplicationId']
 
+        # Avoid race conditions
+        time.sleep(1)
+
+    def tearDown(self):
+        # Delete application for each test
+        self.sar_client.delete_application(ApplicationId=self.application_id)
         # Avoid race conditions
         time.sleep(1)
 
@@ -102,9 +156,3 @@ class TestPublishApp(TestCase):
 
         expected_msg = "AWS Serverless Application Repository doesn't have read permissions"
         self.assertIn(expected_msg, process_stderr.decode('utf-8'))
-
-    def tearDown(self):
-        # Delete application for each test
-        self.sar_client.delete_application(ApplicationId=self.application_id)
-        # Avoid race conditions
-        time.sleep(1)
