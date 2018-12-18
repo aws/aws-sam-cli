ac35ac2cdb27a3a092e45ed2b11656739d0fcb8b
Alex Wood <awood45@gmail.com>
2018-12-17 16:21:01 -0800

    Tweaks
    Global Gemfile for tests. Gemfile in the function for use in tests.
    
    Hat tip to lauratpa and her PR #860 which is the core of this part of
    the change set.

--- samcli/local/init/templates/cookiecutter-aws-sam-hello-ruby/{{cookiecutter.project_name}}/tests/unit/test_handler.rb
+++ samcli/local/init/templates/cookiecutter-aws-sam-hello-ruby/{{cookiecutter.project_name}}/tests/unit/test_handler.rb
@@ -77,6 +77,16 @@ class HelloWorldTest < Test::Unit::TestCase
     end
   end
 
+  def expected_result
+    {
+      statusCode: 200,
+      body: {
+        message: 'Hello World!',
+        location: '1.1.1.1'
+      }.to_json
+    }
+  end
+
   def test_lambda_handler
     HTTParty.expects(:get).with('http://checkip.amazonaws.com/').returns(mock_response)
     assert_equal(lambda_handler(event: event, context: ''), expected_result)
