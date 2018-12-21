5b91fc4448664305134064fe7207b7e1350e61ec
Heitor Lessa <lessa@amazon.com>
2018-12-21 16:18:43 +0000

    feat: Add next step message for both strongly styped vs dynamic

--- samcli/commands/init/__init__.py
+++ samcli/commands/init/__init__.py
@@ -77,18 +77,32 @@ def do_cli(ctx, location, runtime, output_dir, name, no_input):
     LOG.debug("Init command")
     click.secho("[+] Initializing project structure...", fg="green")
 
-    try:
-        generate_project(location, runtime, output_dir, name, no_input)
-        # Custom templates can implement their own visual cues so let's not repeat the message
-        if not location:
-            next_step_msg = """
+    no_build_msg = """
 Project generated: {output_dir}/{name}
 
-Commands you can use next within the project folder
+Steps you can take next within the project folder
 ===================================================
 [*] Invoke Function: sam local invoke HelloWorldFunction --event event.json
 [*] Start API Gateway locally: sam local start-api
 """.format(output_dir=output_dir, name=name)
+
+    build_msg = """
+Project generated: {output_dir}/{name}
+
+Steps you can take next within the project folder
+===================================================
+[*] Install dependencies
+[*] Invoke Function: sam local invoke HelloWorldFunction --event event.json
+[*] Start API Gateway locally: sam local start-api
+""".format(output_dir=output_dir, name=name)
+
+    no_build_step_required = (
+        "python", "python3.7", "python3.6", "python2.7", "nodejs", "nodejs4.3", "nodejs6.10", "nodejs8.10", "ruby2.5")
+    next_step_msg = no_build_msg if runtime in no_build_step_required else build_msg
+
+    try:
+        generate_project(location, runtime, output_dir, name, no_input)
+        if not location:
             click.secho(next_step_msg, bold=True)
             click.secho("Read {name}/README.md for further instructions\n".format(name=name), bold=True)
         click.secho("[*] Project initialization is now complete", fg="green")
