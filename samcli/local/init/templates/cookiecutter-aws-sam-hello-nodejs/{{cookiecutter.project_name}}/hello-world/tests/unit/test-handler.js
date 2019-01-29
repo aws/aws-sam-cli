94ddd7ff0041c8d7f3755c449ba4a6d63a17cf0c
Sebastian Schürmann <sebs@2xs.org>
2019-01-29 13:15:12 +0100

    Node.js default tests improvements
    * 1 assertion per test. This is easier to digest and debug
    * consistent usage of semicolons and string quotes
    * body and result test split

--- samcli/local/init/templates/cookiecutter-aws-sam-hello-nodejs/{{cookiecutter.project_name}}/hello-world/tests/unit/test-handler.js
+++ samcli/local/init/templates/cookiecutter-aws-sam-hello-nodejs/{{cookiecutter.project_name}}/hello-world/tests/unit/test-handler.js
@@ -6,17 +6,37 @@ const expect = chai.expect;
 var event, context;
 
 describe('Tests index', function () {
-    it('verifies successful response', async () => {
-        const result = await app.lambdaHandler(event, context)
+    let result;
+    before( async () => {
+        result = await app.lambdaHandler(event, context);
+    });
+    
+    describe('result', () => {
+        it('is an object', () => {  
+            expect(result).to.be.an('object');
+        });
+        it('is a successful request', () => { 
+            expect(result.statusCode).to.equal(200);
+        });
+        it('has a string as result body', () => { 
+            expect(result.body).to.be.an('string');
+        });
+    })
 
-        expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
-
-        let response = JSON.parse(result.body);
-
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello world");
-        // expect(response.location).to.be.an("string");
+    describe('result.body', ()=> {
+        let body; 
+        before(() => {
+            body = JSON.parse(result.body);
+        })
+        it('parses to object', () => {
+            expect(body).to.be.an('object');
+        });
+        it('message is hello world', () => {
+            expect(body.message).to.be.equal('hello world');
+        });
+        it('location is a string', () => { 
+            expect(body.location).to.be.an('string');
+        });
     });
 });
+
