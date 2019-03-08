43564aa89c6c9990eef11e84e2755012086b22f2
Vicky Wang <paoptu023@gmail.com>
2019-03-08 12:02:23 -0800

    feat(publish): Add --semantic-version option to sam publish command (#1020)

--- samcli/commands/publish/command.py
+++ samcli/commands/publish/command.py
@@ -1,19 +1,24 @@
 """CLI command for "publish" command."""
 
 import json
+import logging
 
 import click
 import boto3
-from botocore.exceptions import ClientError
 from serverlessrepo import publish_application
 from serverlessrepo.publish import CREATE_APPLICATION
-from serverlessrepo.exceptions import ServerlessRepoError
+from serverlessrepo.parser import METADATA, SERVERLESS_REPO_APPLICATION
+from serverlessrepo.exceptions import ServerlessRepoError, InvalidS3UriError
 
 from samcli.cli.main import pass_context, common_options as cli_framework_options, aws_creds_options
 from samcli.commands._utils.options import template_common_option
 from samcli.commands._utils.template import get_template_data
 from samcli.commands.exceptions import UserException
 
+LOG = logging.getLogger(__name__)
+
+SAM_PUBLISH_DOC = "https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-template-publishing-applications.html" # noqa
+SAM_PACKAGE_DOC = "https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/sam-cli-command-reference-sam-package.html" # noqa
 HELP_TEXT = """
 Use this command to publish a packaged AWS SAM template to
 the AWS Serverless Application Repository to share within your team,
@@ -22,29 +27,32 @@ across your organization, or with the community at large.\n
 This command expects the template's Metadata section to contain an
 AWS::ServerlessRepo::Application section with application metadata
 for publishing. For more details on this metadata section, see
-https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-template-publishing-applications.html
+{}
 \b
 Examples
 --------
 To publish an application
 $ sam publish -t packaged.yaml --region <region>
-"""
+""".format(SAM_PUBLISH_DOC)
 SHORT_HELP = "Publish a packaged AWS SAM template to the AWS Serverless Application Repository."
 SERVERLESSREPO_CONSOLE_URL = "https://console.aws.amazon.com/serverlessrepo/home?region={}#/published-applications/{}"
+SEMANTIC_VERSION_HELP = "Optional. The value provided here overrides SemanticVersion in the template metadata."
+SEMANTIC_VERSION = 'SemanticVersion'
 
 
 @click.command("publish", help=HELP_TEXT, short_help=SHORT_HELP)
 @template_common_option
+@click.option('--semantic-version', help=SEMANTIC_VERSION_HELP)
 @aws_creds_options
 @cli_framework_options
 @pass_context
-def cli(ctx, template):
+def cli(ctx, template, semantic_version):
     # All logic must be implemented in the ``do_cli`` method. This helps with easy unit testing
 
-    do_cli(ctx, template)  # pragma: no cover
+    do_cli(ctx, template, semantic_version)  # pragma: no cover
 
 
-def do_cli(ctx, template):
+def do_cli(ctx, template, semantic_version):
     """Publish the application based on command line inputs."""
     try:
         template_data = get_template_data(template)
@@ -52,16 +60,24 @@ def do_cli(ctx, template):
         click.secho("Publish Failed", fg='red')
         raise UserException(str(ex))
 
+    # Override SemanticVersion in template metadata when provided in command input
+    if semantic_version and SERVERLESS_REPO_APPLICATION in template_data.get(METADATA, {}):
+        template_data.get(METADATA).get(SERVERLESS_REPO_APPLICATION)[SEMANTIC_VERSION] = semantic_version
+
     try:
         publish_output = publish_application(template_data)
         click.secho("Publish Succeeded", fg="green")
-        click.secho(_gen_success_message(publish_output), fg="yellow")
+        click.secho(_gen_success_message(publish_output))
+    except InvalidS3UriError:
+        click.secho("Publish Failed", fg='red')
+        raise UserException(
+            "Your SAM template contains invalid S3 URIs. Please make sure that you have uploaded application "
+            "artifacts to S3 by packaging the template. See more details in {}".format(SAM_PACKAGE_DOC))
     except ServerlessRepoError as ex:
         click.secho("Publish Failed", fg='red')
-        raise UserException(str(ex))
-    except ClientError as ex:
-        click.secho("Publish Failed", fg='red')
-        raise _wrap_s3_uri_exception(ex)
+        LOG.debug("Failed to publish application to serverlessrepo", exc_info=True)
+        error_msg = '{}\nPlease follow the instructions in {}'.format(str(ex), SAM_PUBLISH_DOC)
+        raise UserException(error_msg)
 
     application_id = publish_output.get('application_id')
     _print_console_link(ctx.region, application_id)
@@ -108,28 +124,3 @@ def _print_console_link(region, application_id):
     console_link = SERVERLESSREPO_CONSOLE_URL.format(region, application_id.replace('/', '~'))
     msg = "Click the link below to view your application in AWS console:\n{}".format(console_link)
     click.secho(msg, fg="yellow")
-
-
-def _wrap_s3_uri_exception(ex):
-    """
-    Wrap invalid S3 URI exception with a better error message.
-
-    Parameters
-    ----------
-    ex : ClientError
-        boto3 exception
-
-    Returns
-    -------
-    Exception
-        UserException if found invalid S3 URI or ClientError
-    """
-    error_code = ex.response.get('Error').get('Code')
-    message = ex.response.get('Error').get('Message')
-
-    if error_code == 'BadRequestException' and "Invalid S3 URI" in message:
-        return UserException(
-            "Your SAM template contains invalid S3 URIs. Please make sure that you have uploaded application "
-            "artifacts to S3 by packaging the template: 'sam package --template-file <file-path>'.")
-
-    return ex
