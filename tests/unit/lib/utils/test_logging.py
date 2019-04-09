3ef4ef4ded080607894f9e86ed370df18b8b85ec
Jacob Fuss <jfuss@users.noreply.github.com>
2019-04-09 05:45:11 -0700

    feat(cli-logs): Configure and format SAM CLI logging to console

---
+++ tests/unit/lib/utils/test_logging.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from mock import patch, Mock
+
+from samcli.lib.utils.logging import SamCliLogger
+
+
+class TestSamCliLogger(TestCase):
+
+    @patch("samcli.lib.utils.logging.logging")
+    def test_configure_samcli_logger(self, logging_patch):
+        formatter_mock = Mock()
+        logger_mock = Mock()
+        logging_patch.DEBUG = 2
+
+        stream_handler_mock = Mock()
+        logging_patch.StreamHandler.return_value = stream_handler_mock
+
+        SamCliLogger.configure_logger(logger_mock, formatter_mock, level=1)
+
+        self.assertFalse(logger_mock.propagate)
+
+        logger_mock.setLevel.assert_called_once_with(1)
+        logger_mock.addHandler.assert_called_once_with(stream_handler_mock)
+        stream_handler_mock.setLevel.assert_called_once_with(2)
+        stream_handler_mock.setFormatter.assert_called_once_with(formatter_mock)
