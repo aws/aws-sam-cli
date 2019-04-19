7b947cc5f95f4ffce4770087a1366d86b7fbd225
Bubba <rob@stechstudio.com>
2019-04-19 16:26:24 -0400

    Refactor to simply tracking the extracted_path

--- samcli/local/lambdafn/zip.py
+++ samcli/local/lambdafn/zip.py
@@ -106,6 +106,74 @@ def issymlink(file_info):
     return (file_info.external_attr >> 28) == S_IFLNK
 
 
+def extract(file_info, output_dir, zip_ref):
+    """
+    ----------------------------------------------------------------------------
+    Extract: read the link path string, and make a new symlink.
+
+    'zipinfo' is the link file's ZipInfo object stored in zipfile.
+    'pathto'  is the extract's destination folder (relative or absolute)
+    'zipfile' is the ZipFile object, which reads and parses the zip file.
+
+    On Windows, this requires admin permission and an NTFS destination drive.
+    On Unix, this generally works with any writable drive and normal permission.
+
+    Uses target_is_directory on Windows if flagged as dir in zip bits: it's not
+    impossible that the extract may reach a dir link before its dir target.
+
+    Adjusts link path text for host's separators to make links portable across
+    Windows and Unix, unless 'nofixlinks' (which is command arg -nofixlinks).
+    This is switchable because it assumes the target is a drive to be used
+    on this platform - more likely here than for mergeall external drives.
+
+    Caveat: some of this code mimics that in zipfile.ZipFile._extract_member(),
+    but that library does not expose it for reuse here.  Some of this is also
+    superfluous if we only unzip what we zip (e.g., Windows drive names won't
+    be present and upper dirs will have been created), but that's not ensured.
+
+    In ziptools, pathto already has a '\\?\' long-path prefix on Windows (only);
+    this ensures that the file calls here work regardless of joined-path length.
+
+    TBD: should we also call os.chmod() with the zipinfo's permission bits?
+    TBD: does the UTF8 decoding of the unzip pathname here suffice everywhere?
+    ----------------------------------------------------------------------------
+    """
+
+    if not issymlink(file_info):
+        return zip_ref.extract(file_info, output_dir)
+
+    zippath = file_info.filename  # pathname in the zip
+    linkpath = zipfile.read(zippath)  # original link path str
+    linkpath = linkpath.decode('utf8')  # must be same types
+
+    # undo zip-mandated '/' separators on Windows
+    zippath = zippath.replace('/', os.sep)  # no-op if unix or simple
+
+    # drop Win drive + unc, leading slashes, '.' and '..'
+    zippath = os.path.splitdrive(zippath)[1]
+    zippath = zippath.lstrip(os.sep)  # if other programs' zip
+    allparts = zippath.split(os.sep)
+    okparts = [p for p in allparts if p not in ('.', '..')]
+    zippath = os.sep.join(okparts)
+
+    # where to store link now
+    destpath = os.path.join(output_dir, zippath)  # hosting machine path
+    destpath = os.path.normpath(destpath)  # perhaps moot, but...
+
+    # make leading dirs if needed
+    upperdirs = os.path.dirname(destpath)
+    if not os.path.exists(upperdirs):  # don't fail if exists
+        os.makedirs(upperdirs)  # exists_ok in py 3.2+
+
+    # test+remove link, not target
+    if os.path.lexists(destpath):  # else symlink() fails
+        os.remove(destpath)
+
+    # make the link in dest (mtime: caller)
+    os.symlink(linkpath, destpath)  # store new link in dest
+    return destpath  # mtime is set in caller
+
+
 def unzip(zip_file_path, output_dir, permission=None):
     """
     Unzip the given file into the given directory while preserving file permissions in the process.
@@ -127,6 +195,7 @@ def unzip(zip_file_path, output_dir, permission=None):
         # For each item in the zip file, extract the file and set permissions if available
         for file_info in zip_ref.infolist():
             extracted_path = _extract(file_info, output_dir, zip_ref)
+            extracted_path = extract(file_info, output_dir, zip_ref)
             _set_permissions(file_info, extracted_path)
 
             _override_permissions(extracted_path, permission)
