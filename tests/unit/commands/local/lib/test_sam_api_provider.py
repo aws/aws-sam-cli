1157284fa1b62d99bacd0b20767a4e1437057785
Vikranth Srivatsa <viksriva@amazon.com>
2019-06-24 14:54:45 -0700

    Update AWS::ApiGateway::Stage tests

--- tests/unit/commands/local/lib/test_sam_api_provider.py
+++ tests/unit/commands/local/lib/test_sam_api_provider.py
@@ -1349,6 +1349,220 @@ class TestSamStageValues(TestCase):
         self.assertIn(api3, result)
 
 
+class TestCloudFormationStageValues(TestCase):
+
+    def test_provider_parse_stage_name(self):
+        template = {
+            "Resources": {
+                "Stage": {
+                    "Type": "AWS::ApiGateway::Stage",
+                    "Properties": {
+                        "StageName": "dev",
+                        "RestApiId": "TestApi"
+                    }
+                },
+                "TestApi": {
+                    "Type": "AWS::ApiGateway::RestApi",
+                    "Properties": {
+                        "Body": {
+                            "paths": {
+                                "/path": {
+                                    "get": {
+                                        "x-amazon-apigateway-integration": {
+                                            "httpMethod": "POST",
+                                            "type": "aws_proxy",
+                                            "uri": {
+                                                "Fn::Sub": "arn:aws:apigateway:${AWS::Region}:lambda:path/2015-03-31"
+                                                           "/functions/${NoApiEventFunction.Arn}/invocations",
+                                            },
+                                            "responses": {},
+                                        },
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        provider = SamApiProvider(template)
+        api1 = Api(path='/path', method='GET', function_name='NoApiEventFunction', cors=None, binary_media_types=[],
+                   stage_name='dev',
+                   stage_variables=None)
+
+        self.assertIn(api1, provider.apis)
+
+    def test_provider_stage_variables(self):
+        template = {
+            "Resources": {
+                "Stage": {
+                    "Type": "AWS::ApiGateway::Stage",
+                    "Properties": {
+                        "StageName": "dev",
+                        "Variables": {
+                            "vis": "data",
+                            "random": "test",
+                            "foo": "bar"
+                        },
+                        "RestApiId": "TestApi"
+                    }
+                },
+                "TestApi": {
+                    "Type": "AWS::ApiGateway::RestApi",
+                    "Properties": {
+                        "Body": {
+                            "paths": {
+                                "/path": {
+                                    "get": {
+                                        "x-amazon-apigateway-integration": {
+                                            "httpMethod": "POST",
+                                            "type": "aws_proxy",
+                                            "uri": {
+                                                "Fn::Sub": "arn:aws:apigateway:${AWS::Region}:lambda:path/2015-03-31"
+                                                           "/functions/${NoApiEventFunction.Arn}/invocations",
+                                            },
+                                            "responses": {},
+                                        },
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        provider = SamApiProvider(template)
+        api1 = Api(path='/path', method='GET', function_name='NoApiEventFunction', cors=None, binary_media_types=[],
+                   stage_name='dev',
+                   stage_variables={
+                       "vis": "data",
+                       "random": "test",
+                       "foo": "bar"
+                   })
+
+        self.assertIn(api1, provider.apis)
+
+    def test_multi_stage_get_all(self):
+        template = {
+            "Resources": {
+                "StageDev": {
+                    "Type": "AWS::ApiGateway::Stage",
+                    "Properties": {
+                        "StageName": "dev",
+                        "Variables": {
+                            "vis": "data",
+                            "random": "test",
+                            "foo": "bar"
+                        },
+                        "RestApiId": "TestApi"
+                    }
+                },
+                "StageProd": {
+                    "Type": "AWS::ApiGateway::Stage",
+                    "Properties": {
+                        "StageName": "Production",
+                        "Variables": {
+                            "vis": "prod data",
+                            "random": "test",
+                            "foo": "bar"
+                        },
+                        "RestApiId": "ProductionApi"
+                    },
+                },
+                "TestApi": {
+                    "Type": "AWS::ApiGateway::RestApi",
+                    "Properties": {
+                        "Body": {
+                            "paths": {
+                                "/path2": {
+                                    "get": {
+                                        "x-amazon-apigateway-integration": {
+                                            "httpMethod": "POST",
+                                            "type": "aws_proxy",
+                                            "uri": {
+                                                "Fn::Sub": "arn:aws:apigateway:${AWS::Region}:lambda:path/2015-03-31"
+                                                           "/functions/${NoApiEventFunction.Arn}/invocations",
+                                            },
+                                            "responses": {},
+                                        },
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                "ProductionApi": {
+                    "Type": "AWS::ApiGateway::RestApi",
+                    "Properties": {
+                        "Body": {
+                            "paths": {
+                                "/path": {
+                                    "get": {
+                                        "x-amazon-apigateway-integration": {
+                                            "httpMethod": "POST",
+                                            "type": "aws_proxy",
+                                            "uri": {
+                                                "Fn::Sub": "arn:aws:apigateway:${AWS::Region}:lambda:path/2015-03-31"
+                                                           "/functions/${NoApiEventFunction.Arn}/invocations",
+                                            },
+                                            "responses": {},
+                                        },
+                                    }
+                                },
+                                "/anotherpath": {
+                                    "post": {
+                                        "x-amazon-apigateway-integration": {
+                                            "httpMethod": "POST",
+                                            "type": "aws_proxy",
+                                            "uri": {
+                                                "Fn::Sub": "arn:aws:apigateway:${AWS::Region}:lambda:path/2015-03-31"
+                                                           "/functions/${NoApiEventFunction.Arn}/invocations",
+                                            },
+                                            "responses": {},
+                                        },
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        provider = SamApiProvider(template)
+
+        result = [f for f in provider.get_all()]
+
+        api1 = Api(path='/path2', method='GET', function_name='NoApiEventFunction', cors=None, binary_media_types=[],
+                   stage_name='dev',
+                   stage_variables={
+                       "vis": "data",
+                       "random": "test",
+                       "foo": "bar"
+                   })
+        api2 = Api(path='/path', method='GET', function_name='NoApiEventFunction', cors=None, binary_media_types=[],
+                   stage_name='Production',
+                   stage_variables={
+                       "vis": "prod data",
+                       "random": "test",
+                       "foo": "bar"
+                   })
+        api3 = Api(path='/anotherpath', method='POST', function_name='NoApiEventFunction', cors=None,
+                   binary_media_types=[],
+                   stage_name='Production',
+                   stage_variables={
+                       "vis": "prod data",
+                       "random": "test",
+                       "foo": "bar"
+                   })
+        self.assertEquals(len(result), 3)
+        self.assertIn(api1, result)
+        self.assertIn(api2, result)
+        self.assertIn(api3, result)
+
+
 def make_swagger(apis, binary_media_types=None):
     """
     Given a list of API configurations named tuples, returns a Swagger document
