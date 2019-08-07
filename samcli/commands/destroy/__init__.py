ce762b63728433f769c0441f517cc6e5b61de0b5
Vikranth Srivatsa <viksriva@amazon.com>
2019-08-06 23:50:42 -0700

    Update with Nested Stack prompt

--- samcli/commands/destroy/__init__.py
+++ samcli/commands/destroy/__init__.py
@@ -61,19 +61,19 @@ e.g. sam destroy -stack-name sam-app
 @click.option('-w', '--wait', required=False, is_flag=True, help="Option to wait for Stack deletion")
 @click.option('--wait-time', required=False, type=click.INT,
               help="The time to wait for stack to delete in seconds. Used with --wait. The default is 5 minutes")
-@click.option('-y', '--yes', 'ignore_cli_prompt', required=False, is_flag=True,
+@click.option('-f', '--force', 'ignore_cli_prompt', required=False, is_flag=True,
               help="Deletes the Stack without cli prompt")
 @common_options
 @pass_context
 @track_command
 def cli(ctx, args, stack_name, retain_resources, role_arn, client_request_token, wait, wait_time=300,
-        ignore_cli_prompt=False):
+        force=False):
     """
     Destroys the stack
     """
     # All logic must be implemented in the `do_cli` method. This helps ease unit tests
     do_cli(ctx, stack_name, retain_resources, role_arn, client_request_token, wait, wait_time,
-           ignore_cli_prompt)  # pragma: no cover
+           force)  # pragma: no cover
 
 
 def verify_stack_exists(client, stack_name, required_status=None):
@@ -102,6 +102,29 @@ def verify_stack_exists(client, stack_name, required_status=None):
     if required_status and described_stack['Stacks'][0]['StackStatus'] != required_status:
         click.secho("The stack {} does not have the correct status {}".format(stack_name, required_status), fg="red")
         sys.exit(1)
+
+
+def check_nested_stack(client, stack_name):
+    """
+    Prompts if there is a nested stack in the CloudResource.
+    This is to prevent people from deleting more than they expected.
+    Parameters
+    -----------
+    client: boto3 client
+        The cloudformation boto3 client used for making calls
+    stack_name: str
+        The stack name of the stack to check
+    """
+    template = client.get_template(
+        StackName=stack_name, TemplateStage="Processed"
+    )
+    nested_stacks = [logical_id for logical_id, value in template.get("Resources", {}).items() if
+                     value.get("Type") == "AWS::CloudFormation::Stack"]
+
+    click.confirm(
+        'The stack {} is a nested stack with the following resources: {}. Are you want to destroy them?'.format(
+            stack_name, nested_stacks), default=True,
+        abort=True)
 
 
 def verify_stack_retain_resources(client, stack_name, retain_resources=None):
@@ -138,16 +161,18 @@ def verify_stack_retain_resources(client, stack_name, retain_resources=None):
 
 
 def do_cli(ctx, stack_name, retain_resources=None, role_arn=None, client_request_token=None, wait=None, wait_time=None,
-           ignore_cli_prompt=False):
+           force=False):
     """
     Implementation of the ``cli`` method, just separated out for unit testing purposes
     """
-    if not ignore_cli_prompt:
+    if not force:
         click.confirm('Are you sure you want to delete the stack {}?'.format(stack_name), default=True, abort=True)
     cfn = boto3.client('cloudformation', region_name='us-west-1')
 
     verify_stack_exists(cfn, stack_name)
     verify_stack_retain_resources(cfn, stack_name, retain_resources)
+    if not force:
+        check_nested_stack(cfn, stack_name)
 
     args = {'RoleARN': role_arn, 'ClientRequestToken': client_request_token, 'RetainResources': retain_resources}
 
@@ -159,26 +184,16 @@ def do_cli(ctx, stack_name, retain_resources=None, role_arn=None, client_request
         cfn.delete_stack(StackName=stack_name, **args)
     except ClientError as e:
         if "TerminationProtection" in e.response["Error"]["Message"]:
-            click.secho("""The stack {stack_name} has TerminationProtection turned on. Disable it on the aws console at
-              https://us-west-1.console.aws.amazon.com/cloudformation/home \n or run aws cloudformation update-termination-protection --stack-name {stack_name} --no-enable-termination-protection"""
+            msg = 'The stack {stack_name} has TerminationProtection turned on. Disable it on the aws console at ' + \
+                  'https://us-west-1.console.aws.amazon.com/cloudformation/home \n or ' + \
+                  'run aws cloudformation update-termination-protection --stack-name {stack_name} ' + \
+                  '--no-enable-termination-protection'
+            click.secho(msg
                         .format(stack_name=stack_name), fg="red")
         elif "AccessDeniedException" in e.response["Error"]["Message"]:
-            click.secho("""
-                The user account does not have access to delete the stack. Add the cloudformation:delete policy
-                with the following format to the user account.
-                {
-                    "Version": "2012-10-17",
-                    "Statement": [
-                        {
-                            "Effect": "Allow",
-                            "Action": [
-                            "cloudformation:delete"
-                            ],
-                            "Resource": "*"
-                        }
-                    ]
-                }
-            """, fg="red")
+            click.secho(
+                'The user account does not have access to delete the stack. \n' +
+                'Please update the resources required to delete the stack and the required user policies.', fg="red")
         else:
             click.secho("Failed to destroy Stack: {}".format(str(e.response["Error"]["Message"])), fg="red")
 
