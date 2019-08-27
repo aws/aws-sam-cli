fedd162111c9b5e6995b5bc7d8906ab7b9e245c4
Sriram Madapusi Vasudevan <srirammv@amazon.com>
2019-08-27 11:46:15 -0700

    Revert "Revert "release: 0.20.0""
    This reverts commit 20904315bea1db89362b59550e03ab8460a7d73d.

---
+++ samcli/lib/utils/sam_logging.py
@@ -0,0 +1,31 @@
+"""
+Configures a logger
+"""
+import logging
+
+
+class SamCliLogger(object):
+
+    @staticmethod
+    def configure_logger(logger, formatter, level):
+        """
+        Configure a Logger with the formatter provided.
+
+        Parameters
+        ----------
+        logger logging.getLogger
+            Logger to configure
+        formatter logging.formatter
+            Formatter for the logger
+
+        Returns
+        -------
+        None
+        """
+        log_stream_handler = logging.StreamHandler()
+        log_stream_handler.setLevel(logging.DEBUG)
+        log_stream_handler.setFormatter(formatter)
+
+        logger.setLevel(level)
+        logger.propagate = False
+        logger.addHandler(log_stream_handler)
