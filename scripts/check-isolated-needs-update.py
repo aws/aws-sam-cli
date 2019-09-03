f48c1acbe2121d7f7d5d6822b91b7412f599e448
Jacob Fuss <jfuss@users.noreply.github.com>
2019-09-03 10:20:34 -0700

    Fix isolated needs update script to ignore py2 and handle py library as a special case

--- scripts/check-isolated-needs-update.py
+++ scripts/check-isolated-needs-update.py
@@ -1,4 +1,5 @@
 import io
+import sys
 import os
 from subprocess import Popen, PIPE
 
@@ -22,6 +23,10 @@ def get_requirements_list(content):
 
     return pkgs_versions
 
+# Don't try and compare the isolated list with the Python2 version. SAM CLI installers
+# all use Python3.6+ and Python2.7 is going EOL
+if sys.version_info[0] < 3:
+    sys.exit(0)
 
 isolated_req_content = read(os.path.join("requirements", "isolated.txt"))
 base_req_content = read(os.path.join("requirements", "base.txt"))
@@ -36,13 +41,15 @@ for package in process.stdout.readlines():
     package = package.decode("utf-8").strip(os.linesep)
     all_installed_pkgs_list.append(package)
 
-for installed_pkg in all_installed_pkgs_list:
+for installed_pkg_version in all_installed_pkgs_list:
     for base_req in base_req_list:
         # a base requirement can be defined with different specifiers (>, <, ==, etc.). Instead of doing tons of string parsing,
         # brute force the check by assuming the installed_pkgs will have == as a specifier. This is true due to how pip freeze
         # works. So check to make sure the installed pakcage we are looking at is in the base.txt file, if so make sure the
         # full requirement==version is within the isolated list.
-        if base_req.startswith(installed_pkg.split("==")[0]):
-            assert installed_pkg in isolated_req_list, "{} is in base.txt but not in isolated.txt".format(installed_pkg)
-            print("{} is in the isolated.txt file".format(installed_pkg))
+        installed_pkg = installed_pkg_version.split("==")[0]
+        # There is a py library we use but due to how we are comparing requirements, we need to handle this as a special case. :(
+        if installed_pkg != 'py' and base_req.startswith(installed_pkg):
+            assert installed_pkg_version in isolated_req_list, "{} is in base.txt but not in isolated.txt".format(installed_pkg_version)
+            print("{} is in the isolated.txt file".format(installed_pkg_version))
             break
