7438d9f2292dfaeb98d1cc6c538309e3a563c112
Alex Wood <awood45@gmail.com>
2019-10-17 10:30:22 -0700

    Update Init Integ Tests

--- tests/integration/init/test_init_command.py
+++ tests/integration/init/test_init_command.py
@@ -8,11 +8,67 @@ from backports import tempfile
 class TestBasicInitCommand(TestCase):
     def test_init_command_passes_and_dir_created(self):
         with tempfile.TemporaryDirectory() as temp:
-            process = Popen([TestBasicInitCommand._get_command(), "init", "-o", temp])
+            process = Popen([
+                TestBasicInitCommand._get_command(), "init",
+                "--runtime", "nodejs10.x",
+                "--dependency-manager", "npm",
+                "--app-template", "hello-world",
+                "--name", "sam-app",
+                "--no-interactive",
+                "-o", temp
+            ])
             return_code = process.wait()
 
             self.assertEqual(return_code, 0)
             self.assertTrue(os.path.isdir(temp + "/sam-app"))
+
+    def test_init_new_app_template(self):
+        with tempfile.TemporaryDirectory() as temp:
+            process = Popen([
+                TestBasicInitCommand._get_command(), "init",
+                "--runtime", "nodejs10.x",
+                "--dependency-manager", "npm",
+                "--app-template", "quick-start-from-scratch",
+                "--name", "qs-scratch",
+                "--no-interactive",
+                "-o", temp
+            ])
+            return_code = process.wait()
+
+            self.assertEqual(return_code, 0)
+            self.assertTrue(os.path.isdir(temp + "/qs-scratch"))
+
+    def test_init_command_java_maven(self):
+        with tempfile.TemporaryDirectory() as temp:
+            process = Popen([
+                TestBasicInitCommand._get_command(), "init",
+                "--runtime", "java8",
+                "--dependency-manager", "maven",
+                "--app-template", "hello-world",
+                "--name", "sam-app-maven",
+                "--no-interactive",
+                "-o", temp
+            ])
+            return_code = process.wait()
+
+            self.assertEqual(return_code, 0)
+            self.assertTrue(os.path.isdir(temp + "/sam-app-maven"))
+
+    def test_init_command_java_gradle(self):
+        with tempfile.TemporaryDirectory() as temp:
+            process = Popen([
+                TestBasicInitCommand._get_command(), "init",
+                "--runtime", "java8",
+                "--dependency-manager", "gradle",
+                "--app-template", "hello-world",
+                "--name", "sam-app-gradle",
+                "--no-interactive",
+                "-o", temp
+            ])
+            return_code = process.wait()
+
+            self.assertEqual(return_code, 0)
+            self.assertTrue(os.path.isdir(temp + "/sam-app-gradle"))
 
     @staticmethod
     def _get_command():
