290032447d467d8e12a7daf6cde603bda4b212db
Alex Wood <awood45@gmail.com>
2019-10-23 09:31:28 -0700

    Run of Black Reformatting

--- tests/integration/init/test_init_command.py
+++ tests/integration/init/test_init_command.py
@@ -8,15 +8,23 @@ from backports import tempfile
 class TestBasicInitCommand(TestCase):
     def test_init_command_passes_and_dir_created(self):
         with tempfile.TemporaryDirectory() as temp:
-            process = Popen([
-                TestBasicInitCommand._get_command(), "init",
-                "--runtime", "nodejs10.x",
-                "--dependency-manager", "npm",
-                "--app-template", "hello-world",
-                "--name", "sam-app",
-                "--no-interactive",
-                "-o", temp
-            ])
+            process = Popen(
+                [
+                    TestBasicInitCommand._get_command(),
+                    "init",
+                    "--runtime",
+                    "nodejs10.x",
+                    "--dependency-manager",
+                    "npm",
+                    "--app-template",
+                    "hello-world",
+                    "--name",
+                    "sam-app",
+                    "--no-interactive",
+                    "-o",
+                    temp,
+                ]
+            )
             return_code = process.wait()
 
             self.assertEqual(return_code, 0)
@@ -24,15 +32,23 @@ class TestBasicInitCommand(TestCase):
 
     def test_init_new_app_template(self):
         with tempfile.TemporaryDirectory() as temp:
-            process = Popen([
-                TestBasicInitCommand._get_command(), "init",
-                "--runtime", "nodejs10.x",
-                "--dependency-manager", "npm",
-                "--app-template", "quick-start-from-scratch",
-                "--name", "qs-scratch",
-                "--no-interactive",
-                "-o", temp
-            ])
+            process = Popen(
+                [
+                    TestBasicInitCommand._get_command(),
+                    "init",
+                    "--runtime",
+                    "nodejs10.x",
+                    "--dependency-manager",
+                    "npm",
+                    "--app-template",
+                    "quick-start-from-scratch",
+                    "--name",
+                    "qs-scratch",
+                    "--no-interactive",
+                    "-o",
+                    temp,
+                ]
+            )
             return_code = process.wait()
 
             self.assertEqual(return_code, 0)
@@ -40,15 +56,23 @@ class TestBasicInitCommand(TestCase):
 
     def test_init_command_java_maven(self):
         with tempfile.TemporaryDirectory() as temp:
-            process = Popen([
-                TestBasicInitCommand._get_command(), "init",
-                "--runtime", "java8",
-                "--dependency-manager", "maven",
-                "--app-template", "hello-world",
-                "--name", "sam-app-maven",
-                "--no-interactive",
-                "-o", temp
-            ])
+            process = Popen(
+                [
+                    TestBasicInitCommand._get_command(),
+                    "init",
+                    "--runtime",
+                    "java8",
+                    "--dependency-manager",
+                    "maven",
+                    "--app-template",
+                    "hello-world",
+                    "--name",
+                    "sam-app-maven",
+                    "--no-interactive",
+                    "-o",
+                    temp,
+                ]
+            )
             return_code = process.wait()
 
             self.assertEqual(return_code, 0)
@@ -56,15 +80,23 @@ class TestBasicInitCommand(TestCase):
 
     def test_init_command_java_gradle(self):
         with tempfile.TemporaryDirectory() as temp:
-            process = Popen([
-                TestBasicInitCommand._get_command(), "init",
-                "--runtime", "java8",
-                "--dependency-manager", "gradle",
-                "--app-template", "hello-world",
-                "--name", "sam-app-gradle",
-                "--no-interactive",
-                "-o", temp
-            ])
+            process = Popen(
+                [
+                    TestBasicInitCommand._get_command(),
+                    "init",
+                    "--runtime",
+                    "java8",
+                    "--dependency-manager",
+                    "gradle",
+                    "--app-template",
+                    "hello-world",
+                    "--name",
+                    "sam-app-gradle",
+                    "--no-interactive",
+                    "-o",
+                    temp,
+                ]
+            )
             return_code = process.wait()
 
             self.assertEqual(return_code, 0)
