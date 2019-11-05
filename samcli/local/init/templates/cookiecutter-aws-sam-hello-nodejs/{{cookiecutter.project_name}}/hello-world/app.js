2cc0adfb060e7977a5cf088d5a73fbcf95ff78fa
Kyle Roach <kroach.work@gmail.com>
2019-11-05 09:49:11 -0400

    feat: Type hinting for for nodejs hello-world template
    Adds type hints and autocomplete for context, and event parameters on the lambdaHandler.
    
    Knowing what values exist in these parameters can be daunting, and usually require going to the links provided in the jsdoc. Having types here reduces that friction and keeps the developer in the editor.

--- samcli/local/init/templates/cookiecutter-aws-sam-hello-nodejs/{{cookiecutter.project_name}}/hello-world/app.js
+++ samcli/local/init/templates/cookiecutter-aws-sam-hello-nodejs/{{cookiecutter.project_name}}/hello-world/app.js
@@ -5,13 +5,13 @@ let response;
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
+ * @param {AWSLambda.APIGatewayEvent} event - API Gateway Lambda Proxy Input Format
  *
  * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
+ * @param {AWSLambda.Context} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * @returns {Promise<Object>} object - API Gateway Lambda Proxy Output Format
  * 
  */
 exports.lambdaHandler = async (event, context) => {
