8a1d190885d122e73ee4349bf2c1110cd09bc5bb
Sriram Madapusi Vasudevan <srirammv@amazon.com>
2019-11-18 23:56:49 -0800

    feat: allow `--interactive` to ask for parameters
    - if parameters are present on the tempalte, ask for parameters

--- tests/unit/commands/deploy/test_command.py
+++ tests/unit/commands/deploy/test_command.py
@@ -87,8 +87,10 @@ class TestDeployliCommand(TestCase):
     @patch("samcli.commands.deploy.deploy_context.DeployContext")
     @patch("samcli.commands.deploy.command.save_config")
     @patch("samcli.commands.deploy.command.manage_stack")
+    @patch("samcli.commands.deploy.command.get_template_parameters")
     def test_all_args_interactive(
         self,
+        mock_get_template_parameters,
         mock_managed_stack,
         mock_save_config,
         mock_deploy_context,
@@ -98,9 +100,94 @@ class TestDeployliCommand(TestCase):
     ):
 
         context_mock = Mock()
+        mock_get_template_parameters.return_value = {"Myparameter": {"Type": "String"}}
         mock_deploy_context.return_value.__enter__.return_value = context_mock
-        mock_deploy_click.prompt = MagicMock(side_effect=["sam-app", "us-east-1"])
-        mock_deploy_click.confirm = MagicMock(side_effect=[True, True, True])
+        mock_deploy_click.prompt = MagicMock(
+            side_effect=["sam-app", "us-east-1", "InteractiveParameter", ("CAPABILITY_IAM",)]
+        )
+        mock_deploy_click.confirm = MagicMock(side_effect=[True, False, True])
+
+        mock_managed_stack.return_value = "managed-s3-bucket"
+        mock_save_config.return_value = True
+
+        do_cli(
+            template_file=self.template_file,
+            stack_name=self.stack_name,
+            s3_bucket=None,
+            force_upload=self.force_upload,
+            s3_prefix=self.s3_prefix,
+            kms_key_id=self.kms_key_id,
+            parameter_overrides=self.parameter_overrides,
+            capabilities=self.capabilities,
+            no_execute_changeset=self.no_execute_changeset,
+            role_arn=self.role_arn,
+            notification_arns=self.notification_arns,
+            fail_on_empty_changeset=self.fail_on_empty_changset,
+            tags=self.tags,
+            region=self.region,
+            profile=self.profile,
+            use_json=self.use_json,
+            metadata=self.metadata,
+            interactive=True,
+            confirm_changeset=True,
+        )
+
+        mock_deploy_context.assert_called_with(
+            template_file=ANY,
+            stack_name="sam-app",
+            s3_bucket="managed-s3-bucket",
+            force_upload=self.force_upload,
+            s3_prefix=self.s3_prefix,
+            kms_key_id=self.kms_key_id,
+            parameter_overrides={"Myparameter": "InteractiveParameter"},
+            capabilities=self.capabilities,
+            no_execute_changeset=self.no_execute_changeset,
+            role_arn=self.role_arn,
+            notification_arns=self.notification_arns,
+            fail_on_empty_changeset=self.fail_on_empty_changset,
+            tags=self.tags,
+            region="us-east-1",
+            profile=self.profile,
+            confirm_changeset=True,
+        )
+
+        context_mock.run.assert_called_with()
+        mock_save_config.assert_called_with(
+            "input-template-file",
+            capabilities=("CAPABILITY_IAM",),
+            confirm_changeset=True,
+            profile=self.profile,
+            region="us-east-1",
+            s3_bucket="managed-s3-bucket",
+            stack_name="sam-app",
+            parameter_overrides={"Myparameter": "InteractiveParameter"},
+        )
+        mock_managed_stack.assert_called_with(profile=self.profile, region="us-east-1")
+        self.assertEqual(context_mock.run.call_count, 1)
+
+    @patch("samcli.commands.package.command.click")
+    @patch("samcli.commands.package.package_context.PackageContext")
+    @patch("samcli.commands.deploy.command.click")
+    @patch("samcli.commands.deploy.deploy_context.DeployContext")
+    @patch("samcli.commands.deploy.command.save_config")
+    @patch("samcli.commands.deploy.command.manage_stack")
+    @patch("samcli.commands.deploy.command.get_template_parameters")
+    def test_all_args_interactive_no_params(
+        self,
+        mock_get_template_parameters,
+        mock_managed_stack,
+        mock_save_config,
+        mock_deploy_context,
+        mock_deploy_click,
+        mock_package_context,
+        mock_package_click,
+    ):
+
+        context_mock = Mock()
+        mock_get_template_parameters.return_value = {}
+        mock_deploy_context.return_value.__enter__.return_value = context_mock
+        mock_deploy_click.prompt = MagicMock(side_effect=["sam-app", "us-east-1", ("CAPABILITY_IAM",)])
+        mock_deploy_click.confirm = MagicMock(side_effect=[True, False, True])
 
         mock_managed_stack.return_value = "managed-s3-bucket"
         mock_save_config.return_value = True
@@ -155,6 +242,7 @@ class TestDeployliCommand(TestCase):
             region="us-east-1",
             s3_bucket="managed-s3-bucket",
             stack_name="sam-app",
+            parameter_overrides=self.parameter_overrides,
         )
         mock_managed_stack.assert_called_with(profile=self.profile, region="us-east-1")
         self.assertEqual(context_mock.run.call_count, 1)
