ad9c9d4c2fb8d1bbfcfdc013a156724a3a552cf7
Alex Wood <awood45@gmail.com>
2019-11-20 12:50:05 -0800

    Add Exception Handling for Client Misconfiguration

--- samcli/lib/bootstrap/bootstrap.py
+++ samcli/lib/bootstrap/bootstrap.py
@@ -19,9 +19,17 @@ SAM_CLI_STACK_NAME = "aws-sam-cli-managed-stack"
 
 
 def manage_stack(profile, region):
-    session = boto3.Session(profile_name=profile if profile else None)
-    cloudformation_client = session.client("cloudformation", config=Config(region_name=region if region else None))
-
+    try:
+        session = boto3.Session(profile_name=profile if profile else None)
+        cloudformation_client = session.client("cloudformation", config=Config(region_name=region if region else None))
+    except NoCredentialsError:
+        raise UserException(
+            "Unable to resolve credentials for the AWS SDK for Python client. Please see their documentation for options to pass in credentials: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html"
+        )
+    except NoRegionError:
+        raise UserException(
+            "Unable to resolve a region. Please provide a region via the --region parameter or by the AWS_REGION environment variable."
+        )
     return _create_or_get_stack(cloudformation_client)
 
 
