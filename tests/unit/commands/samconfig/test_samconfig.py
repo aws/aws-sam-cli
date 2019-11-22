5f43d5134c6402deeac2f8c0fc2e6ce1a1b8e8c1
Sanath Kumar Ramesh <sanathkr@amazon.com>
2019-11-21 23:52:32 -0800

    adding few more unit tests

--- tests/unit/commands/samconfig/test_samconfig.py
+++ tests/unit/commands/samconfig/test_samconfig.py
@@ -12,48 +12,20 @@ from samcli.lib.config.samconfig import SamConfig, DEFAULT_ENV
 from click.testing import CliRunner
 
 from unittest import TestCase
-from mock import patch
+from mock import patch, ANY
 import logging
 
 LOG = logging.getLogger()
 logging.basicConfig()
 
-config = """
-version = 0.1
-
-[default.build.parameters]
-profile="srirammv"
-parameter_overrides="Stage=Prod Version=python3.7"
-debug=true
-skip_pull_image=true
-use_container=true
-
-[default.package.parameters]
-profile="srirammv"
-region="us-east-1"
-s3_bucket="windowssam"
-output_template_file="packaged.yaml"
-
-[default.deploy.parameters]
-parameter_overrides="Stage=Prod Version=python3.7"
-stack_name="ennamo"
-capabilities="CAPABILITY_IAM CAPABILITY_NAMED_IAM"
-region="us-east-1"
-profile="srirammv"
-s3_bucket="sam-app-package"
-
-[default.local_start_api.parameters]
-port=5401
-
-[default.local_generate_event_alexa_skills_kit_intent_answer.parameters]
-session_id="from_config"
-"""
-
 
 class TestSamConfigForAllCommands(TestCase):
     def setUp(self):
         self._old_cwd = os.getcwd()
+
         self.scratch_dir = tempfile.mkdtemp()
+        Path(self.scratch_dir, "envvar.json").write_text("{}")
+
         os.chdir(self.scratch_dir)
 
     def tearDown(self):
@@ -70,20 +42,205 @@ class TestSamConfigForAllCommands(TestCase):
     def test_build(self):
         pass
 
-    def test_local_invoke(self):
-        pass
+    @patch("samcli.commands.local.invoke.cli.do_cli")
+    def test_local_invoke(self, do_cli_mock):
+        config_values = {
+            "function_identifier": "foo",
+            "template_file": "mytemplate.yaml",
+            "event": "event",
+            "no_event": False,
+            "env_vars": "envvar.json",
+            "debug_port": [1, 2, 3],
+            "debug_args": "args",
+            "debugger_path": "mypath",
+            "docker_volume_basedir": "basedir",
+            "docker_network": "mynetwork",
+            "log_file": "logfile",
+            "layer_cache_basedir": "basedir",
+            "skip_pull_image": True,
+            "force_image_build": True,
+            "parameter_overrides": "ParameterKey=Key,ParameterValue=Value ParameterKey=Key2,ParameterValue=Value2",
+        }
 
-    def test_local_generate_event(self):
-        pass
+        # NOTE: Because we don't load the full Click BaseCommand here, this is mounted as top-level command
+        with samconfig_parameters(["invoke"], self.scratch_dir, **config_values) as config_path:
 
-    def test_local_start_api(self):
-        pass
+            from samcli.commands.local.invoke.cli import cli
 
-    def test_local_start_lambda(self):
-        pass
+            runner = CliRunner()
+            result = runner.invoke(cli, [])
 
-    def test_package(self):
-        pass
+            LOG.info(result.output)
+            LOG.info(result.exception)
+            if result.exception:
+                LOG.exception("Command failed", exc_info=result.exc_info)
+            self.assertIsNone(result.exception)
+
+            do_cli_mock.assert_called_with(
+                ANY,
+                "foo",
+                str(Path(os.getcwd(), "mytemplate.yaml")),
+                "event",
+                False,
+                "envvar.json",
+                (1, 2, 3),
+                "args",
+                "mypath",
+                "basedir",
+                "mynetwork",
+                "logfile",
+                "basedir",
+                True,
+                True,
+                {"Key": "Value", "Key2": "Value2"},
+            )
+
+    @patch("samcli.commands.local.start_api.cli.do_cli")
+    def test_local_start_api(self, do_cli_mock):
+
+        config_values = {
+            "template_file": "mytemplate.yaml",
+            "host": "127.0.0.1",
+            "port": 12345,
+            "static_dir": "static_dir",
+            "env_vars": "envvar.json",
+            "debug_port": [1, 2, 3],
+            "debug_args": "args",
+            "debugger_path": "mypath",
+            "docker_volume_basedir": "basedir",
+            "docker_network": "mynetwork",
+            "log_file": "logfile",
+            "layer_cache_basedir": "basedir",
+            "skip_pull_image": True,
+            "force_image_build": True,
+            "parameter_overrides": "ParameterKey=Key,ParameterValue=Value ParameterKey=Key2,ParameterValue=Value2",
+        }
+
+        # NOTE: Because we don't load the full Click BaseCommand here, this is mounted as top-level command
+        with samconfig_parameters(["start-api"], self.scratch_dir, **config_values) as config_path:
+
+            from samcli.commands.local.start_api.cli import cli
+
+            runner = CliRunner()
+            result = runner.invoke(cli, [])
+
+            LOG.info(result.output)
+            LOG.info(result.exception)
+            if result.exception:
+                LOG.exception("Command failed", exc_info=result.exc_info)
+            self.assertIsNone(result.exception)
+
+            do_cli_mock.assert_called_with(
+                ANY,
+                "127.0.0.1",
+                12345,
+                "static_dir",
+                str(Path(os.getcwd(), "mytemplate.yaml")),
+                "envvar.json",
+                (1, 2, 3),
+                "args",
+                "mypath",
+                "basedir",
+                "mynetwork",
+                "logfile",
+                "basedir",
+                True,
+                True,
+                {"Key": "Value", "Key2": "Value2"},
+            )
+
+    @patch("samcli.commands.local.start_lambda.cli.do_cli")
+    def test_local_start_lambda(self, do_cli_mock):
+
+        config_values = {
+            "template_file": "mytemplate.yaml",
+            "host": "127.0.0.1",
+            "port": 12345,
+            "env_vars": "envvar.json",
+            "debug_port": [1, 2, 3],
+            "debug_args": "args",
+            "debugger_path": "mypath",
+            "docker_volume_basedir": "basedir",
+            "docker_network": "mynetwork",
+            "log_file": "logfile",
+            "layer_cache_basedir": "basedir",
+            "skip_pull_image": True,
+            "force_image_build": True,
+            "parameter_overrides": "ParameterKey=Key,ParameterValue=Value",
+        }
+
+        # NOTE: Because we don't load the full Click BaseCommand here, this is mounted as top-level command
+        with samconfig_parameters(["start-lambda"], self.scratch_dir, **config_values) as config_path:
+
+            from samcli.commands.local.start_lambda.cli import cli
+
+            runner = CliRunner()
+            result = runner.invoke(cli, [])
+
+            LOG.info(result.output)
+            LOG.info(result.exception)
+            if result.exception:
+                LOG.exception("Command failed", exc_info=result.exc_info)
+            self.assertIsNone(result.exception)
+
+            do_cli_mock.assert_called_with(
+                ANY,
+                "127.0.0.1",
+                12345,
+                str(Path(os.getcwd(), "mytemplate.yaml")),
+                "envvar.json",
+                (1, 2, 3),
+                "args",
+                "mypath",
+                "basedir",
+                "mynetwork",
+                "logfile",
+                "basedir",
+                True,
+                True,
+                {"Key": "Value"},
+            )
+
+    @patch("samcli.commands.package.command.do_cli")
+    def test_package(self, do_cli_mock):
+
+        config_values = {
+            "template_file": "mytemplate.yaml",
+            "s3_bucket": "mybucket",
+            "force_upload": True,
+            "s3_prefix": "myprefix",
+            "kms_key_id": "mykms",
+            "use_json": True,
+            "metadata": '{"m1": "value1", "m2": "value2"}',
+            "region": "myregion",
+            "output_template_file": "output.yaml",
+        }
+
+        with samconfig_parameters(["package"], self.scratch_dir, **config_values) as config_path:
+
+            from samcli.commands.package.command import cli
+
+            runner = CliRunner()
+            result = runner.invoke(cli, [])
+
+            LOG.info(result.output)
+            LOG.info(result.exception)
+            if result.exception:
+                LOG.exception("Command failed", exc_info=result.exc_info)
+            self.assertIsNone(result.exception)
+
+            do_cli_mock.assert_called_with(
+                str(Path(os.getcwd(), "mytemplate.yaml")),
+                "mybucket",
+                "myprefix",
+                "mykms",
+                "output.yaml",
+                True,
+                True,
+                {"m1": "value1", "m2": "value2"},
+                "myregion",
+                None,
+            )
 
     @patch("samcli.commands.deploy.command.do_cli")
     def test_deploy(self, do_cli_mock):
