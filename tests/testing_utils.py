af079d74d751cb55eb12120235d410664980c275
Tarun <c2tarun@users.noreply.github.com>
2020-02-27 12:19:39 -0800

    feat: adding separate bucket based on python version for integration tests. (#1828)
    * feat: adding separate bucket based on python version for integration tests.
    
    * Better code formatting.
    
    * Code cleanup after udpating AppVeyor.

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -3,7 +3,6 @@ import platform
 import tempfile
 import shutil
 
-
 IS_WINDOWS = platform.system().lower() == "windows"
 RUNNING_ON_CI = os.environ.get("APPVEYOR", False)
 RUNNING_TEST_FOR_MASTER_ON_CI = os.environ.get("APPVEYOR_REPO_BRANCH", "master") != "master"
