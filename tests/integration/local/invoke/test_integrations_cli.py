7bc702c6774be1870c64f401d10a9ed667b26357
Tarun <c2tarun@users.noreply.github.com>
2020-03-18 08:33:19 -0700

    fix: changing order of parameterized expand and flaky annotation. (#1873)

--- tests/integration/local/invoke/test_integrations_cli.py
+++ tests/integration/local/invoke/test_integrations_cli.py
@@ -89,10 +89,10 @@ class TestSamPython36HelloWorldIntegration(InvokeIntegBase):
         process_stdout = stdout.strip()
         self.assertEqual(process_stdout.decode("utf-8"), '"Hello world"')
 
-    @pytest.mark.flaky(reruns=3)
     @parameterized.expand(
         [("TimeoutFunction"), ("TimeoutFunctionWithParameter"), ("TimeoutFunctionWithStringParameter")]
     )
+    @pytest.mark.flaky(reruns=3)
     def test_invoke_with_timeout_set(self, function_name):
         command_list = self.get_command_list(
             function_name, template_path=self.template_path, event_path=self.event_path
