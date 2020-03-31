0422bcf6c70fcea47309962db09221bddc3d5fbc
Sriram Madapusi Vasudevan <3770774+sriram-mv@users.noreply.github.com>
2020-03-31 12:29:27 -0700

    feat: dotnetcore3.1 support (#1908)
    * feat: dotnetcore3.1 support
    
    Why is this change necessary?
    
    * Add support for init, build, local invoke on dotnetcore3.1 runtimes.
    
    * fix: install latest dotnet core sdk - appveyor windows
    
    Why is this change necessary?
    
    * Appveyor should already have dotnetcore3.1: https://www.appveyor.com/docs/windows-images-software/#net-framework, but does not seem to be picked up.

---
+++ tests/integration/testdata/buildcmd/Dotnetcore3.1/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace HelloWorld
+{
+
+    public class Function
+    {
+
+        public string FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
+        {
+            return "{'message': 'Hello World'}";
+        }
+    }
+}
