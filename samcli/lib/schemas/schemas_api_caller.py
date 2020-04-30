b964325f337740ea2129f7d5f5f35a6d382cea75
Neha Gupta <nehagup@amazon.com>
2020-04-29 17:00:03 -0700

    updated cli to read schemas region from boto3

--- samcli/lib/schemas/schemas_api_caller.py
+++ samcli/lib/schemas/schemas_api_caller.py
@@ -3,12 +3,12 @@
 import json
 from json import JSONDecodeError
 
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError, EndpointConnectionError
 
 from samcli.lib.schemas.schemas_directory_hierarchy_builder import sanitize_name, get_package_hierarchy
 from samcli.lib.schemas.schemas_constants import DEFAULT_EVENT_SOURCE, DEFAULT_EVENT_DETAIL_TYPE
 from samcli.commands.exceptions import SchemasApiException
-from samcli.commands.local.cli_common.user_exceptions import ResourceNotFound
+from samcli.commands.local.cli_common.user_exceptions import ResourceNotFound, NotAvailableInRegion
 
 
 class SchemasApiCaller:
@@ -19,58 +19,74 @@ class SchemasApiCaller:
         if limit is None:
             limit = 10
         registries = []
-        paginator = self._schemas_client.get_paginator("list_registries")
-        page_iterator = paginator.paginate(
-            PaginationConfig={"StartingToken": next_token, "MaxItems": limit, "PageSize": limit}
-        )
-        page = None
-        for page in page_iterator:
-            for registry in page["Registries"]:
-                registries.append(registry["RegistryName"])
-        if not registries:
-            raise ResourceNotFound("No Registries found. This should not be possible, please raise an issue.")
-        next_token = page.get("NextToken", None)
-        return {"registries": registries, "next_token": next_token}
+        try:
+            paginator = self._schemas_client.get_paginator("list_registries")
+            page_iterator = paginator.paginate(
+                PaginationConfig={"StartingToken": next_token, "MaxItems": limit, "PageSize": limit}
+            )
+            page = None
+            for page in page_iterator:
+                for registry in page["Registries"]:
+                    registries.append(registry["RegistryName"])
+            if not registries:
+                raise ResourceNotFound("No Registries found. This should not be possible, please raise an issue.")
+            next_token = page.get("NextToken", None)
+            return {"registries": registries, "next_token": next_token}
+        except EndpointConnectionError:
+            raise NotAvailableInRegion("EventBridge Schemas are not available in provided region. "
+                                       "Please check AWS doc for Schemas supported regions.")
 
     def list_schemas(self, registry_name, next_token=None, limit=10):
         schemas = []
-        paginator = self._schemas_client.get_paginator("list_schemas")
-        page_iterator = paginator.paginate(
-            RegistryName=registry_name,
-            PaginationConfig={"StartingToken": next_token, "MaxItems": limit, "PageSize": limit},
-        )
-        page = None
-        for page in page_iterator:
-            for schema in page["Schemas"]:
-                schemas.append(schema["SchemaName"])
-        if not schemas:
-            raise ResourceNotFound("No Schemas found for registry %s" % registry_name)
-        next_token = page.get("NextToken", None)
-        return {"schemas": schemas, "next_token": next_token}
+        try:
+            paginator = self._schemas_client.get_paginator("list_schemas")
+            page_iterator = paginator.paginate(
+                RegistryName=registry_name,
+                PaginationConfig={"StartingToken": next_token, "MaxItems": limit, "PageSize": limit},
+            )
+            page = None
+            for page in page_iterator:
+                for schema in page["Schemas"]:
+                    schemas.append(schema["SchemaName"])
+            if not schemas:
+                raise ResourceNotFound("No Schemas found for registry %s" % registry_name)
+            next_token = page.get("NextToken", None)
+            return {"schemas": schemas, "next_token": next_token}
+        except EndpointConnectionError:
+            raise NotAvailableInRegion("EventBridge Schemas are not available in provided region. "
+                                       "Please check AWS doc for Schemas supported regions.")
 
     def get_latest_schema_version(self, registry_name, schema_name):
         versions = []
         next_token = None
-        while True:
-            paginator = self._schemas_client.get_paginator("list_schema_versions")
-            page_iterator = paginator.paginate(
-                RegistryName=registry_name, SchemaName=schema_name, PaginationConfig={"StartingToken": next_token}
-            )
-            page = None
-            for page in page_iterator:
-                for version in page["SchemaVersions"]:
-                    versions.append(version["SchemaVersion"])
+        try:
+            while True:
+                paginator = self._schemas_client.get_paginator("list_schema_versions")
+                page_iterator = paginator.paginate(
+                    RegistryName=registry_name, SchemaName=schema_name, PaginationConfig={"StartingToken": next_token}
+                )
+                page = None
+                for page in page_iterator:
+                    for version in page["SchemaVersions"]:
+                        versions.append(version["SchemaVersion"])
 
-            next_token = page.get("NextToken")
-            if next_token is None:
-                break
+                next_token = page.get("NextToken")
+                if next_token is None:
+                    break
+        except EndpointConnectionError:
+            raise NotAvailableInRegion("EventBridge Schemas are not available in provided region. "
+                                       "Please check AWS doc for Schemas supported regions.")
         versions.sort()
         return versions[-1]
 
     def get_schema_metadata(self, registry_name, schema_name):
-        describe_schema_response = self._schemas_client.describe_schema(
-            RegistryName=registry_name, SchemaName=schema_name
-        )
+        try:
+            describe_schema_response = self._schemas_client.describe_schema(
+                RegistryName=registry_name, SchemaName=schema_name
+            )
+        except EndpointConnectionError:
+            raise NotAvailableInRegion("EventBridge Schemas are not available in provided region. "
+                                       "Please check AWS doc for Schemas supported regions.")
         try:
             content = json.loads(describe_schema_response["Content"])
             schemas = content["components"]["schemas"]
@@ -86,7 +102,7 @@ class SchemasApiCaller:
                 if aws_event.get("x-amazon-events-detail-type") is not None:
                     event_source_detail_type = aws_event.get("x-amazon-events-detail-type")
                 possible_root_schema_name = aws_event["properties"]["detail"]["$ref"]
-                schema_root_name = sanitize_name(possible_root_schema_name[len("#/components/schemas/") :])
+                schema_root_name = sanitize_name(possible_root_schema_name[len("#/components/schemas/"):])
             return {
                 "event_source": event_source,
                 "event_source_detail_type": event_source_detail_type,
@@ -100,9 +116,13 @@ class SchemasApiCaller:
             )
 
     def download_source_code_binding(self, runtime, registry_name, schema_name, schema_version, download_location):
-        response = self._schemas_client.get_code_binding_source(
-            Language=runtime, RegistryName=registry_name, SchemaName=schema_name, SchemaVersion=schema_version
-        )
+        try:
+            response = self._schemas_client.get_code_binding_source(
+                Language=runtime, RegistryName=registry_name, SchemaName=schema_name, SchemaVersion=schema_version
+            )
+        except EndpointConnectionError:
+            raise NotAvailableInRegion("EventBridge Schemas are not available in provided region. "
+                                       "Please check AWS doc for Schemas supported regions.")
 
         for data in response["Body"]:
             download_location.write(data)
@@ -112,12 +132,20 @@ class SchemasApiCaller:
             self._schemas_client.put_code_binding(
                 Language=runtime, RegistryName=registry_name, SchemaName=schema_name, SchemaVersion=schema_version
             )
+        except EndpointConnectionError:
+            raise NotAvailableInRegion("EventBridge Schemas are not available in provided region. "
+                                       "Please check AWS doc for Schemas supported regions.")
         except ClientError as e:
             if e.response["Error"]["Code"] != "ConflictException":
                 raise e
 
     def poll_for_code_binding_status(self, schemas_runtime, registry_name, schema_name, schema_version):
-        waiter = self._schemas_client.get_waiter("code_binding_exists")
-        waiter.wait(
-            Language=schemas_runtime, RegistryName=registry_name, SchemaName=schema_name, SchemaVersion=schema_version
-        )
+        try:
+            waiter = self._schemas_client.get_waiter("code_binding_exists")
+            waiter.wait(
+                Language=schemas_runtime, RegistryName=registry_name, SchemaName=schema_name,
+                SchemaVersion=schema_version
+            )
+        except EndpointConnectionError:
+            raise NotAvailableInRegion("EventBridge Schemas are not available in provided region. "
+                                       "Please check AWS doc for Schemas supported regions.")
--- tests/unit/lib/schemas/test_schemas_aws_config.py
+++ tests/unit/lib/schemas/test_schemas_aws_config.py
@@ -26,10 +26,11 @@ class TestInitAWSConfiguration(TestCase):
     @patch("click.prompt")
     def test_get_aws_configuration_choice_selected(self, prompt_mock, confirm_mock, session_mock):
         confirm_mock.side_effect = [False]
-        prompt_mock.side_effect = ["2", "2"]
+        prompt_mock.side_effect = ["2", "us-east-2"]
         session_mock.return_value.profile_name = "default"
         session_mock.return_value.region_name = "us-west-2"
         session_mock.return_value.available_profiles = ["default", "test-profile"]
+        session_mock.return_value.get_available_regions.return_value = ["us-east-1", "us-east-2", "us-west-2", "eu-west-1", "ap-northeast-1"]
         aws_configuration_choice = get_aws_configuration_choice()
         self.assertEqual(aws_configuration_choice["profile"], "test-profile"),
         self.assertEqual(aws_configuration_choice["region"], "us-east-2")
@@ -37,7 +38,7 @@ class TestInitAWSConfiguration(TestCase):
             "\nDo you want to use the default AWS profile [default] and region [us-west-2]?", default=True
         )
         prompt_mock.assert_any_call("Profile", type=ANY, show_choices=False)
-        prompt_mock.assert_any_call("Region", type=ANY, show_choices=False)
+        prompt_mock.assert_any_call("Region [us-west-2]", type=ANY, show_choices=False)
 
     @patch("samcli.lib.schemas.schemas_aws_config.Session")
     @patch("click.confirm")
@@ -51,18 +52,18 @@ class TestInitAWSConfiguration(TestCase):
         msg = "No configured AWS profile found."
         self.assertEqual(str(ctx.exception), msg)
 
-    @patch("samcli.lib.schemas.schemas_aws_config.Session")
-    @patch("click.confirm")
-    @patch("click.prompt")
-    def test_get_aws_configuration_raises_exception_when_schema_service_not_available_in_region(
-        self, prompt_mock, confirm_mock, session_mock
-    ):
-        confirm_mock.side_effect = [True]
-        prompt_mock.side_effect = ["2"]
-        session_mock.return_value.profile_name = "default"
-        session_mock.return_value.region_name = "ap-south-1"
-        session_mock.return_value.available_profiles = ["default", "test-profile"]
-        with self.assertRaises(NotAvailableInRegion) as ctx:
-            get_aws_configuration_choice()
-        msg = "EventBridge Schemas are not yet available in ap-south-1. Please select one of ['us-east-1', 'us-east-2', 'us-west-2', 'eu-west-1', 'ap-northeast-1']"
-        self.assertEqual(str(ctx.exception), msg)
+    # @patch("samcli.lib.schemas.schemas_aws_config.Session")
+    # @patch("click.confirm")
+    # @patch("click.prompt")
+    # def test_get_aws_configuration_raises_exception_when_schema_service_not_available_in_region(
+    #     self, prompt_mock, confirm_mock, session_mock
+    # ):
+    #     confirm_mock.side_effect = [True]
+    #     prompt_mock.side_effect = ["2"]
+    #     session_mock.return_value.profile_name = "default"
+    #     session_mock.return_value.region_name = "ap-south-1"
+    #     session_mock.return_value.available_profiles = ["default", "test-profile"]
+    #     with self.assertRaises(NotAvailableInRegion) as ctx:
+    #         get_aws_configuration_choice()
+    #     msg = "EventBridge Schemas are not yet available in ap-south-1. Please select one of ['us-east-1', 'us-east-2', 'us-west-2', 'eu-west-1', 'ap-northeast-1']"
+    #     self.assertEqual(str(ctx.exception), msg)
