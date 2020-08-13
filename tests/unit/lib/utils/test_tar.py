c986cae180b9073818a47f155e9b6978f6a1dc41
Wilton Wang <CoshUS@users.noreply.github.com>
2020-08-13 10:14:32 -0700

    Removed Unused Import

--- tests/unit/lib/utils/test_tar.py
+++ tests/unit/lib/utils/test_tar.py
@@ -2,7 +2,6 @@ from unittest import TestCase
 from unittest.mock import Mock, patch, call
 
 from samcli.lib.utils.tar import create_tarball
-from tests.testing_utils import IS_WINDOWS
 
 
 class TestTar(TestCase):
