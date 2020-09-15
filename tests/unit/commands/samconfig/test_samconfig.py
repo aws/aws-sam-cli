2d601fe57aa8b1f86e4ae6e3c64d66d8e7e23e1e
Raymond Wang <14915548+wchengru@users.noreply.github.com>
2020-09-15 16:22:41 -0700

    add test cases for the new samconfig alias --template

--- tests/unit/commands/samconfig/test_samconfig.py
+++ tests/unit/commands/samconfig/test_samconfig.py
@@ -715,6 +715,27 @@ class TestSamConfigWithOverrides(TestCase):
                 {"A": "123", "C": "D", "E": "F12!", "G": "H"},
             )
 
+    @patch("samcli.commands.validate.validate.do_cli")
+    def test_secondary_option_name_template_validate(self, do_cli_mock):
+        # "--tempalte" is an alias of "--template-file"
+        config_values = {"template": "mytemplate.yaml"}
+
+        with samconfig_parameters(["validate"], self.scratch_dir, **config_values) as config_path:
+
+            from samcli.commands.validate.validate import cli
+
+            LOG.debug(Path(config_path).read_text())
+            runner = CliRunner()
+            result = runner.invoke(cli, [])
+
+            LOG.info(result.output)
+            LOG.info(result.exception)
+            if result.exception:
+                LOG.exception("Command failed", exc_info=result.exc_info)
+            self.assertIsNone(result.exception)
+
+            do_cli_mock.assert_called_with(ANY, str(Path(os.getcwd(), "mytemplate.yaml")))
+
 
 @contextmanager
 def samconfig_parameters(cmd_names, config_dir=None, env=None, **kwargs):
