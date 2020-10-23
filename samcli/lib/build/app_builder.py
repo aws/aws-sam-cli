eefc5525385b44becfb39d5367317556bb1507bf
Mingkun He <hmingku@amazon.com>
2020-10-23 14:33:52 -0700

    small refactoring

--- samcli/lib/build/app_builder.py
+++ samcli/lib/build/app_builder.py
@@ -129,15 +129,7 @@ class ApplicationBuilder:
         result = self._build_functions(build_graph)
         result.update(self._build_layers(build_graph))
 
-        # clean the redundant cached folder
-        if self._cached:
-            build_graph.clean_redundant_definitions_and_update(not self._is_building_specific_resource)
-            uuids = {bd.uuid for bd in build_graph.get_function_build_definitions()}
-            uuids.update({ld.uuid for ld in build_graph.get_layer_build_definitions()})
-            for cache_dir in pathlib.Path(self._cache_dir).iterdir():
-                if cache_dir.name not in uuids:
-                    shutil.rmtree(pathlib.Path(self._cache_dir, cache_dir.name))
-
+        self._clean_redundant_cached(build_graph)
         return result
 
     def _get_build_graph(self):
@@ -159,6 +151,18 @@ class ApplicationBuilder:
 
         build_graph.clean_redundant_definitions_and_update(not self._is_building_specific_resource)
         return build_graph
+
+    def _clean_redundant_cached(self, build_graph):
+        """
+        clean the redundant cached folder
+        """
+        if self._cached:
+            build_graph.clean_redundant_definitions_and_update(not self._is_building_specific_resource)
+            uuids = {bd.uuid for bd in build_graph.get_function_build_definitions()}
+            uuids.update({ld.uuid for ld in build_graph.get_layer_build_definitions()})
+            for cache_dir in pathlib.Path(self._cache_dir).iterdir():
+                if cache_dir.name not in uuids:
+                    shutil.rmtree(pathlib.Path(self._cache_dir, cache_dir.name))
 
     def _build_functions(self, build_graph):
         """
