197cb38f333eba29e6aed8345f9cd1a107dd4ab3
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2020-10-26 15:30:57 -0700

    feat: build strategy

---
+++ tests/unit/lib/build_module/test_build_strategy.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from unittest.mock import Mock, patch
+
+from samcli.lib.build.build_graph import BuildGraph, FunctionBuildDefinition, LayerBuildDefinition
+from samcli.lib.build.build_strategy import BuildStrategy
+
+
+class BuildStrategyTest(TestCase):
+    pass
+
+
+class DefaultBuildStrategyTest(TestCase):
+    pass
+
+
+class CachedBuildStrategyTest(TestCase):
+    pass
