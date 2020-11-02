f7c69a9480f937238691a468310bbabed0401ffe
Raymond Wang <14915548+wchengru@users.noreply.github.com>
2020-11-02 18:27:37 -0500

    pyInstaller changes to dev branch (#2348)
    * pyInstall configuration, and install script
    
    * update pyinstaller hooks
    
    * fix a bug in package version
    
    * add SPEC file
    
    * limit pyinstaller to 3.5
    
    * add pkg_resources.py2_warn lost in pyInstaller 3.5
    
    * address comments
    
    * remove pyinstaller from base and dev requirements
    
    * Add setuptool dependent for hidden import pkg_resources.py2_warn
    
    * add libs required by staticx
    
    * Remove static requirement, update setuptools version
    
    * remove unnecessary packages

---
+++ installer/pyinstaller/hook-samcli.py
@@ -0,0 +1,23 @@
+from PyInstaller.utils import hooks
+
+hiddenimports = [
+    "cookiecutter.extensions",
+    "jinja2_time",
+    "samtranslator",
+    "samcli.commands.init",
+    "samcli.commands.validate.validate",
+    "samcli.commands.build",
+    "samcli.commands.local.local",
+    "samcli.commands.package",
+    "samcli.commands.deploy",
+    "samcli.commands.logs",
+    "samcli.commands.publish",
+    # default hidden import 'pkg_resources.py2_warn' is added
+    # since pyInstaller 4.0.
+    "pkg_resources.py2_warn",
+    "aws_lambda_builders.workflows",
+]
+datas = (
+    hooks.collect_data_files("samcli")
+    + hooks.collect_data_files("samtranslator")
+)
