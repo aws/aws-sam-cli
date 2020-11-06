cea988bba51b7e57ccb10de9140eac5b3c1c4f2d
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2020-11-06 15:14:57 -0800

    Revert "Revert cached parallel builds (#2351)"
    This reverts commit 74c2ba4ed3f1078f9290f9e61a1773dd385e0eca.

--- tests/integration/buildcmd/build_integ_base.py
+++ tests/integration/buildcmd/build_integ_base.py
@@ -65,6 +65,9 @@ class BuildIntegBase(TestCase):
         mode=None,
         function_identifier=None,
         debug=False,
+        cached=False,
+        cache_dir=None,
+        parallel=False,
     ):
 
         command_list = [self.cmd, "build"]
@@ -91,6 +94,15 @@ class BuildIntegBase(TestCase):
 
         if debug:
             command_list += ["--debug"]
+
+        if cached:
+            command_list += ["--cached"]
+
+        if cache_dir:
+            command_list += ["-cd", cache_dir]
+
+        if parallel:
+            command_list += ["--parallel"]
 
         return command_list
 
@@ -242,3 +254,8 @@ class DedupBuildIntegBase(BuildIntegBase):
             f"Building codeuri: .* runtime: .* metadata: .* functions: "
             f"\\['HelloWorldFunction', 'HelloMarsFunction'\\]",
         )
+
+
+class CachedBuildIntegBase(DedupBuildIntegBase):
+    def _verify_cached_artifact(self, cache_dir):
+        self.assertTrue(cache_dir.exists(), "Cache directory should be created")
