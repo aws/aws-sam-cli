79d1770a33c065757cef167d9061a6b9c48bfa23
_sam <3804518+aahung@users.noreply.github.com>
2020-11-13 16:38:36 -0800

    chore: Enable some pylint rules and add clarity to pylintrc file (#2375)
    * chore: Make pylintrc ignore rule more readable
    
    * chore: Remove pylint rules that do not apply for python 3
    
    * chore: Enable pylint rule Unused import
    
    * chore: Enable pylint multi-processes
    
    * chore: Enable pylint line too long
    
    * chore: Remove unnecessary pylint disable
    
    * chore: Enable pylint Else If Used checker
    
    * chore: Enable pylint compare to zero/empty string

--- samcli/commands/local/lib/local_lambda.py
+++ samcli/commands/local/lib/local_lambda.py
@@ -109,7 +109,8 @@ class LocalLambdaRunner:
             # pylint: disable=no-member
             if hasattr(os_error, "winerror") and os_error.winerror == 1314:
                 raise NoPrivilegeException(
-                    "Administrator, Windows Developer Mode, or SeCreateSymbolicLinkPrivilege is required to create symbolic link for files: {}, {}".format(
+                    "Administrator, Windows Developer Mode, "
+                    "or SeCreateSymbolicLinkPrivilege is required to create symbolic link for files: {}, {}".format(
                         os_error.filename, os_error.filename2
                     )
                 ) from os_error
