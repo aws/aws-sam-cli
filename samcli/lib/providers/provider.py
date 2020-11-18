9d29984c652ef8d0466e023cd5ee79503aa4a9e9
Sam Liu <xinhol@amazon.com>
2020-11-17 17:28:34 -0800

    chore: Enable basic mypy typechecking

--- samcli/lib/providers/provider.py
+++ samcli/lib/providers/provider.py
@@ -194,6 +194,10 @@ class LayerVersion:
     def codeuri(self):
         return self._codeuri
 
+    @codeuri.setter
+    def codeuri(self, codeuri):
+        self._codeuri = codeuri
+
     @property
     def version(self):
         return self._version
@@ -202,10 +206,6 @@ class LayerVersion:
     def layer_arn(self):
         layer_arn, _ = self.arn.rsplit(":", 1)
         return layer_arn
-
-    @codeuri.setter
-    def codeuri(self, codeuri):
-        self._codeuri = codeuri
 
     @property
     def build_method(self):
@@ -251,7 +251,7 @@ class Api:
 
 _CorsTuple = namedtuple("Cors", ["allow_origin", "allow_methods", "allow_headers", "max_age"])
 
-_CorsTuple.__new__.__defaults__ = (
+_CorsTuple.__new__.__defaults__ = (  # type: ignore
     None,  # Allow Origin defaults to None
     None,  # Allow Methods is optional and defaults to empty
     None,  # Allow Headers is optional and defaults to empty
