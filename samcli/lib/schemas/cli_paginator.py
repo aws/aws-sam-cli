a4a71e0b5191b3c5ee714ff1a53870c387c47e21
Sam Liu <xinhol@amazon.com>
2020-11-19 16:38:53 -0800

    Revert "chore: Enable pylint Else If Used checker"
    This reverts commit 5819dedf38800522e587987345866c8ceb4d3aca.

--- samcli/lib/schemas/cli_paginator.py
+++ samcli/lib/schemas/cli_paginator.py
@@ -29,18 +29,19 @@ def do_paginate_cli(pages, page_to_be_rendered, items_per_page, is_last_page, cl
     # Single page is available no pagination
     if len(pages) == 1 and is_last_page:
         message = str.format(cli_display_message["single_page"])
-    elif page_to_be_rendered == 0:
-        # (multi) First page
-        choices = choices + ["N", "n"]
-        message = cli_display_message["first_page"]
-    elif is_last_page and page_to_be_rendered == len(pages) - 1:
-        # (multi) Last page
-        choices = choices + ["P", "p"]
-        message = cli_display_message["last_page"]
     else:
-        # (multi) Middle page
-        choices = choices + ["N", "n", "P", "p"]
-        message = cli_display_message["middle_page"]
+        # First page
+        if page_to_be_rendered == 0:
+            choices = choices + ["N", "n"]
+            message = cli_display_message["first_page"]
+        # Last page
+        elif is_last_page and page_to_be_rendered == len(pages) - 1:
+            choices = choices + ["P", "p"]
+            message = cli_display_message["last_page"]
+        else:
+            # Middle page
+            choices = choices + ["N", "n", "P", "p"]
+            message = cli_display_message["middle_page"]
 
     final_choices = list(map(str, choices))
     choice = click.prompt(message, type=click.Choice(final_choices), show_choices=False)
