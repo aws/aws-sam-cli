af565e0703c4b0505d1b2f412bd3cfed85546fb3
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2020-12-16 11:05:17 -0800

    fix: use container for the builds since some of the runtimes are not present in the Canary environment (#2480)
    * fix: use container for the builds since some of the runtimes are not present in the Canary environment
    
    * chore: bump version
    
    * Revert "chore: bump version"
    
    This reverts commit 99518927
    
    * fix: don't run build on windows
    
    * Fix the warm containers integration test cases
    
    * black reformat
    
    * ci: check appveyor failures
    
    * Revert "ci: check appveyor failures"
    
    This reverts commit 5c0e57b8
    
    * ci: edit windows condition
    
    * ci: edit windows condition
    
    * ci: check windows docker version
    
    * ci: Added Docker Hub Login for Windows (#2484)
    
    * ci: check which command run
    
    * Fix/windows docker login Env (#2485)
    
    * Added Docker Hub Login for Windows
    
    * Changed to Checking env Instead of variable
    
    * ci: fix windows tests
    
    * Revert "ci: fix windows tests"
    
    This reverts commit 0cccd4d8
    
    * fix: run smoke tests in a temporary folder
    
    * fix: remove redundant print statement
    
    * chore: version bump
    
    Co-authored-by: Mohamed Elasmar <melasmar@amazon.com>
    Co-authored-by: Cosh_ <CoshUS@users.noreply.github.com>

--- tests/integration/local/invoke/test_with_credentials.py
+++ tests/integration/local/invoke/test_with_credentials.py
@@ -1,25 +1,28 @@
 from pathlib import Path
-import os
-
 from unittest import skipIf
+
 from parameterized import parameterized
 
 from tests.integration.local.invoke.invoke_integ_base import InvokeIntegBase
-from tests.testing_utils import RUNNING_ON_CI, RUN_BY_CANARY
+from tests.testing_utils import RUNNING_ON_CI, RUN_BY_CANARY, IS_WINDOWS
 
-SKIP_CREDENTIALS_TESTS = RUNNING_ON_CI and not RUN_BY_CANARY
+SKIP_CREDENTIALS_TESTS = IS_WINDOWS or RUNNING_ON_CI or not RUN_BY_CANARY
+
+
+class CredentialsTestBase(InvokeIntegBase):
+    def invoke_functions_and_validate(self, function_name):
+        local_invoke_command_list = self.get_command_list(function_to_invoke=function_name)
+        stdout, _, returncode = self.run_command(local_invoke_command_list)
+        self.assertEqual(returncode, 0)
+        self.assertTrue((b'"statusCode": 200' in stdout) or (b'"statusCode":200' in stdout))
 
 
 @skipIf(SKIP_CREDENTIALS_TESTS, "Run credentials test only in Canary")
-class TestWithCredentials(InvokeIntegBase):
-    template = Path("credential_tests/template.yaml")
+class TestWithCredentials(CredentialsTestBase):
+    template = Path("credential_tests/inprocess/template.yaml")
 
     @parameterized.expand(
         [
-            "JavaStsExample",
-            "PythonStsExample",
-            "RubyStsExample",
-            "NodeStsExample",
             "DotnetStsExample",
             "GoStsExample",
         ]
@@ -34,8 +37,31 @@ class TestWithCredentials(InvokeIntegBase):
         stdout, _, returncode = self.run_command(build_command_list)
         self.assertEqual(returncode, 0)
 
-        # then invoke using temp credentials
-        local_invoke_command_list = self.get_command_list(function_to_invoke=function_name)
-        stdout, _, returncode = self.run_command(local_invoke_command_list)
+        self.invoke_functions_and_validate(function_name)
+
+
+@skipIf(SKIP_CREDENTIALS_TESTS, "Run credentials test only in Canary")
+class TestWithCredentialsBuildUsingContainer(CredentialsTestBase):
+    template = Path("credential_tests/incontainer/template.yaml")
+
+    @parameterized.expand(
+        [
+            "JavaStsExample",
+            "PythonStsExample",
+            "RubyStsExample",
+            "NodeStsExample",
+        ]
+    )
+    def test_build_and_invoke_functions(self, function_name):
+        """
+        This method will first build functions (which contains a credentials call)
+        Then invoke each of them with passing AWS session env variables
+        """
+        # first build application
+        build_command_list = self.get_build_command_list(
+            template_path=self.template_path, cached=True, use_container=True
+        )
+        stdout, _, returncode = self.run_command(build_command_list)
         self.assertEqual(returncode, 0)
-        self.assertTrue(b'"statusCode":200' in stdout)
+
+        self.invoke_functions_and_validate(function_name)
