7c8e2d7432df3364aa9e8284b899e3a8f7db8be3
Wilton Wang <CoshUS@users.noreply.github.com>
2020-12-23 13:23:29 -0800

    Added Python Library ZIP

--- installer/pyinstaller/build-linux.sh
+++ installer/pyinstaller/build-linux.sh
@@ -1,28 +1,32 @@
-pyinstaller_zip_filename=$1
-pyinstaller_package_foldername=$2
-python_package_url=$3
+binary_zip_filename=$1
+python_library_zip_filename=$2
+python_source_url=$3
 
-if [ "$python_package_url" == "" ]; then
-    python_package_url="https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz";
+if [ "$python_library_zip_filename" == "" ]; then
+    python_library_zip_filename="python-libraries.zip";
 fi
 
-echo $python_package_url
+if [ "$python_source_url" == "" ]; then
+    python_source_url="https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz";
+fi
+
+echo $python_source_url
 
 yum install -y zlib-devel openssl-devel
 
 echo "Making Folders"
 mkdir -p .build/src
-mkdir -p .build/output/src
+mkdir -p .build/output/aws-sam-cli-src
 mkdir -p .build/output/python-libraries
 mkdir -p .build/output/pyinstaller-output
 cd .build
 
 echo "Copying Source"
-#cp -r ../[^.]* ./src
-#cp -r ./src/* ./output/src
+cp -r ../[^.]* ./src
+cp -r ./src/* ./output/aws-sam-cli-src
 
 echo "Installing Python"
-curl $python_package_url --output python.tgz
+curl $python_source_url --output python.tgz
 tar -xzf python.tgz
 cd Python*
 ./configure --enable-shared
@@ -35,7 +39,7 @@ python3 -m venv venv
 ./venv/bin/pip install -r src/requirements/reproducible-linux.txt
 
 echo "Copying All Python Libraries"
-#cp -r ./venv/lib/python*/site-packages/* ./output/python-libraries
+cp -r ./venv/lib/python*/site-packages/* ./output/python-libraries
 
 echo "Installing PyInstaller"
 ./venv/bin/pip install -r src/requirements/pyinstaller-build.txt
@@ -53,5 +57,21 @@ chmod 755 pyinstaller-output/install
 echo "Copying Binary"
 cd ..
 cp -r src/pyinstaller-output/* output/pyinstaller-output
-#zip -r $PYINSTALLER_ZIP_FILENAME $pyinstaller_package_foldername
-#mv $PYINSTALLER_ZIP_FILENAME ..
+
+echo "Packaging Binary"
+yum install -y zip
+cd output
+cd pyinstaller-output
+zip -r ../$binary_zip_filename ./*
+cd ..
+zip -r $binary_zip_filename aws-sam-cli-src
+
+echo "Packaging Python Libraries"
+cd python-libraries
+rm -rf *.dist-info
+rm -rf *.egg-info
+rm -rf __pycache__
+rm -rf *.so
+zip -r ../$python_library_zip_filename ./*
+cd ..
+zip -r $python_library_zip_filename aws-sam-cli-src
