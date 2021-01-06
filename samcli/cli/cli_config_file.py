a05434f55f05a30dc6386959f2a4f377b683fa80
Sam Liu <xinhol@amazon.com>
2021-01-06 13:41:40 -0800

    Fix md typo

--- samcli/cli/cli_config_file.py
+++ samcli/cli/cli_config_file.py
@@ -3,7 +3,7 @@ CLI configuration decorator to use TOML configuration files for click commands.
 """
 
 # This section contains code copied and modified from
-# [click_config_file][https://github.com/phha/click_config_file/blob/master/click_config_file.py]
+# [click_config_file](https://github.com/phha/click_config_file/blob/master/click_config_file.py)
 # SPDX-License-Identifier: MIT
 
 import os
@@ -264,4 +264,5 @@ def decorator_customize_config_env(f):
     return click.option(*config_env_param_decls, **config_env_attrs)(f)
 
 
-# End section copied from [click_config_file][https://github.com/phha/click_config_file/blob/master/click_config_file.py
+# End section copied from
+# [click_config_file](https://github.com/phha/click_config_file/blob/master/click_config_file.py)
