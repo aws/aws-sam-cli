cafc76ecb79968c97e95e1c7833143e3d9080f55
Sam Liu <xinhol@amazon.com>
2021-02-10 12:30:43 -0800

    fix: Custom region is not passed properly in local invoke

--- samcli/commands/local/cli_common/invoke_context.py
+++ samcli/commands/local/cli_common/invoke_context.py
@@ -132,7 +132,12 @@ class InvokeContext:
         self._debug_args = debug_args
         self._debugger_path = debugger_path
         self._container_env_vars_file = container_env_vars_file
+
         self._parameter_overrides = parameter_overrides or {}
+        # Override certain CloudFormation pseudo-parameters based on values provided by customer
+        if aws_region:
+            self._parameter_overrides["AWS::Region"] = aws_region
+
         self._layer_cache_basedir = layer_cache_basedir
         self._force_image_build = force_image_build
         self._aws_region = aws_region
@@ -372,10 +377,6 @@ class InvokeContext:
 
     @property
     def parameter_overrides(self):
-        # Override certain CloudFormation pseudo-parameters based on values provided by customer
-        if self._aws_region:
-            self._parameter_overrides["AWS::Region"] = self._aws_region
-
         return self._parameter_overrides
 
     @property
