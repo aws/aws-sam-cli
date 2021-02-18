f6204af4b6d1f23b48431648fe0e5b092be1b734
Wilton Wang <CoshUS@users.noreply.github.com>
2021-02-18 11:18:33 -0800

    Formatted with Black

--- samcli/lib/bootstrap/companion_stack/companion_stack_manager.py
+++ samcli/lib/bootstrap/companion_stack/companion_stack_manager.py
@@ -22,6 +22,7 @@ class CompanionStackManager:
     _s3_prefix: str
     _cfn_client: CloudFormationClient
     _s3_client: S3Client
+
     def __init__(self, stack_name, function_logical_ids, region, s3_bucket, s3_prefix):
         self._companion_stack = CompanionStack(stack_name)
         self._builder = CompanionStackBuilder(self._companion_stack)
@@ -63,32 +64,37 @@ class CompanionStackManager:
         template_url = s3_uploader.to_path_style_s3_url(parts["Key"], parts.get("Version", None))
         waiter_config = {"Delay": 30, "MaxAttempts": 120}
         if self.does_companion_stack_exist():
-            self._cfn_client.update_stack(StackName=stack_name, TemplateURL=template_url, Capabilities=["CAPABILITY_AUTO_EXPAND"])
-            waiter = self._cfn_client.get_waiter('stack_update_complete')
+            self._cfn_client.update_stack(
+                StackName=stack_name, TemplateURL=template_url, Capabilities=["CAPABILITY_AUTO_EXPAND"]
+            )
+            waiter = self._cfn_client.get_waiter("stack_update_complete")
         else:
-            self._cfn_client.create_stack(StackName=stack_name, TemplateURL=template_url, Capabilities=["CAPABILITY_AUTO_EXPAND"])
-            waiter = self._cfn_client.get_waiter('stack_create_complete')
+            self._cfn_client.create_stack(
+                StackName=stack_name, TemplateURL=template_url, Capabilities=["CAPABILITY_AUTO_EXPAND"]
+            )
+            waiter = self._cfn_client.get_waiter("stack_create_complete")
 
         waiter.wait(StackName=stack_name, WaiterConfig=waiter_config)
-
 
     def list_deployed_repos(self) -> List[ECRRepo]:
         """
         Not using create_change_set as it is slow
         """
-        repos:List[ECRRepo] = list()
+        repos: List[ECRRepo] = list()
         stack = boto3.resource("cloudformation", config=self._boto_config).Stack(self._companion_stack.stack_name)
         resources = stack.resource_summaries.all()
         for resource in resources:
             if resource.resource_type == "AWS::ECR::Repository":
-                repos.append(ECRRepo(logical_id=resource.logical_resource_id,physical_id=resource.physical_resource_id))
+                repos.append(
+                    ECRRepo(logical_id=resource.logical_resource_id, physical_id=resource.physical_resource_id)
+                )
         return repos
 
     def get_unreferenced_repos(self) -> List[ECRRepo]:
-        deployed_repos:List[ECRRepo] = self.list_deployed_repos()
+        deployed_repos: List[ECRRepo] = self.list_deployed_repos()
         current_mapping = self._builder.repo_mapping
 
-        unreferenced_repos:List[ECRRepo] = list()
+        unreferenced_repos: List[ECRRepo] = list()
         for deployed_repo in deployed_repos:
             found = False
             for _, current_repo in current_mapping.items():
@@ -99,7 +105,6 @@ class CompanionStackManager:
                 unreferenced_repos.append(deployed_repo)
         return unreferenced_repos
 
-
     def does_companion_stack_exist(self):
         try:
             self._cfn_client.describe_stacks(StackName=self._companion_stack.stack_name)
@@ -108,6 +113,12 @@ class CompanionStackManager:
             return False
 
 
-manager = CompanionStackManager("Auto-ECR-Test-Stack", ["TestFunction01", "AnotherTestFunction03"], "us-west-2", "aws-sam-cli-managed-default-samclisourcebucket-9bu3m109ig6i", "Hello-World-Stack")
+manager = CompanionStackManager(
+    "Auto-ECR-Test-Stack",
+    ["TestFunction01", "AnotherTestFunction03"],
+    "us-west-2",
+    "aws-sam-cli-managed-default-samclisourcebucket-9bu3m109ig6i",
+    "Hello-World-Stack",
+)
 print(manager.get_unreferenced_repos())
 print(manager.update_companion_stack())
