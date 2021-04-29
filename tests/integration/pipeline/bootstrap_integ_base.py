e095ad28f575a6627c06bd4053c87163f2588a6c
Sam Liu <xinhol@amazon.com>
2021-04-29 16:50:39 -0700

    Add integ test for pipeline bootstrap

---
+++ tests/integration/pipeline/bootstrap_integ_base.py
@@ -0,0 +1,76 @@
+import os
+import re
+import shutil
+from typing import List
+from unittest import TestCase
+
+import boto3
+
+
+class BootstrapIntegBase(TestCase):
+    stack_names: List[str]
+
+    @classmethod
+    def setUpClass(cls):
+        cls.cf_client = boto3.client("cloudformation")
+        pass
+
+    def setUp(self):
+        self.stack_names = []
+        super().setUp()
+
+    def tearDown(self):
+        if self.stack_names:
+            for stack_name in self.stack_names:
+                self.cf_client.delete_stack(StackName=stack_name)
+        shutil.rmtree(os.path.join(os.getcwd(), ".aws-sam", "pipeline"), ignore_errors=True)
+        super().tearDown()
+
+    def base_command(self):
+        command = "sam"
+        if os.getenv("SAM_CLI_DEV"):
+            command = "samdev"
+
+        return command
+
+    def get_bootstrap_command_list(
+        self,
+        # right now we only support interactive mode
+        interactive=True,
+    ):
+        command_list = [self.base_command(), "pipeline", "bootstrap"]
+
+        if interactive:
+            command_list = command_list + ["--interactive"]
+        return command_list
+
+    @staticmethod
+    def _method_to_stage_name(method_name: str) -> str:
+        """
+        Method expects method name which can be a full path. Eg: test.integration.test_bootstrap_command.method_name
+        """
+        method_name = method_name.split(".")[-1]
+        return method_name.replace("_", "-")
+
+    @staticmethod
+    def _extract_created_resources(stdout: str) -> List[str]:
+        created_start = r"We have created the following resources.+"
+        provided_start = r"You provided the following resources.+"
+        configure_start = r"Please configure your.+"
+        tokens = re.split(created_start, stdout)
+        if len(tokens) == 1:
+            # no resources created
+            return []
+
+        created_resources_section = tokens[1]
+        # after created resource section, it might be provided resource section,
+        # or configure credential section.
+        # we use two split to find where it ends.
+        created_resources_section = re.split(configure_start, created_resources_section)[0]
+        created_resources_section = re.split(provided_start, created_resources_section)[0]
+        # clean up and return the lines
+        return [line.strip() for line in created_resources_section.split("\n") if line.strip()]
+
+    @staticmethod
+    def _count_created_resources(stdout: str) -> int:
+        return len(BootstrapIntegBase._extract_created_resources(stdout))
---
+++ tests/integration/pipeline/test_bootstrap_command.py
@@ -0,0 +1,156 @@
+from parameterized import parameterized
+
+from samcli.lib.pipeline.bootstrap.stage import Stage
+from tests.integration.pipeline.bootstrap_integ_base import BootstrapIntegBase
+from tests.testing_utils import run_command_with_input
+
+
+class TestBootstrap(BootstrapIntegBase):
+    @parameterized.expand([("create_ecr_repo",), (False,)])
+    def test_interactive_with_no_resources_provided(self, create_ecr_repo: bool):
+        stage_name = self._method_to_stage_name(self.id())
+        self.stack_names = [Stage._get_stack_name(stage_name)]
+
+        bootstrap_command_list = self.get_bootstrap_command_list(interactive=True)
+
+        inputs = [
+            stage_name,
+            "",  # pipeline user
+            "",  # Pipeline execution role
+            "",  # CloudFormation execution role
+            "",  # Artifacts bucket
+            "2" if create_ecr_repo else "1",  # Should we create ECR repo, 1 - No, 2 - Yes
+            "",  # Pipeline IP address range
+            "y",  # proceed
+        ]
+
+        bootstrap_process_execute = run_command_with_input(bootstrap_command_list, ("\n".join(inputs) + "\n").encode())
+
+        self.assertEqual(bootstrap_process_execute.process.returncode, 0)
+        stdout = bootstrap_process_execute.stdout.decode()
+        self.assertIn("We have created the following resources", stdout)
+        # make sure pipeline user's credential is printed
+        self.assertIn("ACCESS_KEY_ID", stdout)
+        self.assertIn("SECRET_ACCESS_KEY", stdout)
+        if create_ecr_repo:
+            self.assertIn("arn:aws:ecr:", stdout)
+            self.assertEqual(5, self._count_created_resources(stdout))
+        else:
+            self.assertNotIn("arn:aws:ecr:", stdout)
+            self.assertEqual(4, self._count_created_resources(stdout))
+
+    def test_interactive_with_all_required_resources_provided(self):
+        stage_name = self._method_to_stage_name(self.id())
+        self.stack_names = [Stage._get_stack_name(stage_name)]
+
+        bootstrap_command_list = self.get_bootstrap_command_list(interactive=True)
+
+        inputs = [
+            stage_name,
+            "arn:aws:iam::123:user/user-name",  # pipeline user
+            "arn:aws:iam::123:role/role-name",  # Pipeline execution role
+            "arn:aws:iam::123:role/role-name",  # CloudFormation execution role
+            "arn:aws:s3:::bucket-name",  # Artifacts bucket
+            "3",  # Should we create ECR repo, 3 - specify one
+            "arn:aws:ecr:::repository/repo-name",  # ecr repo
+            "1.2.3.4/24",  # Pipeline IP address range
+            "y",  # proceed
+        ]
+
+        bootstrap_process_execute = run_command_with_input(bootstrap_command_list, ("\n".join(inputs) + "\n").encode())
+
+        self.assertEqual(bootstrap_process_execute.process.returncode, 0)
+        stdout = bootstrap_process_execute.stdout.decode()
+        self.assertIn("skipping creation", stdout)
+
+    def test_interactive_cancelled_by_user(self):
+        stage_name = self._method_to_stage_name(self.id())
+        self.stack_names = [Stage._get_stack_name(stage_name)]
+
+        bootstrap_command_list = self.get_bootstrap_command_list(interactive=True)
+
+        inputs = [
+            stage_name,
+            "arn:aws:iam::123:user/user-name",  # pipeline user
+            "",  # Pipeline execution role
+            "",  # CloudFormation execution role
+            "",  # Artifacts bucket
+            "1",  # Should we create ECR repo, 1 - No
+            "",  # Pipeline IP address range
+            "N",  # cancel
+        ]
+
+        bootstrap_process_execute = run_command_with_input(bootstrap_command_list, ("\n".join(inputs) + "\n").encode())
+
+        self.assertEqual(bootstrap_process_execute.process.returncode, 0)
+        stdout = bootstrap_process_execute.stdout.decode()
+        self.assertTrue(stdout.strip().endswith("Should we proceed with the creation? [y/N]:"))
+
+    def test_interactive_with_some_required_resources_provided(self):
+        stage_name = self._method_to_stage_name(self.id())
+        self.stack_names = [Stage._get_stack_name(stage_name)]
+
+        bootstrap_command_list = self.get_bootstrap_command_list(interactive=True)
+
+        inputs = [
+            stage_name,
+            "arn:aws:iam::123:user/user-name",  # pipeline user
+            "arn:aws:iam::123:role/role-name",  # Pipeline execution role
+            "",  # CloudFormation execution role
+            "arn:aws:s3:::bucket-name",  # Artifacts bucket
+            "3",  # Should we create ECR repo, 3 - specify one
+            "arn:aws:ecr:::repository/repo-name",  # ecr repo
+            "1.2.3.4/24",  # Pipeline IP address range
+            "y",  # proceed
+        ]
+
+        bootstrap_process_execute = run_command_with_input(bootstrap_command_list, ("\n".join(inputs) + "\n").encode())
+
+        self.assertEqual(bootstrap_process_execute.process.returncode, 0)
+        stdout = bootstrap_process_execute.stdout.decode()
+        self.assertIn("Successfully created!", stdout)
+        # make sure the not provided resource is the only resource created.
+        self.assertIn("aws-sam-cli-managed-test-CloudFormationExecution", stdout)
+        self.assertEqual(1, self._count_created_resources(stdout))
+
+    def test_interactive_pipeline_user_only_created_once(self):
+        """
+        Create 3 stages, only the first stage resource stack creates
+        a pipeline user, and the remaining two share the same pipeline user.
+        """
+        stage_names = [
+            self._method_to_stage_name(self.id() + "1"),
+            self._method_to_stage_name(self.id() + "2"),
+            self._method_to_stage_name(self.id() + "3"),
+        ]
+        self.stack_names = [Stage._get_stack_name(stage_name) for stage_name in stage_names]
+
+        bootstrap_command_list = self.get_bootstrap_command_list(interactive=True)
+
+        for i, stage_name in enumerate(stage_names):
+            inputs = [
+                stage_name,
+                *([""] if i == 0 else []),  # pipeline user
+                "arn:aws:iam::123:role/role-name",  # Pipeline execution role
+                "arn:aws:iam::123:role/role-name",  # CloudFormation execution role
+                "arn:aws:s3:::bucket-name",  # Artifacts bucket
+                "1",  # Should we create ECR repo, 1 - No, 2 - Yes
+                "",  # Pipeline IP address range
+                "y",  # proceed
+            ]
+
+            bootstrap_process_execute = run_command_with_input(
+                bootstrap_command_list, ("\n".join(inputs) + "\n").encode()
+            )
+
+            self.assertEqual(bootstrap_process_execute.process.returncode, 0)
+            stdout = bootstrap_process_execute.stdout.decode()
+
+            # only first stage creates pipeline user
+            if i == 0:
+                self.assertIn("We have created the following resources", stdout)
+                self.assertIn("PipelineUser", str(self._extract_created_resources(stdout)))
+                self.assertEqual(1, self._count_created_resources(stdout))
+            else:
+                self.assertNotIn("PipelineUser", str(self._extract_created_resources(stdout)))
+                self.assertEqual(0, self._count_created_resources(stdout))
