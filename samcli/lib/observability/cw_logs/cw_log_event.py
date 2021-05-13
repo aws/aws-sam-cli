e1afb465ead5a91ca09b28f292c5098666ab89a0
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2021-05-13 16:19:06 -0700

    refactor: refactor logs command library (#2862)
    * refactor logs command library
    
    * re-organize due to click usage
    
    * address comments
    
    * adding pylint disable for console consumer
    
    * make pylint happy with python 3.6
    
    Co-authored-by: Mathieu Grandis <73313235+mgrandis@users.noreply.github.com>

---
+++ samcli/lib/observability/cw_logs/cw_log_event.py
@@ -0,0 +1,40 @@
+"""
+CloudWatch log event type
+"""
+from typing import Optional
+
+from samcli.lib.observability.observability_info_puller import ObservabilityEvent
+
+
+class CWLogEvent(ObservabilityEvent[dict]):
+    """
+    An event class which represents a Cloud Watch log
+    """
+
+    def __init__(self, cw_log_group: str, event: dict, resource_name: Optional[str] = None):
+        """
+        Parameters
+        ----------
+        cw_log_group : str
+            Name of the CloudWatch log group
+        event : dict
+            Event dictionary of the CloudWatch log event
+        resource_name : Optional[str]
+            Resource name that is related to this CloudWatch log event
+        """
+        self.cw_log_group = cw_log_group
+        self.message: str = event.get("message", "")
+        self.log_stream_name: str = event.get("logStreamName", "")
+        timestamp: int = event.get("timestamp", 0)
+        super().__init__(event, timestamp, resource_name)
+
+    def __eq__(self, other):
+        if not isinstance(other, CWLogEvent):
+            return False
+
+        return (
+            self.cw_log_group == other.cw_log_group
+            and self.log_stream_name == other.log_stream_name
+            and self.timestamp == other.timestamp
+            and self.message == other.message
+        )
