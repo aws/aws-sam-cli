df9593cac0b7f358a0f1e543aca2f10ecafedfa7
_sam <3804518+aahung@users.noreply.github.com>
2021-05-28 15:09:11 -0700

    feat: Support creating pipeline files directly into . without hooks (#2911)
    * feat: Support creating pipeline files directly into . without hooks

--- tests/unit/commands/pipeline/init/test_initeractive_init_flow.py
+++ tests/unit/commands/pipeline/init/test_initeractive_init_flow.py
@@ -100,18 +100,22 @@ class TestInteractiveInitFlow(TestCase):
             do_interactive()
 
     @patch("samcli.commands.pipeline.init.interactive_init_flow.SamConfig")
+    @patch("samcli.commands.pipeline.init.interactive_init_flow.osutils")
     @patch("samcli.lib.cookiecutter.template.cookiecutter")
     @patch("samcli.commands.pipeline.init.interactive_init_flow.InteractiveFlowCreator.create_flow")
     @patch("samcli.commands.pipeline.init.interactive_init_flow.PipelineTemplatesManifest")
     @patch("samcli.commands.pipeline.init.interactive_init_flow.GitRepo.clone")
+    @patch("samcli.commands.pipeline.init.interactive_init_flow._copy_dir_contents_to_cwd_fail_on_exist")
     @patch("samcli.lib.cookiecutter.question.click")
     def test_generate_pipeline_configuration_file_from_app_pipeline_template_happy_case(
         self,
         click_mock,
+        _copy_dir_contents_to_cwd_fail_on_exist_mock,
         clone_mock,
         PipelineTemplatesManifest_mock,
         create_interactive_flow_mock,
         cookiecutter_mock,
+        osutils_mock,
         samconfig_mock,
     ):
         # setup
@@ -131,9 +135,11 @@ class TestInteractiveInitFlow(TestCase):
             templates=[jenkins_template_mock],
         )
         PipelineTemplatesManifest_mock.return_value = pipeline_templates_manifest_mock
+        cookiecutter_output_dir_mock = "/tmp/any/dir2"
+        osutils_mock.mkdir_temp.return_value.__enter__ = Mock(return_value=cookiecutter_output_dir_mock)
         interactive_flow_mock = Mock()
         create_interactive_flow_mock.return_value = interactive_flow_mock
-        cookiecutter_context_mock = Mock()
+        cookiecutter_context_mock = {"key": "value"}
         interactive_flow_mock.run.return_value = cookiecutter_context_mock
         config_file = Mock()
         samconfig_mock.return_value = config_file
@@ -152,6 +158,7 @@ class TestInteractiveInitFlow(TestCase):
         do_interactive()
 
         # verify
+        osutils_mock.mkdir_temp.assert_called()  # cookiecutter project is generated to temp
         expected_cookicutter_template_location = any_app_pipeline_templates_path.joinpath(jenkins_template_location)
         clone_mock.assert_called_once_with(shared_path, APP_PIPELINE_TEMPLATES_REPO_LOCAL_NAME, replace_existing=True)
         PipelineTemplatesManifest_mock.assert_called_once()
@@ -166,9 +173,10 @@ class TestInteractiveInitFlow(TestCase):
         )
         cookiecutter_mock.assert_called_once_with(
             template=str(expected_cookicutter_template_location),
-            output_dir=".",
+            output_dir=cookiecutter_output_dir_mock,
             no_input=True,
             extra_context=cookiecutter_context_mock,
+            overwrite_if_exists=True,
         )
 
     @patch("samcli.commands.pipeline.init.interactive_init_flow._read_app_pipeline_templates_manifest")
@@ -236,10 +244,12 @@ class TestInteractiveInitFlow(TestCase):
     @patch("samcli.commands.pipeline.init.interactive_init_flow.InteractiveFlowCreator.create_flow")
     @patch("samcli.commands.pipeline.init.interactive_init_flow.GitRepo.clone")
     @patch("samcli.commands.pipeline.init.interactive_init_flow.click")
+    @patch("samcli.commands.pipeline.init.interactive_init_flow._copy_dir_contents_to_cwd_fail_on_exist")
     @patch("samcli.lib.cookiecutter.question.click")
     def test_generate_pipeline_configuration_file_from_custom_remote_pipeline_template_happy_case(
         self,
         questions_click_mock,
+        _copy_dir_contents_to_cwd_fail_on_exist_mock,
         init_click_mock,
         clone_mock,
         create_interactive_flow_mock,
@@ -248,14 +258,16 @@ class TestInteractiveInitFlow(TestCase):
     ):
         # setup
         any_temp_dir = "/tmp/any/dir"
-        osutils_mock.mkdir_temp.return_value.__enter__ = Mock(return_value=any_temp_dir)
+        cookiecutter_output_dir_mock = "/tmp/any/dir2"
+        osutils_mock.mkdir_temp.return_value.__enter__ = Mock(side_effect=[any_temp_dir, cookiecutter_output_dir_mock])
         osutils_mock.mkdir_temp.return_value.__exit__ = Mock()
         any_custom_pipeline_templates_path = Path(os.path.join(any_temp_dir, CUSTOM_PIPELINE_TEMPLATE_REPO_LOCAL_NAME))
         clone_mock.return_value = any_custom_pipeline_templates_path
         interactive_flow_mock = Mock()
         create_interactive_flow_mock.return_value = interactive_flow_mock
-        cookiecutter_context_mock = Mock()
+        cookiecutter_context_mock = {"key": "value"}
         interactive_flow_mock.run.return_value = cookiecutter_context_mock
+        _copy_dir_contents_to_cwd_fail_on_exist_mock.return_value = ["file1"]
 
         questions_click_mock.prompt.return_value = "2"  # Custom pipeline templates
         init_click_mock.prompt.return_value = "https://github.com/any-custom-pipeline-template-repo.git"
@@ -264,7 +276,8 @@ class TestInteractiveInitFlow(TestCase):
         do_interactive()
 
         # verify
-        osutils_mock.mkdir_temp.assert_called_once()  # Custom templates are cloned to temp
+        # Custom templates are cloned to temp; cookiecutter project is generated to temp
+        osutils_mock.mkdir_temp.assert_called()
         clone_mock.assert_called_once_with(
             Path(any_temp_dir), CUSTOM_PIPELINE_TEMPLATE_REPO_LOCAL_NAME, replace_existing=True
         )
@@ -274,7 +287,8 @@ class TestInteractiveInitFlow(TestCase):
         interactive_flow_mock.run.assert_called_once()
         cookiecutter_mock.assert_called_once_with(
             template=str(any_custom_pipeline_templates_path),
-            output_dir=".",
+            output_dir=cookiecutter_output_dir_mock,
             no_input=True,
             extra_context=cookiecutter_context_mock,
+            overwrite_if_exists=True,
         )
