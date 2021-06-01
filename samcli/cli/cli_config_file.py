6beca00ac72fbe65a261992375be882ac2f89caa
Mathieu Grandis <73313235+mgrandis@users.noreply.github.com>
2021-06-01 14:03:00 -0700

    Revert "samconfig debug level logging fixed; documentation updated (#2891)" (#2918)
    This reverts commit 2a13a69822660538c478118125eef50d0164995a.

--- samcli/cli/cli_config_file.py
+++ samcli/cli/cli_config_file.py
@@ -57,12 +57,14 @@ class TomlProvider:
 
         samconfig = SamConfig(config_file_dir, config_file_name)
 
-        # bringing samconfig file location up to info level,
-        # to improve UX and make it clear where we're looking for samconfig file
-        if samconfig.exists():
-            click.echo(f"Config file location: {samconfig.path()}")
-        else:
-            click.secho(f"Config file '{samconfig.path()}' does not exist", fg="yellow")
+        # Enable debug level logging by environment variable "SAM_DEBUG"
+        if os.environ.get("SAM_DEBUG", "").lower() == "true":
+            LOG.setLevel(logging.DEBUG)
+
+        LOG.debug("Config file location: %s", samconfig.path())
+
+        if not samconfig.exists():
+            LOG.debug("Config file '%s' does not exist", samconfig.path())
             return resolved_config
 
         try:
@@ -234,10 +236,8 @@ def decorator_customize_config_file(f):
     config_file_param_decls = ("--config-file",)
     config_file_attrs["help"] = (
         "The path and file name of the configuration file containing default parameter values to use. "
-        "Its default value is 'samconfig.toml' in project root directory. Project root directory is defined by the "
-        "template file location. When using config file and specifing --template-file SAM CLI expects samconfig.toml "
-        "and the template file to be in the same directory. Alternatively, if --config-file is explicitly specified, "
-        "it can point to a custom samconfig.toml location. For more information about configuration files, see "
+        "Its default value is 'samconfig.toml' in project directory. For more information about configuration files, "
+        "see: "
         "https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-config.html."
     )
     config_file_attrs["default"] = "samconfig.toml"
