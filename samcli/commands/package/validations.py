2cef205cf30d4e5ce7591b3e159934a2716b40b3
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2021-06-29 15:08:31 -0700

    Cdk support package and deploy fix (#2996)
    * Fix --resolve-s3 --s3-bucket validation under guided flow
    
    * Fix package resource assets

--- samcli/commands/package/validations.py
+++ samcli/commands/package/validations.py
@@ -30,12 +30,13 @@ def package_option_validation(func):
         # NOTE(sriram-mv): Both params and default_map need to be checked, as the option can be either be
         # passed in directly or through configuration file.
         # If passed in through configuration file, default_map is loaded with those values.
+        guided = ctx.params.get("guided", False) or ctx.params.get("g", False)
         resolve_s3_provided = ctx.params.get("resolve_s3", False) or ctx.default_map.get("resolve_s3", False)
         s3_bucket_provided = ctx.params.get("s3_bucket", False) or ctx.default_map.get("s3_bucket", False)
         either_required = stack.has_assets_of_package_type(ZIP) if stack is not None else False
-        if s3_bucket_provided and resolve_s3_provided:
+        if not guided and s3_bucket_provided and resolve_s3_provided:
             raise PackageResolveS3AndS3SetError()
-        if either_required and not s3_bucket_provided and not resolve_s3_provided:
+        if not guided and either_required and not s3_bucket_provided and not resolve_s3_provided:
             raise PackageResolveS3AndS3NotSetError()
 
         return func(*args, **kwargs)
