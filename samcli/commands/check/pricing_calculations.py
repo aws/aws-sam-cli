6a07e314dfb2b77996d511564d0fedd0e8ce25e8
jordon-ogo <jordonogonoski@gmail.com>
2021-07-15 14:34:16 -0600

    sku are now retrieved dynamically
    Updated pricing so that sku from Api file are retrieved dynamically and not hard coded.

--- samcli/commands/check/pricing_calculations.py
+++ samcli/commands/check/pricing_calculations.py
@@ -8,6 +8,8 @@ from urllib.error import HTTPError
 
 import ast
 
+from botocore.session import get_session
+
 
 class PricingCalculations:
     def __init__(self, graph):
@@ -17,59 +19,21 @@ class PricingCalculations:
         self.max_free_GBs = None
         self.monthly_compute_charge = None
         self.monthly_request_charge = None
-        self.max_request_skus = [
-            "ZQVCC2M69R4S79XS",
-            "ZQVCC2M69R4S79XS.A429C66SYZ",
-            "ZQVCC2M69R4S79XS.A429C66SYZ.SB2VSFF4TP",
-        ]
-        self.max_free_GBs_skus = [
-            "4VCGJ7EG8PJVZG7S",
-            "4VCGJ7EG8PJVZG7S.A429C66SYZ",
-            "4VCGJ7EG8PJVZG7S.A429C66SYZ.KJYPJPDD2A",
-        ]
-        self.compute_skus = [
-            "TG3M4CAGBA3NYQBH",
-            "TG3M4CAGBA3NYQBH.JRTCKXETXF",
-            "TG3M4CAGBA3NYQBH.JRTCKXETXF.6YS6EN2CT7",
-        ]
-        self.request_charge_skus = [
-            "GU2ZS9HVP6QTQ7KE",
-            "GU2ZS9HVP6QTQ7KE.JRTCKXETXF",
-            "GU2ZS9HVP6QTQ7KE.JRTCKXETXF.6YS6EN2CT7",
-        ]
+        self.max_request_usage_type = "Global-Request"
+        self.max_free_GBs_usage_type = "Global-Lambda-GB-Second"
+        self.compute_usage_type = "Lambda-GB-Second"
+        self.request_charge_usage_type = "Request"
+        self.region_prefix = ""
 
     def get_lambda_pricing_results(self):
         return self.lambda_pricing_results
 
     def run_calculations(self):
-        aws_lambda_pricing_info = self.get_aws_lambda_pricing_info()
-        self.get_charge_and_request_amounts(aws_lambda_pricing_info)
+        self.get_charge_and_request_amounts()
         self.determine_lambda_cost()
 
-    def get_charge_and_request_amounts(self, aws_lambda_pricing_info):
-        self.monthly_compute_charge = float(
-            aws_lambda_pricing_info["terms"]["OnDemand"][self.compute_skus[0]][self.compute_skus[1]]["priceDimensions"][
-                self.compute_skus[2]
-            ]["pricePerUnit"]["USD"]
-        )
-
-        self.max_free_GBs = float(
-            aws_lambda_pricing_info["terms"]["OnDemand"][self.max_free_GBs_skus[0]][self.max_free_GBs_skus[1]][
-                "priceDimensions"
-            ][self.max_free_GBs_skus[2]]["endRange"]
-        )
-
-        self.monthly_request_charge = float(
-            aws_lambda_pricing_info["terms"]["OnDemand"][self.request_charge_skus[0]][self.request_charge_skus[1]][
-                "priceDimensions"
-            ][self.request_charge_skus[2]]["pricePerUnit"]["USD"]
-        )
-
-        self.max_num_of_free_requests = float(
-            aws_lambda_pricing_info["terms"]["OnDemand"][self.max_request_skus[0]][self.max_request_skus[1]][
-                "priceDimensions"
-            ][self.max_request_skus[2]]["endRange"]
-        )
+    def get_charge_and_request_amounts(self):
+        self.get_lambda_charge_and_request_amounts()
 
     def get_aws_lambda_pricing_info(self):
         try:
@@ -118,3 +82,102 @@ class PricingCalculations:
         monthly_lambda_costs = round(monthly_compute_amount + monthly_request_amount, 2)
 
         self.lambda_pricing_results = monthly_lambda_costs
+
+    def get_lambda_charge_and_request_amounts(self):
+        aws_lambda_price = self.get_aws_lambda_pricing_info()
+        products = aws_lambda_price["products"]
+        terms = aws_lambda_price["terms"]
+
+        region = get_session().get_config_variable("region")
+
+        # default is us-east-1, so no prefix is needed for Bulk Api. Every other region needs a prefix
+        if region != "us-east-1":
+            self.get_region_prefix(region)
+
+        for product in products.values():
+            usage_type = self.region_prefix + product["attributes"]["usagetype"]
+
+            if usage_type == self.region_prefix + "Global-Request":
+                self.get_pricing_or_request_value(product, terms, "global-request")
+
+            elif usage_type == self.region_prefix + "Global-Lambda-GB-Second":
+                self.get_pricing_or_request_value(product, terms, "global-lambda")
+
+            elif usage_type == self.region_prefix + "Lambda-GB-Second":
+                self.get_pricing_or_request_value(product, terms, "lambda")
+
+            elif usage_type == self.region_prefix + "Request":
+                self.get_pricing_or_request_value(product, terms, "request")
+
+    def get_pricing_or_request_value(self, product, terms, get_type):
+        sku = product["sku"]
+
+        temp_key = terms["OnDemand"][sku]
+
+        """
+        The dictionaries have unknown sku numbers. To prevent a massive storage of over
+        400 sku numbers, plus 2 additional unique keys per sku number, a temp_key is used
+        to bypass the need to know the keys. This is just to hold the value of the first
+        key in a given dictionary until the final path is reached. It is only done when
+        there is only one value in a given object.
+        """
+        temp_key = next(iter(temp_key.values()))
+        temp_key = temp_key["priceDimensions"]
+        price_dimentions = next(iter(temp_key.values()))
+
+        if get_type == "global-request":
+            self.max_num_of_free_requests = float(price_dimentions["endRange"])
+        elif get_type == "global-lambda":
+            self.max_free_GBs = float(price_dimentions["endRange"])
+        elif get_type == "lambda":
+            self.monthly_compute_charge = float(price_dimentions["pricePerUnit"]["USD"])
+        elif get_type == "request":
+            self.monthly_request_charge = float(price_dimentions["pricePerUnit"]["USD"])
+
+    def get_region_prefix(self, region):
+        if region == "ap-northeast-1":
+            self.region_prefix = "APN1-"
+        elif region == "ap-northeast-2":
+            self.region_prefix = "APN2-"
+        elif region == "ap-south-1":
+            self.region_prefix = "APS3-"
+        elif region == "ap-southeast-1":
+            self.region_prefix = "APS1-"
+        elif region == "ap-southeast-2":
+            self.region_prefix = "APS2-"
+        elif region == "ca-central-1":
+            self.region_prefix = "CAN1-"
+        elif region == "eu-central-1":
+            self.region_prefix = "EUC1-"
+        elif region == "eu-north-1":
+            self.region_prefix = "EUN1-"
+        elif region == "eu-south-1":
+            self.region_prefix = "EUS1-"
+        elif region == "eu-west-1":
+            self.region_prefix = "EU-"
+        elif region == "eu-west-2":
+            self.region_prefix = "EUW2-"
+        elif region == "eu-west-3":
+            self.region_prefix = "EUW3-"
+        elif region == "me-south-1":
+            self.region_prefix = "MES1-"
+        elif region == "sa-east-1":
+            self.region_prefix = "SAE1-"
+        elif region == "us-east-1":
+            self.region_prefix = "USE1-"
+        elif region == "us-gov-east-1":
+            self.region_prefix = "UGE1-"
+        elif region == "us-gov-west-1":
+            self.region_prefix = "UGW1-"
+        elif region == "us-west-1":
+            self.region_prefix = "USW1-"
+        elif region == "us-west-2":
+            self.region_prefix = "USW2-"
+        elif region == "ap-east-1":
+            self.region_prefix = "APE1-"
+        elif region == "af-south-1":
+            self.region_prefix = "AFS1-"
+        elif region == "us-east-2":
+            self.region_prefix = "USE2-"
+        else:
+            raise Exception("Invalid region. Please use a valid region to calcualte pricing of application")
