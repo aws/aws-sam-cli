f6c8a8daffbd9eb03d536e4ec94a9186df2194aa
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2021-07-25 09:02:44 -0700

    More polishing in CdkPythonEnv

--- tests/cdk_testing_utils.py
+++ tests/cdk_testing_utils.py
@@ -1,12 +1,11 @@
 import os
+import uuid
 import platform
-import sys
 import logging
 import venv
 import shutil
 
 from tests.testing_utils import run_command
-from distutils.dir_util import copy_tree
 
 
 LOG = logging.getLogger(__name__)
@@ -14,18 +13,22 @@ LOG = logging.getLogger(__name__)
 
 class CdkPythonEnv:
     """
-    A self-contained and temporary environment for testing CDK app written in Python
+    Provides an environment (either using the base Python environment or creating a venv )
+    for testing CDK app written in Python
     """
 
     def __init__(self, base_dir: str, use_venv: bool = False):
         self._base_dir = base_dir
-        self._venv_path = os.path.join(self._base_dir, ".venv")
+        self._venv_path = os.path.join(
+            self._base_dir,
+            "venv{}".format(str(uuid.uuid4()).replace("-", "")[:5])
+        )
         self._use_venv = use_venv
         if self._use_venv:
             self._create_virtual_env()
 
-    def _create_virtual_env(self):
-        LOG.info("creating virtual env")
+    def _create_virtual_env(self) -> None:
+        LOG.debug("creating virtual env for testing CDK Python apps")
         try:
             venv.create(
                 self._venv_path,
@@ -34,15 +37,18 @@ class CdkPythonEnv:
                 symlinks=False,
             )
         except shutil.SameFileError:
+            # SameFileError happens when the python binary in the root env and the virtual env are
+            # actually the same file
+            # Bypassing it because it does not affect the use of the created virtual env
             pass
 
-    def install_dependencies(self, requirements_path):
+    def install_dependencies(self, requirements_path: str) -> None:
         if os.path.exists(requirements_path):
             command = [self.pip_executable, "install", "-r", requirements_path]
             run_command(command_list=command)
 
     @property
-    def python_executable(self):
+    def python_executable(self) -> str:
         if not self._use_venv:
             return "python"
         if platform.system().lower() == "windows":
@@ -50,7 +56,7 @@ class CdkPythonEnv:
         return os.path.join(self._venv_path, "bin", "python")
 
     @property
-    def pip_executable(self):
+    def pip_executable(self) -> str:
         if not self._use_venv:
             return "pip"
         if platform.system().lower() == "windows":
