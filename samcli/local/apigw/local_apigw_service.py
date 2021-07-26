9e80b11c4e6d26c87fd50548e0cdf186e28f119a
Marco Stazi <marco.stazi@gmail.com>
2021-07-26 13:27:52 +1000

    made dictionary keys lowercase as in API Gateway

--- samcli/local/apigw/local_apigw_service.py
+++ samcli/local/apigw/local_apigw_service.py
@@ -798,14 +798,14 @@ class LocalApigwService(BaseLocalService):
         # Multi-value request headers is not really supported by Flask.
         # See https://github.com/pallets/flask/issues/850
         for header_key in flask_request.headers.keys():
-            headers_dict[header_key] = flask_request.headers.get(header_key)
-            multi_value_headers_dict[header_key] = flask_request.headers.getlist(header_key)
+            headers_dict[header_key.lower()] = flask_request.headers.get(header_key)
+            multi_value_headers_dict[header_key.lower()] = flask_request.headers.getlist(header_key)
 
-        headers_dict["X-Forwarded-Proto"] = flask_request.scheme
-        multi_value_headers_dict["X-Forwarded-Proto"] = [flask_request.scheme]
+        headers_dict["x-forwarded-proto"] = flask_request.scheme
+        multi_value_headers_dict["x-forwarded-proto"] = [flask_request.scheme]
 
-        headers_dict["X-Forwarded-Port"] = str(port)
-        multi_value_headers_dict["X-Forwarded-Port"] = [str(port)]
+        headers_dict["x-forwarded-port"] = str(port)
+        multi_value_headers_dict["x-forwarded-port"] = [str(port)]
         return headers_dict, multi_value_headers_dict
 
     @staticmethod
@@ -851,10 +851,10 @@ class LocalApigwService(BaseLocalService):
         # Multi-value request headers is not really supported by Flask.
         # See https://github.com/pallets/flask/issues/850
         for header_key in flask_request.headers.keys():
-            headers[header_key] = flask_request.headers.get(header_key)
+            headers[header_key.lower()] = flask_request.headers.get(header_key)
 
-        headers["X-Forwarded-Proto"] = flask_request.scheme
-        headers["X-Forwarded-Port"] = str(port)
+        headers["x-forwarded-proto"] = flask_request.scheme
+        headers["x-forwarded-port"] = str(port)
         return headers
 
     @staticmethod
