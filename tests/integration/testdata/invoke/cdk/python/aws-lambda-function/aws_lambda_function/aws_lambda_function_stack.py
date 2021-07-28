7d8c9113e4feb85b9cb9286681dfbe97497bc675
Raymond Wang <14915548+wchengru@users.noreply.github.com>
2021-07-28 09:21:05 -0700

    test: Integration test cases for CDK invoke  (#3112)
    * Integration tests for CDK local invoke
    
    * Install requirement in setup
    
    * black reformat
    
    * fix test case on parameter overrides

---
+++ tests/integration/testdata/invoke/cdk/python/aws-lambda-function/aws_lambda_function/aws_lambda_function_stack.py
@@ -0,0 +1,93 @@
+from aws_cdk import core as cdk
+
+# For consistency with other languages, `cdk` is the preferred import name for
+# the CDK's core module.  The following line also imports it as `core` for use
+# with examples from the CDK Developer's Guide, which are in the process of
+# being updated to use `cdk`.  You may delete this import if you don't need it.
+from aws_cdk import core, aws_lambda as _lambda, aws_iam as _iam
+
+DEFAULT_TIMEOUT = 5
+
+class AwsLambdaFunctionStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        _lambda.Function(
+            scope=self,
+            id="helloworld-serverless-function",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.from_asset("./lambda_code"),
+            handler="app.handler",
+        )
+
+        _lambda.Function(
+            scope=self,
+            id="timeout-function",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.from_asset("./lambda_code"),
+            handler="app.timeout_handler",
+            timeout=core.Duration.seconds(DEFAULT_TIMEOUT)
+        )
+
+        _lambda.Function(
+            scope=self,
+            id="custom-env-vars-function",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.from_asset("./lambda_code"),
+            handler="app.custom_env_var_echo_handler",
+            environment={"CustomEnvVar": "MyVar"}
+        )
+
+        _lambda.Function(
+            scope=self,
+            id="write-to-stdout-function",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.from_asset("./lambda_code"),
+            handler="app.write_to_stdout",
+        )
+
+        _lambda.Function(
+            scope=self,
+            id="write-to-stderr-function",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.from_asset("./lambda_code"),
+            handler="app.write_to_stderr",
+        )
+
+        _lambda.Function(
+            scope=self,
+            id="echo-event-function",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.from_asset("./lambda_code"),
+            handler="app.echo_event",
+        )
+
+        my_runtime_version_parameter = core.CfnParameter(
+            scope=self,
+            id="MyRuntimeVersion",
+            type="String",
+            description="A custom parameter",
+            default="",
+        )
+
+        empty_default_parameter = core.CfnParameter(
+            scope=self,
+            id="EmptyDefaultParameter",
+            type="String",
+            description="A custom parameter",
+            default="",
+        )
+
+        _lambda.Function(
+            scope=self,
+            id="echo-env-with-parameters",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.from_asset("./lambda_code"),
+            environment={
+                "MyRuntimeVersion" : my_runtime_version_parameter.value_as_string,
+                "EmptyDefaultParameter" : empty_default_parameter.value_as_string
+            },
+            handler="app.parameter_echo_handler"
+        )
+
