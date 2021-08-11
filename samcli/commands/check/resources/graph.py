3d6e2a8cf58f727ebae472a664c256a2fb9eabef
jordon-ogo <jordonogonoski@gmail.com>
2021-08-10 18:43:29 -0600

    Merge conflicts resolved
    Manual conflicts that had to be resolved

--- samcli/commands/check/resources/graph.py
+++ samcli/commands/check/resources/graph.py
@@ -1,33 +1,51 @@
 """
 Class for graph. All data is stored in the graph directly, or within nodes that are stored in the graph
 """
-from typing import List
+from typing import Dict, List, Union
 
-from samcli.commands.check.resources.lambda_function_pricing import LambdaFunctionPricing
+from samcli.commands.check.resources.lambda_function import LambdaFunction
+from samcli.commands.check.resources.warning import CheckWarning
+from samcli.commands.check.resources.unique_pricing_info import UniquePricingInfo
 
 
-class Graph:
-    entry_points: List
-    resources_to_analyze: List
-    green_warnings: List
-    yellow_warnings: List
-    red_warnings: List
-    red_burst_warnings: List
-    lambda_function_pricing_info: LambdaFunctionPricing
+class CheckGraph:
+    """
+    A graph object stores four main item types.
 
-    def __init__(self):
-        self.entry_points: List = []
-        self.resources_to_analyze: List = []
-        self.green_warnings: List = []
-        self.yellow_warnings: List = []
-        self.red_warnings: List = []
-        self.red_burst_warnings: List = []
-        self.lambda_function_pricing_info: LambdaFunctionPricing = None
+    entry_points: All possible entry points to the application based on the template.
+    resources_to_analyze: All resources that need to be directly checked for bottle
+      neck issues (just lambda functions for now).
+    green/yellow/red/red_burst_warnings: The four different warning types that
+      the user can be presented
+    unique_pricing_info: Contains the user entered data for all pricing questions
+      for every resource
+    """
 
-    def generate(self, lambda_functions: List) -> None:
+    entry_points: List[Union[LambdaFunction]]
+    resources_to_analyze: List[Union[LambdaFunction]]
+    green_warnings: List[CheckWarning]
+    yellow_warnings: List[CheckWarning]
+    red_warnings: List[CheckWarning]
+    red_burst_warnings: List[CheckWarning]
+    unique_pricing_info: Dict[str, UniquePricingInfo]
+
+    def __init__(self, lambda_functions: List[LambdaFunction]):
+        self.entry_points = []
+        self.resources_to_analyze = []
+        self.green_warnings = []
+        self.yellow_warnings = []
+        self.red_warnings = []
+        self.red_burst_warnings = []
+        self.unique_pricing_info = {}
+
+        self._generate(lambda_functions)
+
+    def _generate(self, lambda_functions: List[LambdaFunction]) -> None:
         """Generates the graph based on the connections calulated
-        Args:
-            lambda_functions (List): List of all lambda functions in template
+        Parameters
+        ----------
+            lambda_functions: List[LambdaFunction]
+              List of all lambda functions in template
         """
         # Find all entry points
         for function in lambda_functions:
