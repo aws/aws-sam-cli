4675c8eba0f7af67329342134c3382fb0e54f4fe
Jacob Fuss <jfuss@users.noreply.github.com>
2021-08-11 16:32:21 -0500

    fix(integ): Fix Invalid image tag errors

--- tests/integration/deploy/test_deploy_command.py
+++ tests/integration/deploy/test_deploy_command.py
@@ -37,6 +37,8 @@ class TestDeploy(PackageIntegBase, DeployIntegBase):
         # setup some images locally by pulling them.
         for repo, tag in cls.local_images:
             cls.docker_client.api.pull(repository=repo, tag=tag)
+            cls.docker_client.api.tag(f"{repo}:{tag}", "emulation-python3.8", tag="latest")
+
         # setup signing profile arn & name
         cls.signing_profile_name = os.environ.get("AWS_SIGNING_PROFILE_NAME")
         cls.signing_profile_version_arn = os.environ.get("AWS_SIGNING_PROFILE_VERSION_ARN")
