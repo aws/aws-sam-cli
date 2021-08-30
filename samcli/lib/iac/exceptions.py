1164abf15787dc01d93e5e2e5da224eefc1a0f06
Daniel Mil <84205762+mildaniel@users.noreply.github.com>
2021-08-30 15:35:35 -0700

    feat: Update IaC Interface to Match New Design (#3202)
    * Update interfaces according to new design
    
    * Update interfaces
    
    * Test disable pylint duplicate code
    
    * Temporarily disable code cov. and linting for the new interfaces
    
    * Change type of click options to Dict[Any, Any]
    
    * Add IaCFactory and tests
    
    * Black reformat
    
    * Minor code quality update
    
    * Changes based on comments

---
+++ samcli/lib/iac/exceptions.py
@@ -0,0 +1,19 @@
+"""
+IaC Exceptions
+"""
+from typing import Optional
+
+from samcli.commands.exceptions import UserException
+
+
+class InvalidIaCPluginException(UserException):
+    def __init__(self, files: Optional[list] = None):
+        if files is None:
+            files = []
+        msg = "Could not determine the plugin type from the provided files:\n\n{files}"
+        UserException.__init__(self, msg.format(files=", ".join(files)))
+
+
+class InvalidProjectTypeException(UserException):
+    def __init__(self, msg):
+        UserException.__init__(self, msg)
