07ca931f5fa63b3296d289fbdfa092c8786a74f6
Daniel Mil <mladan@amazon.com>
2021-09-02 12:01:13 -0700

    Add types to parameters

--- samcli/lib/iac/cfn/helpers.py
+++ samcli/lib/iac/cfn/helpers.py
@@ -1,12 +1,16 @@
 """
 CFN IaC helper functions
 """
+from typing import Dict
+
 from samcli.lib.iac.plugins_interfaces import Stack
 from samcli.lib.providers.sam_base_provider import SamBaseProvider
 from samcli.lib.providers.sam_stack_provider import SamLocalStackProvider
 
 
-def get_resolved_cfn_template(template, parameter_overrides, global_parameter_overrides) -> Stack:
+def get_resolved_cfn_template(
+    template: Stack, parameter_overrides: Dict[str, str], global_parameter_overrides: Dict[str, str]
+) -> Stack:
     return SamBaseProvider.get_resolved_template_dict(
         template,
         SamLocalStackProvider.merge_parameter_overrides(parameter_overrides, global_parameter_overrides),
