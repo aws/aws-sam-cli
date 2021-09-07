ea9b74b6b9452e0212387ebc3d29a6df93c6abe5
Wilton Wang <CoshUS@users.noreply.github.com>
2021-09-07 12:34:55 -0700

    Added Constraint on Region and Python Version

--- tests/integration/deploy/test_managed_stack_deploy.py
+++ tests/integration/deploy/test_managed_stack_deploy.py
@@ -20,15 +20,22 @@ from tests.integration.package.package_integ_base import PackageIntegBase
 from tests.testing_utils import RUNNING_ON_CI, RUNNING_TEST_FOR_MASTER_ON_CI, RUN_BY_CANARY
 from tests.testing_utils import run_command, run_command_with_input
 
-# Deploy tests require credentials and CI/CD will only add credentials to the env if the PR is from the same repo.
+PYTHON_VERSION = os.environ.get("PYTHON_VERSION", "0.0.0")
+
+# Managed stack tests require credentials and CI/CD will only add credentials to the env if the PR is from the same repo.
 # This is to restrict package tests to run outside of CI/CD, when the branch is not master or tests are not run by Canary
-SKIP_DEPLOY_TESTS = RUNNING_ON_CI and RUNNING_TEST_FOR_MASTER_ON_CI and not RUN_BY_CANARY
+SKIP_MANAGED_STACK_TESTS = RUNNING_ON_CI and RUNNING_TEST_FOR_MASTER_ON_CI and not RUN_BY_CANARY
+# Limits the managed stack tests to be run on a single python version to avoid CI race conditions
+IS_TARGETTED_PYTHON_VERSION = PYTHON_VERSION.startswith("3.6")
+
+CFN_PYTHON_VERSION_SUFFIX = PYTHON_VERSION.replace(".", "-")
 CFN_SLEEP = 3
 TIMEOUT = 300
-CFN_PYTHON_VERSION_SUFFIX = os.environ.get("PYTHON_VERSION", "0.0.0").replace(".", "-")
+# Set region for managed stacks to be in a different region than the ones in deploy
+DEFAULT_REGION = "us-west-2"
 
 
-@skipIf(SKIP_DEPLOY_TESTS, "Skip deploy tests in CI/CD only")
+@skipIf(SKIP_MANAGED_STACK_TESTS or not IS_TARGETTED_PYTHON_VERSION, "Skip managed stack tests in CI/CD only")
 class TestManagedStackDeploy(PackageIntegBase, DeployIntegBase):
     @classmethod
     def setUpClass(cls):
@@ -36,13 +43,13 @@ class TestManagedStackDeploy(PackageIntegBase, DeployIntegBase):
         DeployIntegBase.setUpClass()
 
     def setUp(self):
-        self.cfn_client = boto3.client("cloudformation")
-        self.s3_client = boto3.client("s3")
+        self.cfn_client = boto3.client("cloudformation", region_name=DEFAULT_REGION)
+        self.s3_client = boto3.client("s3", region_name=DEFAULT_REGION)
         self.sns_arn = os.environ.get("AWS_SNS")
         self.stacks = []
         time.sleep(CFN_SLEEP)
 
-        self._delete_managed_stack(self.cfn_client, self.s3_client)
+        self._delete_managed_stack(self.cfn_client, self.s3_client, DEFAULT_REGION)
         self.assertFalse(self._does_stack_exist(self.cfn_client, SAM_CLI_STACK_NAME))
 
         super().setUp()
@@ -53,7 +60,7 @@ class TestManagedStackDeploy(PackageIntegBase, DeployIntegBase):
             # because of the termination protection, do not delete aws-sam-cli-managed-default stack
             stack_name = stack["name"]
             if stack_name != SAM_CLI_STACK_NAME:
-                region = stack.get("region")
+                region = stack.get("region") or DEFAULT_REGION
                 cfn_client = (
                     self.cfn_client if not region else boto3.client("cloudformation", config=Config(region_name=region))
                 )
@@ -77,11 +84,12 @@ class TestManagedStackDeploy(PackageIntegBase, DeployIntegBase):
             kms_key_id=self.kms_key,
             tags="integ=true clarity=yes foo_bar=baz",
             resolve_s3=True,
+            region=DEFAULT_REGION,
         )
 
         deploy_process_execute = run_command(deploy_command_list)
         self.assertEqual(deploy_process_execute.process.returncode, 0)
-        self._managed_stack_sanity_check(self.cfn_client, self.s3_client)
+        self._managed_stack_sanity_check(self.cfn_client, self.s3_client, DEFAULT_REGION)
 
     @parameterized.expand(["aws-serverless-function.yaml"])
     def test_managed_stack_creation_guided(self, template_file):
@@ -91,7 +99,9 @@ class TestManagedStackDeploy(PackageIntegBase, DeployIntegBase):
         self.stacks.append({"name": stack_name})
 
         # Package and Deploy in one go without confirming change set.
-        deploy_command_list = self.get_deploy_command_list(template_file=template_path, guided=True)
+        deploy_command_list = self.get_deploy_command_list(
+            template_file=template_path, region=DEFAULT_REGION, guided=True
+        )
 
         deploy_process_execute = run_command_with_input(
             deploy_command_list, "{}\n\n\n\n\n\n\n\n\n".format(stack_name).encode()
@@ -102,25 +112,25 @@ class TestManagedStackDeploy(PackageIntegBase, DeployIntegBase):
         self.stacks.append({"name": SAM_CLI_STACK_NAME})
         # Remove samconfig.toml
         os.remove(self.test_data_path.joinpath(DEFAULT_CONFIG_FILE_NAME))
-        self._managed_stack_sanity_check(self.cfn_client, self.s3_client)
+        self._managed_stack_sanity_check(self.cfn_client, self.s3_client, DEFAULT_REGION)
 
     def _method_to_stack_name(self, method_name):
         """Method expects method name which can be a full path. Eg: test.integration.test_deploy_command.method_name"""
         method_name = method_name.split(".")[-1]
         return f"{method_name.replace('_', '-')}-{CFN_PYTHON_VERSION_SUFFIX}"
 
-    def _delete_managed_stack(self, cfn_client, s3_client, wait=True):
+    def _delete_managed_stack(self, cfn_client, s3_client, region, wait=True):
         if not self._does_stack_exist(cfn_client, SAM_CLI_STACK_NAME):
             return
 
-        stack = boto3.resource("cloudformation").Stack(SAM_CLI_STACK_NAME)
+        stack = boto3.resource("cloudformation", region_name=region).Stack(SAM_CLI_STACK_NAME)
         resources = stack.resource_summaries.all()
         for resource in resources:
             if resource.resource_type == "AWS::S3::Bucket":
                 s3_bucket_name = resource.physical_resource_id
 
         if s3_bucket_name:
-            s3 = boto3.resource("s3")
+            s3 = boto3.resource("s3", region_name=region)
             bucket = s3.Bucket(s3_bucket_name)
             bucket.object_versions.delete()
             s3_client.delete_bucket(Bucket=s3_bucket_name)
@@ -131,11 +141,11 @@ class TestManagedStackDeploy(PackageIntegBase, DeployIntegBase):
             waiter_config = {"Delay": 15, "MaxAttempts": 120}
             waiter.wait(StackName=SAM_CLI_STACK_NAME, WaiterConfig=waiter_config)
 
-    def _managed_stack_sanity_check(self, cfn_client, s3_client):
+    def _managed_stack_sanity_check(self, cfn_client, s3_client, region):
         if not self._does_stack_exist(cfn_client, SAM_CLI_STACK_NAME):
             raise ManagedStackError("Managed stack does not exist")
 
-        stack = boto3.resource("cloudformation").Stack(SAM_CLI_STACK_NAME)
+        stack = boto3.resource("cloudformation", region_name=region).Stack(SAM_CLI_STACK_NAME)
 
         if stack.stack_status not in ["CREATE_COMPLETE", "UPDATE_COMPLETE"]:
             raise ManagedStackError("Managed stack status is not in CREATE_COMPLETE or UPDATE_COMPLETE")
@@ -148,7 +158,7 @@ class TestManagedStackDeploy(PackageIntegBase, DeployIntegBase):
         if not s3_bucket_name:
             raise ManagedStackError("Managed stack does not have S3 bucket")
 
-        s3 = boto3.resource("s3")
+        s3 = boto3.resource("s3", region_name=region)
         if s3.Bucket(s3_bucket_name) not in s3.buckets.all():
             raise ManagedStackError("Managed stack S3 bucket does not exist")
 
