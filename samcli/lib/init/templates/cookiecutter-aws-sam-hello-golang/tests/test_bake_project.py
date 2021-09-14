899e4b6461818478e34d63a92aba12ba634a7cb5
Jonathan Ifegunni <jonife@amazon.com>
2021-09-14 18:53:02 -0500

    make black update

--- samcli/lib/init/templates/cookiecutter-aws-sam-hello-golang/tests/test_bake_project.py
+++ samcli/lib/init/templates/cookiecutter-aws-sam-hello-golang/tests/test_bake_project.py
@@ -19,29 +19,29 @@ def inside_dir(dirpath):
 
 
 def test_project_tree(cookies):
-    result = cookies.bake(extra_context={'project_name': 'test_project'})
+    result = cookies.bake(extra_context={"project_name": "test_project"})
     assert result.exit_code == 0
     assert result.exception is None
-    assert result.project.basename == 'test_project'
+    assert result.project.basename == "test_project"
 
     assert result.project.isdir()
-    assert result.project.join('README.md').isfile()
-    assert result.project.join('template.yaml').isfile()
-    assert result.project.join('hello-world').isdir()
-    assert result.project.join('hello-world', 'main.go').isfile()
-    assert result.project.join('hello-world', 'main_test.go').isfile()
+    assert result.project.join("README.md").isfile()
+    assert result.project.join("template.yaml").isfile()
+    assert result.project.join("hello-world").isdir()
+    assert result.project.join("hello-world", "main.go").isfile()
+    assert result.project.join("hello-world", "main_test.go").isfile()
 
 
 def test_app_content(cookies):
-    result = cookies.bake(extra_context={'project_name': 'test_project'})
-    app_file = result.project.join('hello-world', 'main.go')
+    result = cookies.bake(extra_context={"project_name": "test_project"})
+    app_file = result.project.join("hello-world", "main.go")
     app_content = app_file.readlines()
-    app_content = ''.join(app_content)
+    app_content = "".join(app_content)
 
     contents = (
         "github.com/aws/aws-lambda-go/events",
         "resp, err := http.Get(DefaultHTTPGetAddress)",
-        "lambda.Start(handler)"
+        "lambda.Start(handler)",
     )
 
     for content in contents:
@@ -49,15 +49,15 @@ def test_app_content(cookies):
 
 
 def test_app_test_content(cookies):
-    result = cookies.bake(extra_context={'project_name': 'test_project'})
-    app_file = result.project.join('hello-world', 'main_test.go')
+    result = cookies.bake(extra_context={"project_name": "test_project"})
+    app_file = result.project.join("hello-world", "main_test.go")
     app_content = app_file.readlines()
-    app_content = ''.join(app_content)
+    app_content = "".join(app_content)
 
     contents = (
-        "DefaultHTTPGetAddress = \"http://127.0.0.1:12345\"",
+        'DefaultHTTPGetAddress = "http://127.0.0.1:12345"',
         "DefaultHTTPGetAddress = ts.URL",
-        "Successful Request"
+        "Successful Request",
     )
 
     for content in contents:
@@ -65,10 +65,10 @@ def test_app_test_content(cookies):
 
 
 def test_app_template_content(cookies):
-    result = cookies.bake(extra_context={'project_name': 'test_project'})
-    app_file = result.project.join('template.yaml')
+    result = cookies.bake(extra_context={"project_name": "test_project"})
+    app_file = result.project.join("template.yaml")
     app_content = app_file.readlines()
-    app_content = ''.join(app_content)
+    app_content = "".join(app_content)
 
     contents = (
         "Runtime: go1.x",
