fcafcc76123126288c9f2af8824efd2f722c0530
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2021-09-16 14:59:17 -0700

    revert: remove test cmd (#390)

--- samcli/cli/command.py
+++ samcli/cli/command.py
@@ -24,7 +24,6 @@ _SAM_CLI_COMMAND_PACKAGES = [
     "samcli.commands.publish",
     "samcli.commands.traces",
     "samcli.commands.sync",
-    "samcli.commands.test",
     "samcli.commands.pipeline.pipeline",
     # We intentionally do not expose the `bootstrap` command for now. We might open it up later
     # "samcli.commands.bootstrap",
--- samcli/commands/test/__init__.py
+++
@@ -1,4 +0,0 @@
-"""`sam test` command."""
-
-# Expose the cli object here
-from .command import cli  # noqa
