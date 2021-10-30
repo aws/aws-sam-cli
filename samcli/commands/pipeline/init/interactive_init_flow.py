6d3ebb0e63c2feb25e01a981f8ff1e84c59d4481
Simon Moore <ssimmoo@amazon.com>
2021-10-29 18:15:40 -0700

    Correct documentation link for credentials

--- samcli/commands/pipeline/init/interactive_init_flow.py
+++ samcli/commands/pipeline/init/interactive_init_flow.py
@@ -152,8 +152,7 @@ class InteractiveInitFlow:
                         resources.
 
                         We recommend using an individual AWS account profiles for each stage in your
-                        pipeline. You can set these profiles up using [little bit of info on how to do
-                        this/docs].
+                        pipeline. You can set these profiles up using aws configure or ~/.aws/credentials see [https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-getting-started-set-up-credentials.html].
                         """
                     )
                 )
