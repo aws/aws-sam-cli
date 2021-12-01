2752f9d2ffa9eeed7a6cda88da6e8bec1579c941
Qingchuan Ma <qingchm@amazon.com>
2021-12-01 12:46:14 -0800

    Enabling httparty dependency for ruby tests

--- tests/integration/testdata/sync/infra/before/Ruby/function/app.rb
+++ tests/integration/testdata/sync/infra/before/Ruby/function/app.rb
@@ -1,3 +1,4 @@
+require 'httparty'
 require 'json'
 require 'layer_test'
 
@@ -21,19 +22,19 @@ def lambda_handler(event:, context:)
   #     # api-gateway-simple-proxy-for-lambda-output-format
   #     Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
 
-  # begin
-  #   response = HTTParty.get('http://checkip.amazonaws.com/')
-  # rescue HTTParty::Error => error
-  #   puts error.inspect
-  #   raise error
-  # end
+  begin
+    response = HTTParty.get('http://checkip.amazonaws.com/')
+  rescue HTTParty::Error => error
+    puts error.inspect
+    raise error
+  end
 
   {
     statusCode: 200,
     body: {
       message: "#{layer_test()+1}",
-      location: "location"
-      # location: response.body
+      # location: "location"
+      location: response.body
     }.to_json
   }
 end
