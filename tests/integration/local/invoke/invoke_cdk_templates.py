67c8b7046928b8d47dfd813c4376ae1168b898b0
Daniel Mil <mladan@amazon.com>
2021-11-30 17:45:40 -0800

    Isolate resources to enable test parallelism

--- tests/integration/local/invoke/invoke_cdk_templates.py
+++ tests/integration/local/invoke/invoke_cdk_templates.py
@@ -1,4 +1,5 @@
 import json
+import shutil
 from pathlib import Path
 from unittest import skipIf
 
@@ -30,26 +31,33 @@ class TestCDKSynthesizedTemplatesFunctions(InvokeIntegBase):
     @classmethod
     def setUpClass(cls):
         # Run sam build first to build the image functions
-        # We only need to create these images once, and we can reuse the for
-        # all tests since they don't change. Then we remove them after all tests are run.
+        # We only need to create these images once
+        # We remove them after they are no longer used
         super(TestCDKSynthesizedTemplatesFunctions, cls).setUpClass()
         build_command_list = super().get_build_command_list(cls, template_path=cls.template_path)
         super().run_command(cls, command_list=build_command_list)
 
-    @classmethod
-    def tearDownClass(cls) -> None:
-        # Remove images
+    def tearDown(self) -> None:
+        # Tear down a unique image resource after it is finished being used
         docker_client = docker.from_env()
-        for function in cls.functions:
-            try:
-                docker_client.api.remove_image(f"{function.lower()}")
-                docker_client.api.remove_image(f"{function.lower()}:{RAPID_IMAGE_TAG_PREFIX}-{version}-{X86_64}")
-            except APIError:
-                pass
+        try:
+            to_remove = self.teardown_function_name
+            docker_client.api.remove_image(f"{to_remove.lower()}")
+            docker_client.api.remove_image(f"{to_remove.lower()}:{RAPID_IMAGE_TAG_PREFIX}-{version}-{X86_64}")
+        except (APIError, AttributeError):
+            pass
+
+        try:
+            # We don't actually use the build dir so we don't care if it's removed before another process finishes
+            shutil.rmtree(str(Path().joinpath(".aws-sam")))
+        except FileNotFoundError:
+            pass
 
     @parameterized.expand(functions)
     @pytest.mark.flaky(reruns=3)
     def test_build_and_invoke_image_function(self, function_name):
+        # Set the function name to be removed during teardown
+        self.teardown_function_name = function_name
         local_invoke_command_list = self.get_command_list(
             function_to_invoke=function_name, template_path=self.template_path
         )
@@ -62,11 +70,9 @@ class TestCDKSynthesizedTemplatesFunctions(InvokeIntegBase):
         self.assertEqual(return_code, 0)
         self.assertEqual(response, expected_response)
 
-    @parameterized.expand(functions)
-    @pytest.mark.flaky(reruns=3)
-    def test_invoke_with_utf8_event(self, function_name):
+    def test_invoke_with_utf8_event(self):
         command_list = self.get_command_list(
-            function_name, template_path=self.template_path, event_path=self.event_utf8_path
+            "StandardFunctionConstructZipFunction", template_path=self.template_path, event_path=self.event_utf8_path
         )
         stdout, _, return_code = self.run_command(command_list)
         self.assertEqual(return_code, 0)
