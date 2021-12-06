4d24a261bb1ed02517e2d9c4354e125604d18f51
Raymond Wang <14915548+wchengru@users.noreply.github.com>
2021-12-06 08:46:53 -0800

    Add unique id for customer defined cdk functions (#3498)
    * Add unique id for customer defined cdk functions
    
    * Add unit test
    
    * black reformat
    
    * changes to resolve comments
    
    * remove temp build dir
    
    * Add integration tests
    
    * Fix test setup
    
    * black reformat
    
    * Add integration test cases
    
    Co-authored-by: Jacob Fuss <32497805+jfuss@users.noreply.github.com>

--- tests/integration/testdata/invoke/cdk/lambda_code/app.js
+++ tests/integration/testdata/invoke/cdk/lambda_code/app.js
@@ -15,3 +15,19 @@ exports.lambdaHandler = async (event, context) => {
 
     return response
 };
+
+exports.lambdaFunctionNameHandler = async (event, context) => {
+    try {
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: context.functionName,
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
