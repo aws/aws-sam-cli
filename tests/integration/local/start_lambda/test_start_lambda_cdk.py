e438969766aefcebd141f66fb1dd5ed6a807f09e
Daniel Mil <mladan@amazon.com>
2021-12-07 13:56:58 -0800

    Remove duplicate lambda warm container tests

--- tests/integration/local/start_lambda/test_start_lambda_cdk.py
+++ tests/integration/local/start_lambda/test_start_lambda_cdk.py
@@ -1,17 +1,10 @@
-import json
-import uuid
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from time import time
-
 import boto3
 import pytest
 from botocore import UNSIGNED
 from botocore.config import Config
 from botocore.exceptions import ClientError
 
-from samcli.commands.local.cli_common.invoke_context import ContainersInitializationMode
 from tests.integration.local.start_lambda.start_lambda_api_integ_base import StartLambdaIntegBaseClass
-from tests.integration.local.start_lambda.test_start_lambda import TestWarmContainersBaseClass
 
 
 class TestCDKLambdaService(StartLambdaIntegBaseClass):
@@ -126,119 +119,3 @@ class TestLambdaServiceErrorCases(StartLambdaIntegBaseClass):
             self.lambda_client.invoke(FunctionName="EchoEventFunction", InvocationType="DryRun")
 
         self.assertEqual(str(error.exception), expected_error_message)
-
-
-class TestParallelRequests(StartLambdaIntegBaseClass):
-    template_path = "/testdata/invoke/cdk/cdk_template.yaml"
-
-    def setUp(self):
-        self.url = "http://127.0.0.1:{}".format(self.port)
-        self.lambda_client = boto3.client(
-            "lambda",
-            endpoint_url=self.url,
-            region_name="us-east-1",
-            use_ssl=False,
-            verify=False,
-            config=Config(signature_version=UNSIGNED, read_timeout=120, retries={"max_attempts": 0}),
-        )
-
-    @pytest.mark.flaky(reruns=3)
-    @pytest.mark.timeout(timeout=300, method="thread")
-    def test_same_endpoint(self):
-        """
-        Send two requests to the same path at the same time. This is to ensure we can handle
-        multiple requests at once and do not block/queue up requests
-        """
-        number_of_requests = 10
-        start_time = time()
-        with ThreadPoolExecutor(number_of_requests) as thread_pool:
-            futures = [
-                thread_pool.submit(self.lambda_client.invoke, FunctionName="HelloWorldFunction")
-                for _ in range(0, number_of_requests)
-            ]
-            results = [r.result() for r in as_completed(futures)]
-
-            end_time = time()
-
-            self.assertEqual(len(results), 10)
-            self.assertGreater(end_time - start_time, 10)
-
-            for result in results:
-                self.assertEqual(result.get("Payload").read().decode("utf-8"), '"Hello world!"')
-
-
-class TestWarmContainers(TestWarmContainersBaseClass):
-    template_path = "/testdata/start_api/cdk/template-cdk-warm-container.yaml"
-    container_mode = ContainersInitializationMode.EAGER.value
-    mode_env_variable = str(uuid.uuid4())
-    parameter_overrides = {"ModeEnvVariable": mode_env_variable}
-
-    @pytest.mark.flaky(reruns=3)
-    @pytest.mark.timeout(timeout=600, method="thread")
-    def test_can_invoke_lambda_function_successfully(self):
-        result = self.lambda_client.invoke(FunctionName="HelloWorldFunction")
-        self.assertEqual(result.get("StatusCode"), 200)
-
-        response = json.loads(result.get("Payload").read().decode("utf-8"))
-        self.assertEqual(response.get("statusCode"), 200)
-        self.assertEqual(json.loads(response.get("body")), {"hello": "world"})
-
-
-class TestWarmContainersInitialization(TestWarmContainersBaseClass):
-    template_path = "/testdata/start_api/cdk/template-cdk-warm-container.yaml"
-    container_mode = ContainersInitializationMode.EAGER.value
-    mode_env_variable = str(uuid.uuid4())
-    parameter_overrides = {"ModeEnvVariable": mode_env_variable}
-
-    @pytest.mark.flaky(reruns=3)
-    @pytest.mark.timeout(timeout=600, method="thread")
-    def test_all_containers_are_initialized_before_any_invoke(self):
-        initiated_containers = self.count_running_containers()
-        self.assertEqual(initiated_containers, 2)
-
-
-class TestLazyContainers(TestWarmContainersBaseClass):
-    template_path = "/testdata/start_api/cdk/template-cdk-warm-container.yaml"
-    container_mode = ContainersInitializationMode.LAZY.value
-    mode_env_variable = str(uuid.uuid4())
-    parameter_overrides = {"ModeEnvVariable": mode_env_variable}
-
-    @pytest.mark.flaky(reruns=3)
-    @pytest.mark.timeout(timeout=600, method="thread")
-    def test_can_invoke_lambda_function_successfully(self):
-        result = self.lambda_client.invoke(FunctionName="HelloWorldFunction")
-        self.assertEqual(result.get("StatusCode"), 200)
-
-        response = json.loads(result.get("Payload").read().decode("utf-8"))
-        self.assertEqual(response.get("statusCode"), 200)
-        self.assertEqual(json.loads(response.get("body")), {"hello": "world"})
-
-
-class TestLazyContainersInitialization(TestWarmContainersBaseClass):
-    template_path = "/testdata/start_api/cdk/template-cdk-warm-container.yaml"
-    container_mode = ContainersInitializationMode.LAZY.value
-    mode_env_variable = str(uuid.uuid4())
-    parameter_overrides = {"ModeEnvVariable": mode_env_variable}
-
-    @pytest.mark.flaky(reruns=3)
-    @pytest.mark.timeout(timeout=600, method="thread")
-    def test_all_containers_are_initialized_before_any_invoke(self):
-        initiated_containers = self.count_running_containers()
-        self.assertEqual(initiated_containers, 0)
-
-
-class TestLazyContainersMultipleInvoke(TestWarmContainersBaseClass):
-    template_path = "/testdata/start_api/cdk/template-cdk-warm-container.yaml"
-    container_mode = ContainersInitializationMode.LAZY.value
-    mode_env_variable = str(uuid.uuid4())
-    parameter_overrides = {"ModeEnvVariable": mode_env_variable}
-
-    @pytest.mark.flaky(reruns=3)
-    @pytest.mark.timeout(timeout=600, method="thread")
-    def test_only_one_new_created_containers_after_lambda_function_invoke(self):
-        initiated_containers_before_any_invoke = self.count_running_containers()
-        self.lambda_client.invoke(FunctionName="HelloWorldFunction")
-        initiated_containers = self.count_running_containers()
-
-        # only one container is initialized
-        self.assertEqual(initiated_containers, initiated_containers_before_any_invoke + 1)
