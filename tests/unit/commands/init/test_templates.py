4d250ce7672d06a20f2e526ce281eb546aab55e3
jonife <79116465+jonife@users.noreply.github.com>
2021-12-17 19:26:52 -0600

    feat: SAM Init interactive flow redesign (#3410)
    * SAM INIT Update
    
    * resolved conflict
    
    * test added and updated
    
    * block default prompt when Image packageType is provide and skip use case prompt when --app-template is provided
    
    * update default hello world prompt
    
    * filtering updates
    
    * added typing
    
    * Fixing failed canaries (#3532)
    
    * (fix) stop resolving relative paths for Function image URI (#3531)
    
    * (fix) stop resolve relative paths for function imageUri
    
    * fix unit testing
    
    * update template_does_not_meet_filter_criteria use <dict>.get
    
    Co-authored-by: Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
    Co-authored-by: Qingchuan Ma <69653965+qingchm@users.noreply.github.com>
    Co-authored-by: Mohamed Elasmar <71043312+moelasmar@users.noreply.github.com>

--- tests/unit/commands/init/test_templates.py
+++ tests/unit/commands/init/test_templates.py
@@ -15,7 +15,7 @@ class TestTemplates(TestCase):
     @patch("samcli.lib.utils.git_repo.GitRepo._ensure_clone_directory_exists")
     @patch("shutil.copytree")
     def test_location_from_app_template_zip(self, subprocess_mock, git_exec_mock, cd_mock, copy_mock):
-        it = InitTemplates(True)
+        it = InitTemplates()
 
         manifest = {
             "ruby2.5": [
@@ -42,7 +42,7 @@ class TestTemplates(TestCase):
     @patch("samcli.lib.utils.git_repo.GitRepo._ensure_clone_directory_exists")
     @patch("shutil.copytree")
     def test_location_from_app_template_image(self, subprocess_mock, git_exec_mock, cd_mock, copy_mock):
-        it = InitTemplates(True)
+        it = InitTemplates()
 
         manifest = {
             "ruby2.5-image": [
@@ -65,31 +65,3 @@ class TestTemplates(TestCase):
                     IMAGE, None, "ruby2.5-image", "bundler", "hello-world-lambda-image"
                 )
                 self.assertTrue(search("mock-ruby-image-template", location))
-
-    @patch("samcli.lib.utils.git_repo.GitRepo._git_executable")
-    @patch("click.prompt")
-    @patch("samcli.lib.utils.git_repo.GitRepo._ensure_clone_directory_exists")
-    def test_fallback_options(self, git_exec_mock, prompt_mock, cd_mock):
-        prompt_mock.return_value = "1"
-        with patch("samcli.lib.utils.git_repo.check_output", new_callable=MagicMock) as mock_sub:
-            with patch("samcli.cli.global_config.GlobalConfig.config_dir", new_callable=PropertyMock) as mock_cfg:
-                mock_sub.side_effect = OSError("Fail")
-                mock_cfg.return_value = Path("/tmp/test-sam")
-                it = InitTemplates(True)
-                location, app_template = it.prompt_for_location(ZIP, "ruby2.5", None, "bundler")
-                self.assertTrue(search("cookiecutter-aws-sam-hello-ruby", location))
-                self.assertEqual("hello-world", app_template)
-
-    @patch("samcli.lib.utils.git_repo.GitRepo._git_executable")
-    @patch("click.prompt")
-    @patch("samcli.lib.utils.git_repo.GitRepo._ensure_clone_directory_exists")
-    def test_fallback_process_error(self, git_exec_mock, prompt_mock, cd_mock):
-        prompt_mock.return_value = "1"
-        with patch("samcli.lib.utils.git_repo.check_output", new_callable=MagicMock) as mock_sub:
-            with patch("samcli.cli.global_config.GlobalConfig.config_dir", new_callable=PropertyMock) as mock_cfg:
-                mock_sub.side_effect = subprocess.CalledProcessError("fail", "fail", "not found".encode("utf-8"))
-                mock_cfg.return_value = Path("/tmp/test-sam")
-                it = InitTemplates(True)
-                location, app_template = it.prompt_for_location(ZIP, "ruby2.5", None, "bundler")
-                self.assertTrue(search("cookiecutter-aws-sam-hello-ruby", location))
-                self.assertEqual("hello-world", app_template)
