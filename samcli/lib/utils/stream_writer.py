418fcf9dddbc879e922e136f917352eefa7a854b
Brian Villemarette <brian.villemarette@trucesoftware.com>
2022-01-12 21:03:21 +0000

    Add typehints

--- samcli/lib/utils/stream_writer.py
+++ samcli/lib/utils/stream_writer.py
@@ -4,28 +4,19 @@ This class acts like a wrapper around output streams to provide any flexibility 
 import io
 import json
 from json import JSONDecodeError
+from typing import IO, Callable, Dict, Union
 
 from click.termui import style
 
 
 class StreamWriter:
-
-    COLORS = {
-        'GENERIC_JSON': {'fg': 'green', 'bold': True},
-        'DEBUG': {'fg': 'blue', 'bold': True},
-        'INFO': {'fg': 'cyan', 'bold': True},
-        'WARNING': {'fg': 'yellow', 'bold': True},
-        'ERROR': {'fg': 'red', 'bold': True},
-        'CRITICAL': {'fg': 'white', 'bg': 'red', 'bold': True},
-    }
-
-    def __init__(self, stream, auto_flush=False, pretty_json=False):
+    def __init__(self, stream: IO, auto_flush: bool = False, pretty_json: bool = False):
         """
         Instatiates new StreamWriter to the specified stream
 
         Parameters
         ----------
-        stream io.RawIOBase
+        stream IO
             Stream to wrap
         auto_flush bool
             Whether to autoflush the stream upon writing
@@ -40,10 +31,10 @@ class StreamWriter:
         self._buffer = io.BytesIO()
 
     @property
-    def stream(self):
+    def stream(self) -> IO:
         return self._stream
 
-    def write(self, output, encode=False):
+    def write(self, output: Union[str, bytes], encode: bool = False) -> None:
         """
         Writes specified text to the underlying stream
 
@@ -52,25 +43,25 @@ class StreamWriter:
         output bytes-like object
             Bytes to write
         """
-        if encode:
+        if isinstance(output, str) and encode:
             output = output.encode()
 
         if self._pretty_json:
-            try:
-                self._buffer.write(output.encode())
-            except AttributeError:
+            if isinstance(output, bytes):
                 self._stream_supports_bytes = True
-                self._buffer.write(output)
+            else:
+                output = output.encode()
+            self._buffer.write(output)
             self.write_from_buffer()
         else:
             self._stream.write(output)
 
-    def flush(self):
+    def flush(self) -> None:
         if self._pretty_json:
             self.write_from_buffer(force_write=True)
         self._stream.flush()
 
-    def write_from_buffer(self, encode=False, force_write=False):
+    def write_from_buffer(self, encode: bool = False, force_write: bool = False) -> None:
         remainder = b''
         self._buffer.seek(0)
         for line in self._buffer:
@@ -90,20 +81,28 @@ class StreamWriter:
         if self._auto_flush:
             self._stream.flush()
 
-    def format_line(self, line):
+    def format_line(self, line: bytes) -> bytes:
         try:
             line_obj = json.loads(line)
-            line = json.dumps(line_obj, indent=4)
+            line_str = json.dumps(line_obj, indent=4)
             if self._colorize:
-                if 'exception' in line_obj and 'message' in line_obj:
-                    style_args = StreamWriter.COLORS['CRITICAL']
-                elif 'level' in line_obj:
-                    style_args = StreamWriter.COLORS.get(line_obj['level'], StreamWriter.COLORS['GENERIC_JSON'])
-                else:
-                    style_args = StreamWriter.COLORS['GENERIC_JSON']
+                styles: Dict[str, Callable] = {
+                    'GENERIC_JSON': lambda line_str: style(line_str, fg='green', bold=True),
+                    'DEBUG': lambda line_str: style(line_str, fg='blue', bold=True),
+                    'INFO': lambda line_str: style(line_str, fg='cyan', bold=True),
+                    'WARNING': lambda line_str: style(line_str, fg='yellow', bold=True),
+                    'ERROR': lambda line_str: style(line_str, fg='red', bold=True),
+                    'CRITICAL': lambda line_str: style(line_str, fg='white', bg='red', bold=True),
+                }
 
-                line = style(line, **style_args)
-            line = line.encode()
+                if 'exception' in line_obj and 'message' in line_obj:
+                    line_str = styles['CRITICAL'](line_str)
+                elif 'level' in line_obj and line_obj['level'].upper() in styles.keys():
+                    line_str = styles[line_obj['level'].upper()](line_str)
+                else:
+                    line_str = styles['GENERIC_JSON'](line_str)
+
+            line = line_str.encode()
         except JSONDecodeError:
             pass
         return line
