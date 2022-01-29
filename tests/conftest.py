bd69c485b96e128cbfc17b91bcb897d8d067ac3c
Wilton Wang <CoshUS@users.noreply.github.com>
2022-01-29 01:18:59 -0800

    Test Collection Modification POC

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,22 @@
 import os
 
+from typing import List
+
+from pytest import Session, Item
+from _pytest.config import Config
+
 
 if "__SAM_CLI_TELEMETRY_ENDPOINT_URL" not in os.environ:
     os.environ["__SAM_CLI_TELEMETRY_ENDPOINT_URL"] = ""
+
+
+def pytest_collection_modifyitems(session: Session, config: Config, items: List[Item]) -> None:
+    selected_items = list()
+    for i in range(len(items)):
+        if i % 100 == 0:
+            selected_items.append(items[i])
+
+    for item in items.copy():
+        if item not in selected_items:
+            items.remove(item)
+    print(items)
