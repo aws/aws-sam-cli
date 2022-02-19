bd9f5d312ad3e8f77ca46ede4ce2703f151ee207
Wilton_ <CoshUS@users.noreply.github.com>
2022-02-19 00:45:23 -0800

    fix: Base Dir Support for Watch (#3614)
    * Added Base Dir Support for Watch
    
    * Updated path to base Path Class
    
    * Updated Tests
    
    Co-authored-by: Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>

--- tests/unit/lib/utils/test_resource_trigger.py
+++ tests/unit/lib/utils/test_resource_trigger.py
@@ -20,10 +20,8 @@ from samcli.lib.providers.provider import ResourceIdentifier
 class TestResourceTrigger(TestCase):
     @patch("samcli.lib.utils.resource_trigger.PathHandler")
     @patch("samcli.lib.utils.resource_trigger.RegexMatchingEventHandler")
-    @patch("samcli.lib.utils.resource_trigger.Path")
-    def test_single_file_path_handler(self, path_mock, handler_mock, bundle_mock):
+    def test_single_file_path_handler(self, handler_mock, bundle_mock):
         path = MagicMock()
-        path_mock.return_value = path
         file_path = MagicMock()
         file_path.__str__.return_value = "/parent/file"
 
@@ -34,9 +32,9 @@ class TestResourceTrigger(TestCase):
 
         path.resolve.return_value = file_path
 
-        ResourceTrigger.get_single_file_path_handler("/parent/file")
+        ResourceTrigger.get_single_file_path_handler(path)
 
-        path_mock.assert_called_once_with("/parent/file")
+        path.resolve.assert_called_once()
         escaped_path = re.escape("/parent/file")
         handler_mock.assert_called_once_with(
             regexes=[f"^{escaped_path}$"], ignore_regexes=[], ignore_directories=True, case_sensitive=ANY
@@ -45,17 +43,15 @@ class TestResourceTrigger(TestCase):
 
     @patch("samcli.lib.utils.resource_trigger.PathHandler")
     @patch("samcli.lib.utils.resource_trigger.RegexMatchingEventHandler")
-    @patch("samcli.lib.utils.resource_trigger.Path")
-    def test_dir_path_handler(self, path_mock, handler_mock, bundle_mock):
+    def test_dir_path_handler(self, handler_mock, bundle_mock):
         path = MagicMock()
-        path_mock.return_value = path
         folder_path = MagicMock()
 
         path.resolve.return_value = folder_path
 
-        ResourceTrigger.get_dir_path_handler("/parent/folder/", ignore_regexes=["a", "a/b"])
+        ResourceTrigger.get_dir_path_handler(path, ignore_regexes=["a", "a/b"])
 
-        path_mock.assert_called_once_with("/parent/folder/")
+        path.resolve.assert_called_once()
         handler_mock.assert_called_once_with(
             regexes=["^.*$"], ignore_regexes=["a", "a/b"], ignore_directories=False, case_sensitive=ANY
         )
@@ -90,8 +86,9 @@ class TestCodeResourceTrigger(TestCase):
     @patch("samcli.lib.utils.resource_trigger.get_resource_by_id")
     def test_init(self, get_resource_by_id_mock):
         stacks = [MagicMock(), MagicMock()]
+        base_dir = MagicMock()
         on_code_change_mock = MagicMock()
-        trigger = CodeResourceTrigger(ResourceIdentifier("A"), stacks, on_code_change_mock)
+        trigger = CodeResourceTrigger(ResourceIdentifier("A"), stacks, base_dir, on_code_change_mock)
         self.assertEqual(trigger._resource, get_resource_by_id_mock.return_value)
         self.assertEqual(trigger._on_code_change, on_code_change_mock)
 
@@ -99,11 +96,12 @@ class TestCodeResourceTrigger(TestCase):
     @patch("samcli.lib.utils.resource_trigger.get_resource_by_id")
     def test_init_invalid(self, get_resource_by_id_mock):
         stacks = [MagicMock(), MagicMock()]
+        base_dir = MagicMock()
         on_code_change_mock = MagicMock()
         get_resource_by_id_mock.return_value = None
 
         with self.assertRaises(ResourceNotFound):
-            CodeResourceTrigger(ResourceIdentifier("A"), stacks, on_code_change_mock)
+            CodeResourceTrigger(ResourceIdentifier("A"), stacks, base_dir, on_code_change_mock)
 
 
 class TestLambdaFunctionCodeTrigger(TestCase):
@@ -112,13 +110,14 @@ class TestLambdaFunctionCodeTrigger(TestCase):
     @patch("samcli.lib.utils.resource_trigger.get_resource_by_id")
     def test_init(self, get_resource_by_id_mock, function_provider_mock):
         stacks = [MagicMock(), MagicMock()]
+        base_dir = MagicMock()
         on_code_change_mock = MagicMock()
         function_mock = function_provider_mock.return_value.get.return_value
 
         code_uri_mock = MagicMock()
         LambdaFunctionCodeTrigger._get_code_uri = code_uri_mock
 
-        trigger = LambdaFunctionCodeTrigger(ResourceIdentifier("A"), stacks, on_code_change_mock)
+        trigger = LambdaFunctionCodeTrigger(ResourceIdentifier("A"), stacks, base_dir, on_code_change_mock)
         self.assertEqual(trigger._function, function_mock)
         self.assertEqual(trigger._code_uri, code_uri_mock.return_value)
 
@@ -127,6 +126,7 @@ class TestLambdaFunctionCodeTrigger(TestCase):
     @patch("samcli.lib.utils.resource_trigger.get_resource_by_id")
     def test_init_invalid(self, get_resource_by_id_mock, function_provider_mock):
         stacks = [MagicMock(), MagicMock()]
+        base_dir = MagicMock()
         on_code_change_mock = MagicMock()
         function_provider_mock.return_value.get.return_value = None
 
@@ -134,7 +134,7 @@ class TestLambdaFunctionCodeTrigger(TestCase):
         LambdaFunctionCodeTrigger._get_code_uri = code_uri_mock
 
         with self.assertRaises(FunctionNotFound):
-            LambdaFunctionCodeTrigger(ResourceIdentifier("A"), stacks, on_code_change_mock)
+            LambdaFunctionCodeTrigger(ResourceIdentifier("A"), stacks, base_dir, on_code_change_mock)
 
     @patch.multiple(LambdaFunctionCodeTrigger, __abstractmethods__=set())
     @patch("samcli.lib.utils.resource_trigger.ResourceTrigger.get_dir_path_handler")
@@ -142,6 +142,7 @@ class TestLambdaFunctionCodeTrigger(TestCase):
     @patch("samcli.lib.utils.resource_trigger.get_resource_by_id")
     def test_get_path_handlers(self, get_resource_by_id_mock, function_provider_mock, get_dir_path_handler_mock):
         stacks = [MagicMock(), MagicMock()]
+        base_dir = MagicMock()
         on_code_change_mock = MagicMock()
         function_mock = function_provider_mock.return_value.get.return_value
 
@@ -151,7 +152,7 @@ class TestLambdaFunctionCodeTrigger(TestCase):
         bundle = MagicMock()
         get_dir_path_handler_mock.return_value = bundle
 
-        trigger = LambdaFunctionCodeTrigger(ResourceIdentifier("A"), stacks, on_code_change_mock)
+        trigger = LambdaFunctionCodeTrigger(ResourceIdentifier("A"), stacks, base_dir, on_code_change_mock)
         result = trigger.get_path_handlers()
 
         self.assertEqual(result, [bundle])
@@ -165,9 +166,10 @@ class TestLambdaZipCodeTrigger(TestCase):
     @patch("samcli.lib.utils.resource_trigger.get_resource_by_id")
     def test_get_code_uri(self, get_resource_by_id_mock, function_provider_mock):
         stacks = [MagicMock(), MagicMock()]
+        base_dir = MagicMock()
         on_code_change_mock = MagicMock()
         function_mock = function_provider_mock.return_value.get.return_value
-        trigger = LambdaZipCodeTrigger(ResourceIdentifier("A"), stacks, on_code_change_mock)
+        trigger = LambdaZipCodeTrigger(ResourceIdentifier("A"), stacks, base_dir, on_code_change_mock)
         result = trigger._get_code_uri()
         self.assertEqual(result, function_mock.codeuri)
 
@@ -177,9 +179,10 @@ class TestLambdaImageCodeTrigger(TestCase):
     @patch("samcli.lib.utils.resource_trigger.get_resource_by_id")
     def test_get_code_uri(self, get_resource_by_id_mock, function_provider_mock):
         stacks = [MagicMock(), MagicMock()]
+        base_dir = MagicMock()
         on_code_change_mock = MagicMock()
         function_mock = function_provider_mock.return_value.get.return_value
-        trigger = LambdaImageCodeTrigger(ResourceIdentifier("A"), stacks, on_code_change_mock)
+        trigger = LambdaImageCodeTrigger(ResourceIdentifier("A"), stacks, base_dir, on_code_change_mock)
         result = trigger._get_code_uri()
         self.assertEqual(result, function_mock.metadata.get.return_value)
 
@@ -189,10 +192,11 @@ class TestLambdaLayerCodeTrigger(TestCase):
     @patch("samcli.lib.utils.resource_trigger.get_resource_by_id")
     def test_init(self, get_resource_by_id_mock, layer_provider_mock):
         stacks = [MagicMock(), MagicMock()]
+        base_dir = MagicMock()
         on_code_change_mock = MagicMock()
         layer_mock = layer_provider_mock.return_value.get.return_value
 
-        trigger = LambdaLayerCodeTrigger(ResourceIdentifier("A"), stacks, on_code_change_mock)
+        trigger = LambdaLayerCodeTrigger(ResourceIdentifier("A"), stacks, base_dir, on_code_change_mock)
         self.assertEqual(trigger._layer, layer_mock)
         self.assertEqual(trigger._code_uri, layer_mock.codeuri)
 
@@ -201,13 +205,14 @@ class TestLambdaLayerCodeTrigger(TestCase):
     @patch("samcli.lib.utils.resource_trigger.get_resource_by_id")
     def test_get_path_handlers(self, get_resource_by_id_mock, layer_provider_mock, get_dir_path_handler_mock):
         stacks = [MagicMock(), MagicMock()]
+        base_dir = MagicMock()
         on_code_change_mock = MagicMock()
         layer_mock = layer_provider_mock.return_value.get.return_value
 
         bundle = MagicMock()
         get_dir_path_handler_mock.return_value = bundle
 
-        trigger = LambdaLayerCodeTrigger(ResourceIdentifier("A"), stacks, on_code_change_mock)
+        trigger = LambdaLayerCodeTrigger(ResourceIdentifier("A"), stacks, base_dir, on_code_change_mock)
         result = trigger.get_path_handlers()
 
         self.assertEqual(result, [bundle])
@@ -223,9 +228,10 @@ class TestDefinitionCodeTrigger(TestCase):
     @patch("samcli.lib.utils.resource_trigger.get_resource_by_id")
     def test_get_path_handler(self, get_resource_by_id_mock, single_file_handler_mock, path_mock, validator_mock):
         stacks = [MagicMock(), MagicMock()]
+        base_dir = MagicMock()
         resource = {"Properties": {"DefinitionUri": "abc"}}
         get_resource_by_id_mock.return_value = resource
-        trigger = DefinitionCodeTrigger("TestApi", "AWS::Serverless::Api", stacks, MagicMock())
+        trigger = DefinitionCodeTrigger("TestApi", "AWS::Serverless::Api", stacks, base_dir, MagicMock())
         result = trigger.get_path_handlers()
         self.assertEqual(result, [single_file_handler_mock.return_value])
         self.assertEqual(single_file_handler_mock.return_value.event_handler.on_any_event, trigger._validator_wrapper)
@@ -238,21 +244,23 @@ class TestDefinitionCodeTrigger(TestCase):
         self, get_resource_by_id_mock, single_file_handler_mock, path_mock, validator_mock
     ):
         stacks = [MagicMock(), MagicMock()]
+        base_dir = MagicMock()
         resource = {"Properties": {"Field": "abc"}}
         get_resource_by_id_mock.return_value = resource
         with self.assertRaises(MissingLocalDefinition):
-            trigger = DefinitionCodeTrigger("TestApi", "AWS::Serverless::Api", stacks, MagicMock())
+            trigger = DefinitionCodeTrigger("TestApi", "AWS::Serverless::Api", stacks, base_dir, MagicMock())
 
     @patch("samcli.lib.utils.resource_trigger.DefinitionValidator")
     @patch("samcli.lib.utils.resource_trigger.Path")
     @patch("samcli.lib.utils.resource_trigger.get_resource_by_id")
     def test_validator_wrapper(self, get_resource_by_id_mock, path_mock, validator_mock):
         stacks = [MagicMock(), MagicMock()]
+        base_dir = MagicMock()
         on_definition_change_mock = MagicMock()
         event_mock = MagicMock()
         validator_mock.return_value.validate.return_value = True
         resource = {"Properties": {"DefinitionUri": "abc"}}
         get_resource_by_id_mock.return_value = resource
-        trigger = DefinitionCodeTrigger("TestApi", "AWS::Serverless::Api", stacks, on_definition_change_mock)
+        trigger = DefinitionCodeTrigger("TestApi", "AWS::Serverless::Api", stacks, base_dir, on_definition_change_mock)
         trigger._validator_wrapper(event_mock)
         on_definition_change_mock.assert_called_once_with(event_mock)
