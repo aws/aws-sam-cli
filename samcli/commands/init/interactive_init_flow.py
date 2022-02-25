b2e33a47467bf57e49934c68818cad95e52b1dba
Jonathan Ifegunni <jonife@amazon.com>
2022-02-25 15:23:33 -0600

    fixed issues experienced in version 1.37.0-1.39.0

--- samcli/commands/init/interactive_init_flow.py
+++ samcli/commands/init/interactive_init_flow.py
@@ -1,6 +1,8 @@
 """
 Isolates interactive init prompt flow. Expected to call generator logic at end of flow.
 """
+import functools
+import re
 import tempfile
 import logging
 from typing import Optional, Tuple
@@ -15,7 +17,7 @@ from samcli.commands.init.interactive_event_bridge_flow import (
 )
 from samcli.commands.exceptions import SchemasApiException, InvalidInitOptionException
 from samcli.lib.schemas.schemas_code_manager import do_download_source_code_binding, do_extract_and_merge_schemas_code
-from samcli.local.common.runtime_template import LAMBDA_IMAGES_RUNTIMES_MAP, INIT_RUNTIMES
+from samcli.local.common.runtime_template import LAMBDA_IMAGES_RUNTIMES_MAP
 from samcli.commands.init.init_generator import do_generate
 from samcli.commands.init.init_templates import InitTemplates, InvalidInitTemplateError
 from samcli.lib.utils.osutils import remove
@@ -320,15 +322,96 @@ def _get_choice_from_options(chosen, options, question, msg):
     return options_list[int(choice) - 1]
 
 
-def get_sorted_runtimes(options_list):
-    runtimes = []
-    for runtime in options_list:
-        position = INIT_RUNTIMES.index(runtime)
-        runtimes.append(position)
-    sorted_runtimes = sorted(runtimes)
-    for index, position in enumerate(sorted_runtimes):
-        sorted_runtimes[index] = INIT_RUNTIMES[position]
-    return sorted_runtimes
+def get_sorted_runtimes(runtime_option_list):
+    """
+    return a list of sorted runtimes in ascending order of runtime names and
+    descending order of runtime version.
+
+    Parameters
+    ----------
+    runtime_option_list : list
+        list of possible runtime to be selected
+
+    Returns
+    -------
+    list
+        sorted list of possible runtime to be selected
+    """
+    runtime_list = []
+    for runtime in runtime_option_list:
+        extractLanguageFromRuntime = re.split(r"\d", runtime)[0]
+        extractVersionFromRuntime = re.search(r"\d.*", runtime).group()
+        runtime_list.append((extractLanguageFromRuntime, extractVersionFromRuntime))
+
+    runtime_list = list(sorted(runtime_list, key=functools.cmp_to_key(compare_runtimes)))
+    sorted_runtime = ["".join(runtime_tuple) for runtime_tuple in runtime_list]
+    return sorted_runtime
+
+
+def compare_runtimes(first_runtime, second_runtime):
+    """
+    Logic for comparsion supported runtime against each other
+
+    Parameters
+    ----------
+    first_runtime : str
+        runtime to be compare
+    second_runtime : str
+        runtime to be compare
+
+    Returns
+    -------
+    int
+        comparison result
+    """
+
+    first_runtime_name, first_runtime_version = first_runtime
+    second_runtime_name, second_runtime_version = second_runtime
+
+    if first_runtime_name == second_runtime_name:
+        first_version_number = get_version_number(first_runtime_version)
+        second_version_number = get_version_number(second_runtime_version)
+
+        if first_version_number == second_version_number:
+            return -1 if _ends_with_al2(first_runtime_version) else 1
+
+        return second_version_number - first_version_number
+
+    return 1 if first_runtime_name > second_runtime_name else -1
+
+
+def _ends_with_al2(runtime):
+    """
+    verify if the suffix of a runtime name is al2
+
+    Parameters
+    ----------
+    runtime : runtime
+        runtime to be verified
+
+    Returns
+    -------
+    bool
+        True if it ends with al2
+    """
+    return runtime.endswith(".al2")
+
+
+def get_version_number(runtime_version):
+    """
+    returns the runtime version number
+
+    Parameters
+    ----------
+    runtime_version : str
+        version of a runtime
+
+    Returns
+    -------
+    float
+        version number
+    """
+    return float(re.search(r"\d*(\.\d+)?", runtime_version).group())
 
 
 def _get_app_template_choice(templates_options, dependency_manager):
