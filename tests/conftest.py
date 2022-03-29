d692d5081840ec8b29c4fbb74bbed32bd991cf83
Mingkun He <hmingku@amazon.com>
2022-03-29 16:54:12 -0700

    parallel tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,24 @@
 import os
 
+from typing import List
+
+from pytest import Session, Item
+from _pytest.config import Config
+
 
 if "__SAM_CLI_TELEMETRY_ENDPOINT_URL" not in os.environ:
     os.environ["__SAM_CLI_TELEMETRY_ENDPOINT_URL"] = ""
+
+WORKER_COUNT = int(os.environ.get("WORKER_COUNT", "1"))
+WORKER_ID = int(os.environ.get("WORKER_ID", "0"))
+
+
+def pytest_collection_modifyitems(session: Session, config: Config, items: List[Item]) -> None:
+    selected_items = list()
+    for i in range(len(items)):
+        if i % WORKER_COUNT == WORKER_ID:
+            selected_items.append(items[i])
+
+    for item in items.copy():
+        if item not in selected_items:
+            items.remove(item)
