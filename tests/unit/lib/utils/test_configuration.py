5c977ea01fc8f1cf296aad7fb059803db7733192
Mingkun He <hmingku@amazon.com>
2022-04-18 11:58:00 -0700

    fix unit test

--- tests/unit/lib/utils/test_configuration.py
+++ tests/unit/lib/utils/test_configuration.py
@@ -4,4 +4,4 @@ from samcli.lib.utils import configuration
 
 class TestConfiguration(TestCase):
     def test_return_correct_value(self):
-        self.assertEqual(configuration.get_app_template_repo_commit(), "773d842c8f721d08c35321defa9087aaabf251f7")
+        self.assertEqual(configuration.get_app_template_repo_commit(), "09b7de41c32ee5f50ec8ceeec4a304534767844b")
