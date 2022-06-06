ee2015abbe4d93c9f198773a45712262fb311653
Andrew Zhan <zhandr@amazon.com>
2022-06-06 10:39:37 -0700

    removed uneccessary folder creation and deletion

--- tests/integration/list/list_integ_base.py
+++ tests/integration/list/list_integ_base.py
@@ -11,18 +11,6 @@ class ListIntegBase(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.cmd = cls.base_command()
-        cls.test_data_path = Path(__file__).resolve().parents[1].joinpath("testdata", "list")
-
-    def setUp(self):
-        super().setUp()
-        self.scratch_dir = str(Path(__file__).resolve().parent.joinpath(str(uuid.uuid4()).replace("-", "")[:10]))
-        os.mkdir(self.scratch_dir)
-        self.working_dir = tempfile.mkdtemp(dir=self.scratch_dir)
-
-    def tearDown(self):
-        super().tearDown()
-        self.working_dir and shutil.rmtree(self.working_dir, ignore_errors=True)
-        self.scratch_dir and shutil.rmtree(self.scratch_dir, ignore_errors=True)
 
     @classmethod
     def base_command(cls):
