77ce2ed34aaf97805624b1fb52418d9a1526a0fe
Ruperto Torres <torresxb@amazon.com>
2022-06-21 13:35:50 -0700

    redirect all output to stderr except hook output

--- samcli/hook_packages/terraform/prepare/main.bat
+++ samcli/hook_packages/terraform/prepare/main.bat
@@ -1,14 +1,29 @@
-:: ensure the script works regardless of where it's invoked from
 @echo off&setlocal
-for %%i in ("%~dp0.") do set "parent_path=%%~fi"
-cd %parent_path%
+:: redirect all stdout except for hook output to stderr
+(
+    :: ensure the script works regardless of where it's invoked from
+    for %%i in ("%~dp0.") do set "parent_path=%%~fi"
+    cd %parent_path%
 
-:: set up virtual environment
-python -m venv ./venv
-CALL venv/Scripts/activate
+    :: check that python 3 is installed
+    py -3 --version >NUL 2>&1
+    if errorlevel 1 goto errorNoPython
 
-:: install dependencies
-pip install -r requirements.txt
+    :: set up virtual environment
+    py -3 -m venv ./venv
+    CALL venv/Scripts/activate
+
+    :: install dependencies
+    pip install -r requirements.txt
+
+    :: execute hook if setup was successful
+    goto:hook
+)>&2
+
+:errorNoPython
+echo Python 3 not found. Please install before proceeding. >&2
+exit /b 1
 
 :: execute hook with any arguments passed in
+:hook
 python hook.py %*
--- samcli/hook_packages/terraform/prepare/main.sh
+++ samcli/hook_packages/terraform/prepare/main.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# copy stdout to file descriptor 3, and redirect stdout to stderr
+# this is to log everything to stderr (while maintaining a reference of stdout to restore later)
+exec 3>&1 1>&2
+
 # ensure the script works regardless of where it's invoked from
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
+
+# check that python 3 is installed
+if ! $(command -v python3 >/dev/null 2>&1)
+then
+    echo Python 3 not found. Please install before proceeding.
+    exit 1
+fi
 
 # set up virtual environment
 python3 -m venv ./venv
@@ -11,5 +22,8 @@ source venv/bin/activate
 # install dependencies
 pip install -r requirements.txt
 
+# copy file descriptor 3 back to stdout and close file descriptor 3
+# this will ensure the output of the hook goes to stdout
+exec 1>&3 3>&-
 # execute hook with any arguments passed in
 python hook.py "$@"
