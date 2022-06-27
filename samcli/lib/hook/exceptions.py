914ef5f728d9ed8f2d356ba9de163676ca83c86c
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2022-06-27 10:20:57 -0700

    Hooks wrapper (#3998)
    * Add Hook Wrapper
    
    * run black reformat
    
    * Update hook config and tests
    
    * Update hook wrapper and tests
    
    * Update prepare; Add docstrings
    
    * Improve code coverage
    
    * Update package look up logic and remove logs_path param
    
    * Fix lint issue
    
    * Update logic to parse metadata file loc from output in prepare method
    
    * Update json schema
    
    * Update json schema

---
+++ samcli/lib/hook/exceptions.py
@@ -0,0 +1,18 @@
+"""Hooks Exceptions"""
+from samcli.commands.exceptions import UserException
+
+
+class InvalidHookWrapperException(UserException):
+    pass
+
+
+class InvalidHookPackageException(UserException):
+    pass
+
+
+class HookPackageExecuteFunctionalityException(UserException):
+    pass
+
+
+class InvalidHookPackageConfigException(UserException):
+    pass
