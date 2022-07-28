f92c4b7460134f3f322e0d34687312d1a499d7b5
Mohamed Elasmar <71043312+moelasmar@users.noreply.github.com>
2022-07-27 18:09:58 -0700

    feat: add experimental flag to sam local invoke, and sam local start-lambda commands  (#4070)
    * fix the terraform hook config file to align with the Config schema
    
    * make sure to create the iacs_metadata output directory if it does not exist
    
    * exclude the files from the returned available hooks packages
    
    * update resources with relative paths to be absolute paths by joining them to the terraform application root directory
    
    * sam local commands integration testing
    
    * fix lint issues
    
    * install terraform cli in Appveyor
    
    * testing install terraform on ubuntu
    
    * fix install terraform on ubuntu
    
    * fix install terraform on ubuntu
    
    * remove printing sam local start-lambda command output
    
    * image package type lambda function mapping
    
    * feat: add experimental flag to sam local invoke and start-lambda commands
    
    * running black, and fixing lint issues
    
    * enhance the _update_resources_paths docs
    
    * use osutils.tempfile_platform_independent instead of NamedTemporaryFile to avoid windows issues
    
    * resolve Pr comments
    
    * resolve Pr comments
    
    * fix comment
    
    * feat: add experimental flag to sam local invoke and start-lambda commands
    
    * fix merging issue
    
    * fix merging issue
    
    * apply black and lint issues
    
    * handle PR comments
    
    * handle PR comments

--- samcli/commands/_utils/custom_options/hook_package_id_option.py
+++ samcli/commands/_utils/custom_options/hook_package_id_option.py
@@ -7,6 +7,7 @@ import os
 import click
 
 from samcli.commands._utils.constants import DEFAULT_BUILT_TEMPLATE_PATH
+from samcli.commands._utils.experimental import prompt_experimental, ExperimentalFlag
 from samcli.lib.hook.exceptions import InvalidHookWrapperException
 from samcli.lib.hook.hook_wrapper import IacHookWrapper, get_available_hook_packages_ids
 
@@ -28,6 +29,9 @@ class HookPackageIdOption(click.Option):
     def handle_parse_result(self, ctx, opts, args):
         opt_name = self.name.replace("_", "-")
         if self.name in opts:
+            command_name = ctx.command.name
+            if command_name in ["invoke", "start-lambda", "start-api"]:
+                command_name = f"local {command_name}"
             # validate the hook_package_id value exists
             hook_package_id = opts[self.name]
             iac_hook_wrapper = None
@@ -46,6 +50,23 @@ class HookPackageIdOption(click.Option):
                     raise click.BadParameter(
                         f"Parameters {opt_name}, and {','.join(self._invalid_coexist_options)} can not be used together"
                     )
+            # check beta-feature
+            beta_features = opts.get("beta_features")
+
+            # check if beta feature flag is required for a specific hook package
+            # The IaCs support experimental flag map will contain only the beta IaCs. In case we support the external
+            # hooks, we need to first know that the hook package is an external, and to handle the beta feature of it
+            # using different approach
+            experimental_entry = ExperimentalFlag.IaCsSupport.get(hook_package_id)
+            if beta_features is None and experimental_entry is not None:
+
+                iac_support_message = _get_iac_support_experimental_prompt_message(hook_package_id, command_name)
+                if not prompt_experimental(experimental_entry, iac_support_message):
+                    LOG.debug("Experimental flag is disabled and prepare hook is not run")
+                    return super().handle_parse_result(ctx, opts, args)
+            elif not beta_features:
+                LOG.debug("beta-feature flag is disabled and prepare hook is not run")
+                return super().handle_parse_result(ctx, opts, args)
 
             # call prepare hook
             built_template_path = DEFAULT_BUILT_TEMPLATE_PATH
@@ -68,3 +89,30 @@ class HookPackageIdOption(click.Option):
                 LOG.info("Prepare Hook is done, and metadata file generated at %s", metadata_file)
                 opts["template_file"] = metadata_file
         return super().handle_parse_result(ctx, opts, args)
+
+
+def _get_iac_support_experimental_prompt_message(hook_package_id: str, command: str) -> str:
+    """
+    return the customer prompt message for a specific hook package.
+
+    Parameters
+    ----------
+    hook_package_id: str
+        the hook package id to determine what is the supported iac
+
+    command: str
+        the current sam command
+    Returns
+    -------
+    str
+        the customer prompt message for a specific IaC.
+    """
+
+    supported_iacs_messages = {
+        "terraform": (
+            "Supporting Terraform applications is a beta feature.\n"
+            "Please confirm if you would like to proceed using SAM CLI with terraform application.\n"
+            f"You can also enable this beta feature with 'sam {command} --beta-features'."
+        )
+    }
+    return supported_iacs_messages.get(hook_package_id, "")
