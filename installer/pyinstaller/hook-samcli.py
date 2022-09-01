dd6a2b2cfb5769edc24c7e8f51e46f3aa1bcaf5d
Ruperto Torres <torresxb@amazon.com>
2022-09-01 12:30:43 -0700

    update script location in pyinstaller command

--- installer/pyinstaller/hook-samcli.py
+++ installer/pyinstaller/hook-samcli.py
@@ -4,10 +4,9 @@ from installer.pyinstaller.hidden_imports import SAM_CLI_HIDDEN_IMPORTS
 hiddenimports = SAM_CLI_HIDDEN_IMPORTS
 
 datas = (
-    # collect just the scripts
-    hooks.collect_all("samcli",
-                      include_py_files=True,
-                      include_datas=["scripts/*.py"])[0]
+    hooks.collect_all(
+        "samcli", include_py_files=True, include_datas=["hook_packages/terraform/copy_terraform_built_artifacts.py"]
+    )[0]
     + hooks.collect_data_files("samcli")
     + hooks.collect_data_files("samtranslator")
     + hooks.collect_data_files("aws_lambda_builders")
