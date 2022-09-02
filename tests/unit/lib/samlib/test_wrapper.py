b26a88bc4c7419e0eda08572527572431dbfe53f
Daniel Mil <mladan@amazon.com>
2022-09-02 14:44:37 -0700

    fix: Check for language extensions prefix

--- tests/unit/lib/samlib/test_wrapper.py
+++ tests/unit/lib/samlib/test_wrapper.py
@@ -11,13 +11,15 @@ class TestLanguageExtensionsPatching(TestCase):
         [
             ({"Transform": ["AWS::LanguageExtensions", "AWS::Serverless-2016-10-31"]}, True),
             ({"Transform": ["AWS::LanguageExtensions"]}, True),
+            ({"Transform": ["AWS::LanguageExtensions-extension"]}, True),
             ({"Transform": "AWS::LanguageExtensions"}, True),
+            ({"Transform": "AWS::LanguageExtensions-extension"}, True),
             ({"Transform": "AWS::Serverless-2016-10-31"}, False),
             ({}, False),
         ]
     )
     def test_check_using_langauge_extension(self, template, expected):
-        self.assertEqual(SamTranslatorWrapper._check_using_langauge_extension(template), expected)
+        self.assertEqual(SamTranslatorWrapper._check_using_language_extension(template), expected)
 
     @patch("samcli.lib.samlib.wrapper.SamResource")
     def test_patch_language_extensions(self, patched_sam_resource):
