188a5d8ae3607d63c24a01372587ab9674941d99
Sriram Madapusi Vasudevan <srirammv@amazon.com>
2022-09-10 22:28:05 -0700

    perf: init --help text to 350ms

---
+++ samcli/lib/build/deprecated_runtimes.py
@@ -0,0 +1,12 @@
+from typing import Set
+
+DEPRECATED_RUNTIMES: Set[str] = {
+    "nodejs4.3",
+    "nodejs6.10",
+    "nodejs8.10",
+    "nodejs10.x",
+    "dotnetcore2.0",
+    "dotnetcore2.1",
+    "python2.7",
+    "ruby2.5",
+}
