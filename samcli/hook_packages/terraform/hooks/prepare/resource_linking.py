b6de9fa997a0ac9a4d372d8542a8b5c6809134f6
Lucas <12496191+lucashuy@users.noreply.github.com>
2022-09-20 16:34:21 -0700

    Added configuration address cleaning function

--- samcli/hook_packages/terraform/hooks/prepare/resource_linking.py
+++ samcli/hook_packages/terraform/hooks/prepare/resource_linking.py
@@ -2,3 +2,21 @@
 Use Terraform plan to link resources together
 e.g. linking layers to functions
 """
+import re
+
+
+def _get_configuration_address(address: str) -> str:
+    """
+    Cleans all addresses of indicies and returns a clean address
+
+    Parameters
+    ==========
+    address : str
+        The address to clean
+
+    Returns
+    =======
+    str
+        The address clean of indices
+    """
+    return re.sub(r"\[[^\[\]]*\]", "", address)
