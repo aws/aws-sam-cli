a9347724f7c580ede82fc1cad6f28f162eef33a5
Ruperto Torres <86501267+torresxb1@users.noreply.github.com>
2022-09-20 16:20:44 -0700

    refactor: move hook into (new) prepare dir, and create resource_linking.py file (#4247)

--- samcli/hook_packages/terraform/main.py
+++ samcli/hook_packages/terraform/main.py
@@ -1,8 +1,8 @@
 """
 Module for Terraform hook entry points
 """
-from .hooks.prepare import prepare as tf_prepare
+from .hooks.prepare.hook import prepare as prepare_hook
 
 
 def prepare(params: dict) -> dict:
-    return tf_prepare(params)
+    return prepare_hook(params)
