dda368cd983022a6d3f6fcd4abf07e1a718bcc77
Ruperto Torres <torresxb@amazon.com>
2022-09-21 13:37:08 -0700

    address review feedback

--- samcli/hook_packages/terraform/hooks/prepare/resource_linking.py
+++ samcli/hook_packages/terraform/hooks/prepare/resource_linking.py
@@ -8,35 +8,37 @@ from typing import Any, Dict, List, Optional
 
 
 @dataclass
-class Value:
+class TFValue:
     constant_value: Any
     references: List[str]
 
 
 @dataclass
-class Module:
+class TFModule:
     # full path to the module, including parent modules
-    full_address: str
-    parent_module: Optional["Module"]
-    variables: Dict[str, Value]
-    resources: Dict[str, "Resource"]
-    children_modules: Dict[str, "Module"]
-    outputs: Dict[str, Value]
+    full_address: Optional[str]
+    parent_module: Optional["TFModule"]
+    variables: Dict[str, TFValue]
+    resources: Dict[str, "TFResource"]
+    children_modules: Dict[str, "TFModule"]
+    outputs: Dict[str, TFValue]
 
     # current module's + all child modules' resources
-    def get_all_resources(self) -> List["Resource"]:
+    def get_all_resources(self) -> List["TFResource"]:
         # TODO
         pass
 
 
 @dataclass
-class Resource:
+class TFResource:
     address: str
     type: str
     # the module this resource is defined in
-    module: "Module"
-    attributes: Dict[str, Value]
+    module: TFModule
+    attributes: Dict[str, TFValue]
 
     @property
     def full_address(self) -> str:
-        return f"{self.module.full_address}.{self.address}"
+        if self.module.full_address:
+            return f"{self.module.full_address}.{self.address}"
+        return self.address
