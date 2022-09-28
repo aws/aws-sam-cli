53b3a04e17c6b229856a011e6d9c0880e55d0121
Daniel Mil <84205762+mildaniel@users.noreply.github.com>
2022-09-28 15:31:14 -0700

    feat: Resolve Module Variables (#4260)
    * feat: Resolve module variables
    
    * Address comments
    
    * Update for readability

--- tests/unit/hook_packages/terraform/hooks/prepare/test_resource_linking.py
+++ tests/unit/hook_packages/terraform/hooks/prepare/test_resource_linking.py
@@ -1,9 +1,10 @@
 from copy import deepcopy
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 from parameterized import parameterized
 
+from samcli.hook_packages.terraform.hooks.prepare.exceptions import InvalidResourceLinkingException
 from samcli.hook_packages.terraform.hooks.prepare.resource_linking import (
     _clean_references_list,
     _get_configuration_address,
@@ -11,6 +12,7 @@ from samcli.hook_packages.terraform.hooks.prepare.resource_linking import (
     TFResource,
     ConstantValue,
     References,
+    _resolve_module_variable,
 )
 
 
@@ -131,3 +133,220 @@ class TestResourceLinking(TestCase):
         module = TFModule(None, None, {}, {}, {}, {})
         resource = TFResource("resource_address", "type", module, {})
         self.assertEqual(resource.full_address, "resource_address")
+
+    def test_resolve_module_variable_constant_value(self):
+        constant_value = ConstantValue(value="layer.arn")
+        module = TFModule(
+            variables={"layer": constant_value},
+            resources=[],
+            child_modules={},
+            outputs={},
+            full_address="",
+            parent_module=None,
+        )
+        results = _resolve_module_variable(module, "layer")
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].value, "layer.arn")
+
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._get_configuration_address")
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._clean_references_list")
+    def test_resolve_module_variable_nested_variables(self, mock_clean_references, mock_get_configuration_address):
+        references = ["var.layer_name"]
+        mock_clean_references.return_value = references
+        mock_get_configuration_address.return_value = "layer_name"
+        references = References(value=references)
+        constant_value = ConstantValue(value="layer.arn")
+        parent_module = TFModule(
+            variables={"layer_name": constant_value},
+            resources=[],
+            child_modules={},
+            outputs={},
+            full_address="",
+            parent_module=None,
+        )
+        child_module = TFModule(
+            variables={"layer": references},
+            resources=[],
+            child_modules={},
+            outputs={},
+            full_address="",
+            parent_module=parent_module,
+        )
+        results = _resolve_module_variable(child_module, "layer")
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].value, "layer.arn")
+
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._get_configuration_address")
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._clean_references_list")
+    def test_resolve_module_variable_local_variable(self, mock_clean_references, mock_get_configuration_address):
+        references = ["local.layer_arn"]
+        mock_clean_references.return_value = references
+        mock_get_configuration_address.return_value = "layer_arn"
+        local_reference = References(value=references)
+        parent_module = TFModule(
+            variables={},
+            resources=[],
+            child_modules={},
+            outputs={},
+            full_address="full/address",
+            parent_module=None,
+        )
+        module = TFModule(
+            variables={"layer": local_reference},
+            resources=[],
+            child_modules={},
+            outputs={},
+            full_address="full/address",
+            parent_module=parent_module,
+        )
+        results = _resolve_module_variable(module, "layer")
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].value, "local.layer_arn")
+        self.assertEqual(results[0].module_address, "full/address")
+
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._get_configuration_address")
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._resolve_module_output")
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._clean_references_list")
+    def test_resolve_module_variable_nested_modules(
+        self, mock_clean_references, mock_resolve_module_output, mock_get_configuration_address
+    ):
+        references = ["module.layer_module.layer_arn"]
+        mock_resolve_module_output.return_value = [ConstantValue(value="layer_arn")]
+        mock_clean_references.return_value = references
+        mock_get_configuration_address.return_value = "layer_module"
+        references = References(value=references)
+        constant_value = ConstantValue(value="layer_arn")
+        parent_module = TFModule(
+            variables={"layer_name": constant_value},
+            resources=[],
+            child_modules={},
+            outputs={},
+            full_address="",
+            parent_module=None,
+        )
+        child_module = TFModule(
+            variables={"layer": references},
+            resources=[],
+            child_modules={},
+            outputs={},
+            full_address="",
+            parent_module=parent_module,
+        )
+        parent_module.child_modules.update({"layer_module": child_module})
+        results = _resolve_module_variable(child_module, "layer")
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].value, "layer_arn")
+
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._get_configuration_address")
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._resolve_module_output")
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._clean_references_list")
+    def test_resolve_module_variable_combined_nested_modules(
+        self, mock_clean_references, mock_resolve_module_output, mock_get_configuration_address
+    ):
+        references = ["module.layer_module.layer_arn", "var.layer_name_b"]
+        mock_resolve_module_output.return_value = [ConstantValue(value="layer_arn_a")]
+        mock_clean_references.return_value = references
+        mock_get_configuration_address.side_effect = ["layer_module", "layer_name_b"]
+        references = References(value=references)
+        parent_module = TFModule(
+            variables={
+                "layer_name": ConstantValue(value="layer_arn_a"),
+                "layer_name_b": ConstantValue(value="layer_arn_b"),
+            },
+            resources=[],
+            child_modules={},
+            outputs={},
+            full_address="",
+            parent_module=None,
+        )
+        child_module = TFModule(
+            variables={"layer": references, "layer_name_c": ConstantValue(value="layer_arn_c")},
+            resources=[],
+            child_modules={},
+            outputs={},
+            full_address="",
+            parent_module=parent_module,
+        )
+        parent_module.child_modules.update({"layer_module": child_module})
+        results_a = _resolve_module_variable(child_module, "layer")
+        results_b = _resolve_module_variable(child_module, "layer_name_c")
+        self.assertEqual(len(results_a), 2)
+        self.assertEqual(len(results_b), 1)
+        self.assertEqual(
+            results_a[0].value,
+            "layer_arn_a",
+        )
+        self.assertEqual(
+            results_a[1].value,
+            "layer_arn_b",
+        )
+        self.assertEqual(results_b[0].value, "layer_arn_c")
+
+    def test_resolve_module_variable_invalid_variable(self):
+        constant_value = None
+        module = TFModule(
+            variables={"layer": constant_value},
+            resources=[],
+            child_modules={},
+            outputs={},
+            full_address="",
+            parent_module=None,
+        )
+
+        with self.assertRaises(InvalidResourceLinkingException) as ex:
+            _resolve_module_variable(module, "layer")
+
+        self.assertEqual(
+            ex.exception.args[0],
+            "An error occurred when attempting to link two resources: The variable "
+            "layer could not be found in module root module.",
+        )
+
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._get_configuration_address")
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._clean_references_list")
+    def test_resolve_module_variable_enters_invalid_state(self, mock_clean_references, mock_get_configuration_address):
+        references = ["local.layer_arn"]
+        mock_clean_references.return_value = references
+        mock_get_configuration_address.return_value = "layer_arn"
+        local_reference = References(value=references)
+        module = TFModule(
+            variables={"layer": local_reference},
+            resources=[],
+            child_modules={},
+            outputs={},
+            full_address="full/address",
+            parent_module=None,
+        )
+
+        with self.assertRaises(InvalidResourceLinkingException) as ex:
+            _resolve_module_variable(module, "layer")
+
+        self.assertEqual(
+            ex.exception.args[0],
+            "An error occurred when attempting to link two resources: Resource linking entered an invalid state.",
+        )
+
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._get_configuration_address")
+    @patch("samcli.hook_packages.terraform.hooks.prepare.resource_linking._clean_references_list")
+    def test_resolve_module_variable_invalid_module_reference(
+        self, mock_clean_references, mock_get_configuration_address
+    ):
+        references = ["module.layer_module"]
+        mock_clean_references.return_value = references
+        mock_get_configuration_address.return_value = "layer_module"
+        references = References(value=references)
+        child_module = TFModule(
+            variables={"layer": references},
+            resources=[],
+            child_modules={},
+            outputs={},
+            full_address="",
+            parent_module=None,
+        )
+        with self.assertRaises(InvalidResourceLinkingException) as ex:
+            _resolve_module_variable(child_module, "layer")
+
+        self.assertEqual(
+            ex.exception.args[0],
+            "An error occurred when attempting to link two resources: Couldn't find child module layer_module.",
+        )
