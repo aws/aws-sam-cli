2bce1198d54ed516824b78581cec8031070297b8
Chad Cannell <chad@thecannells.org>
2022-10-10 15:06:00 -0600

    Update options.py  help message
    Remove reference to `--s3-guided parameter` as it is not a valid parameter for `sam deploy`

--- samcli/commands/_utils/options.py
+++ samcli/commands/_utils/options.py
@@ -675,8 +675,7 @@ def resolve_s3_click_option(guided):
         callback=callback,
         help="Automatically resolve s3 bucket for non-guided deployments. "
         "Enabling this option will also create a managed default s3 bucket for you. "
-        "If you do not provide a --s3-bucket value, the managed bucket will be used. "
-        "Do not use --s3-guided parameter with this option.",
+        "If you do not provide a --s3-bucket value, the managed bucket will be used. ",
     )
 
 
