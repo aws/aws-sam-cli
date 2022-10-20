4bc7d0df02f48b83e184bcb689aa2dfd9a1d70d1
hnnasit <84355507+hnnasit@users.noreply.github.com>
2022-10-19 18:00:03 -0700

    Changing python3.8 to run from local bin

--- installer/pyinstaller/build-mac.sh
+++ installer/pyinstaller/build-mac.sh
@@ -78,7 +78,7 @@ sudo make install
 cd ..
 
 echo "Installing Python Libraries"
-python3.8 -m venv venv
+/usr/local/bin/python3.8 -m venv venv
 ./venv/bin/pip install --upgrade pip
 ./venv/bin/pip install -r src/requirements/reproducible-linux.txt
 
