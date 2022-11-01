213e29d833602ef173c7c31ddd0ab4147e1c75fd
Mohamed Elasmar <71043312+moelasmar@users.noreply.github.com>
2022-11-01 03:19:02 -0700

    fix the merge

--- tests/iac_integration/cdk/testdata/cdk_v2/java/src/main/java/com/myorg/JavaStack.java
+++ tests/iac_integration/cdk/testdata/cdk_v2/java/src/main/java/com/myorg/JavaStack.java
@@ -129,7 +129,7 @@ public class JavaStack extends Stack {
         cfnLayerVersionNodeJsRuntime.addMetadata("BuildMethod", "nodejs14.x");
 
         NodejsFunction nodejsFunction = NodejsFunction.Builder.create(this, "NodejsFunction")
-                .entry("../../src/nodejs/NodeJsFunctionConstruct/handler.rb")
+                .entry("../../src/nodejs/NodeJsFunctionConstruct/app.ts")
                 .depsLockFilePath("../../src/nodejs/NodeJsFunctionConstruct/package-lock.json")
                 .handler("lambdaHandler")
                 .layers(Arrays.asList(layerVersionNodeJsRuntime))
