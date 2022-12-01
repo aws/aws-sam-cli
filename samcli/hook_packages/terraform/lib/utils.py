3ce9f111b6e80c9c6c829cb55c3ad711addca9b8
Lucas <12496191+lucashuy@users.noreply.github.com>
2022-11-30 16:05:45 -0800

    Split logic in prepare hook and tests

--- samcli/hook_packages/terraform/lib/utils.py
+++ samcli/hook_packages/terraform/lib/utils.py
@@ -1,13 +1,53 @@
 """Terraform utilities"""
 import hashlib
+import re
 from typing import List, Union, Dict, Any
+from subprocess import run, CalledProcessError
 
-from samcli.hook_packages.terraform.hooks.prepare.types import ConstantValue, ResolvedReference
 from samcli.lib.utils.hash import str_checksum
+from samcli.hook_packages.terraform.hooks.prepare.types import (
+    ConstantValue,
+    ResolvedReference,
+)
+from samcli.lib.utils.resources import (
+    AWS_LAMBDA_FUNCTION as CFN_AWS_LAMBDA_FUNCTION,
+    AWS_LAMBDA_LAYERVERSION as CFN_AWS_LAMBDA_LAYER_VERSION,
+)
+from samcli.lib.hook.exceptions import PrepareHookException
 
 # max logical id len is 255
 LOGICAL_ID_HASH_LEN = 8
 LOGICAL_ID_MAX_HUMAN_LEN = 247
+
+SAM_METADATA_DOCKER_TAG_ATTRIBUTE = "docker_tag"
+SAM_METADATA_DOCKER_BUILD_ARGS_ATTRIBUTE = "docker_build_args"
+SAM_METADATA_DOCKER_FILE_ATTRIBUTE = "docker_file"
+SAM_METADATA_RESOURCE_TYPE_ATTRIBUTE = "resource_type"
+SAM_METADATA_RESOURCE_NAME_ATTRIBUTE = "resource_name"
+SAM_METADATA_RESOURCE_TYPE = "null_resource"
+SAM_METADATA_ADDRESS_ATTRIBUTE = "address"
+SAM_METADATA_NAME_PREFIX = "sam_metadata_"
+
+# check for python 3, 3.7 or above
+# regex: search for 'Python', whitespace, '3.', digits 7-9 or 2+ digits, any digit or '.' 0+ times
+PYTHON_VERSION_REGEX = re.compile(r"Python\s*3.([7-9]|\d{2,})[\d.]*")
+
+AWS_PROVIDER_NAME = "registry.terraform.io/hashicorp/aws"
+NULL_RESOURCE_PROVIDER_NAME = "registry.terraform.io/hashicorp/null"
+
+TERRAFORM_METADATA_FILE = "template.json"
+TERRAFORM_BUILD_SCRIPT = "copy_terraform_built_artifacts.py"
+TF_BACKEND_OVERRIDE_FILENAME = "z_samcli_backend_override"
+
+HOOK_METADATA_KEY = "AWS::SAM::Hook"
+TERRAFORM_HOOK_METADATA = {
+    "HookName": "terraform",
+}
+
+CFN_CODE_PROPERTIES = {
+    CFN_AWS_LAMBDA_FUNCTION: "Code",
+    CFN_AWS_LAMBDA_LAYER_VERSION: "Content",
+}
 
 
 def build_cfn_logical_id(tf_address: str) -> str:
@@ -84,3 +124,54 @@ def get_sam_metadata_planned_resource_value_attribute(
     sam_metadata_resource_planned_values: Dict, attr_name: str
 ) -> Any:
     return sam_metadata_resource_planned_values.get("values", {}).get("triggers", {}).get(attr_name)
+
+
+def _get_python_command_name() -> str:
+    """
+    Verify that python is installed and return the name of the python command
+
+    Returns
+    -------
+    str
+        The name of the python command installed
+    """
+    command_names_to_try = ["python3", "py3", "python", "py"]
+    for command_name in command_names_to_try:
+        try:
+            run_result = run([command_name, "--version"], check=True, capture_output=True, text=True)
+        except CalledProcessError:
+            pass
+        except OSError:
+            pass
+        else:
+            # check python version
+            if not PYTHON_VERSION_REGEX.match(run_result.stdout):
+                continue
+            return command_name
+    raise PrepareHookException("Python not found. Please ensure that python 3.7 or above is installed.")
+
+
+def _get_s3_object_hash(
+    bucket: Union[str, List[Union[ConstantValue, ResolvedReference]]],
+    key: Union[str, List[Union[ConstantValue, ResolvedReference]]],
+) -> str:
+    """
+    Creates a hash for an AWS S3 object out of the bucket and key
+
+    Parameters
+    ----------
+    bucket: Union[str, List[Union[ConstantValue, ResolvedReference]]]
+        bucket for the S3 object
+    key: Union[str, List[Union[ConstantValue, ResolvedReference]]]
+        key for the S3 object
+
+    Returns
+    -------
+    str
+        hash for the given bucket and key
+    """
+    md5 = hashlib.md5()
+    md5.update(_calculate_configuration_attribute_value_hash(bucket).encode())
+    md5.update(_calculate_configuration_attribute_value_hash(key).encode())
+    # TODO: Hash version if it exists in addition to key and bucket
+    return md5.hexdigest()
--- tests/unit/hook_packages/terraform/lib/test_utils.py
+++ tests/unit/hook_packages/terraform/lib/test_utils.py
@@ -2,14 +2,26 @@
 from unittest import TestCase
 from unittest.mock import patch, Mock, call
 from parameterized import parameterized
+from subprocess import CalledProcessError
 
 from samcli.hook_packages.terraform.hooks.prepare.types import ConstantValue, ResolvedReference
-from samcli.hook_packages.terraform.lib.utils import build_cfn_logical_id, _calculate_configuration_attribute_value_hash
+from samcli.hook_packages.terraform.lib.utils import (
+    build_cfn_logical_id,
+    _calculate_configuration_attribute_value_hash,
+    _get_python_command_name,
+    _get_s3_object_hash,
+)
+
+from samcli.lib.hook.exceptions import PrepareHookException
 
 
-class TestPrepareHook(TestCase):
+class TestTerraformUtils(TestCase):
     def setUp(self) -> None:
         self.mock_logical_id_hash = "12AB34CD"
+        self.s3_bucket = "bucket"
+        self.s3_bucket_2 = "not a bucket"
+        self.s3_key = "key"
+        self.s3_key_2 = "not a key"
 
     @parameterized.expand(
         [
@@ -85,4 +97,146 @@ class TestPrepareHook(TestCase):
                     "module.m1.aws_lambda_function.arn".encode(),
                 ),
             ]
+        )
+
+    @parameterized.expand(
+        [
+            ([CalledProcessError(-2, "python3 --version"), Mock(stdout="Python 3.8.10")], "py3"),
+            ([Mock(stdout="Python 3.7.12"), CalledProcessError(-2, "py3 --version")], "python3"),
+            ([Mock(stdout="Python 3.7")], "python3"),
+            ([Mock(stdout="Python 3.7.0")], "python3"),
+            ([Mock(stdout="Python 3.7.12")], "python3"),
+            ([Mock(stdout="Python 3.8")], "python3"),
+            ([Mock(stdout="Python 3.8.0")], "python3"),
+            ([Mock(stdout="Python 3.8.12")], "python3"),
+            ([Mock(stdout="Python 3.9")], "python3"),
+            ([Mock(stdout="Python 3.9.0")], "python3"),
+            ([Mock(stdout="Python 3.9.12")], "python3"),
+            ([Mock(stdout="Python 3.10")], "python3"),
+            ([Mock(stdout="Python 3.10.0")], "python3"),
+            ([Mock(stdout="Python 3.10.12")], "python3"),
+            (
+                [
+                    Mock(stdout="Python 3.6.10"),
+                    Mock(stdout="Python 3.0.10"),
+                    Mock(stdout="Python 2.7.10"),
+                    Mock(stdout="Python 3.7.12"),
+                ],
+                "py",
+            ),
+        ]
+    )
+    @patch("samcli.hook_packages.terraform.lib.utils.run")
+    def test_get_python_command_name(self, mock_run_side_effect, expected_python_command, mock_subprocess_run):
+        mock_subprocess_run.side_effect = mock_run_side_effect
+
+        python_command = _get_python_command_name()
+        self.assertEqual(python_command, expected_python_command)
+
+    @parameterized.expand(
+        [
+            (
+                [
+                    CalledProcessError(-2, "python3 --version"),
+                    CalledProcessError(-2, "py3 --version"),
+                    CalledProcessError(-2, "python --version"),
+                    CalledProcessError(-2, "py --version"),
+                ],
+            ),
+            (
+                [
+                    Mock(stdout="Python 3"),
+                    Mock(stdout="Python 3.0"),
+                    Mock(stdout="Python 3.0.10"),
+                    Mock(stdout="Python 3.6"),
+                ],
+            ),
+            (
+                [
+                    Mock(stdout="Python 3.6.10"),
+                    Mock(stdout="Python 2"),
+                    Mock(stdout="Python 2.7"),
+                    Mock(stdout="Python 2.7.10"),
+                ],
+            ),
+            (
+                [
+                    Mock(stdout="Python 4"),
+                    Mock(stdout="Python 4.7"),
+                    Mock(stdout="Python 4.7.10"),
+                    Mock(stdout="Python 4.7.10"),
+                ],
+            ),
+        ]
+    )
+    @patch("samcli.hook_packages.terraform.lib.utils.run")
+    def test_get_python_command_name_python_not_found(self, mock_run_side_effect, mock_subprocess_run):
+        mock_subprocess_run.side_effect = mock_run_side_effect
+
+        expected_error_msg = "Python not found. Please ensure that python 3.7 or above is installed."
+        with self.assertRaises(PrepareHookException, msg=expected_error_msg):
+            _get_python_command_name()
+
+    def test_get_s3_object_hash(self):
+        self.assertEqual(
+            _get_s3_object_hash(self.s3_bucket, self.s3_key), _get_s3_object_hash(self.s3_bucket, self.s3_key)
+        )
+        self.assertEqual(
+            _get_s3_object_hash(
+                [ConstantValue("A"), ResolvedReference("aws_lambda_function.arn", "module.m1")], self.s3_key
+            ),
+            _get_s3_object_hash(
+                [ResolvedReference("aws_lambda_function.arn", "module.m1"), ConstantValue("A")], self.s3_key
+            ),
+        )
+        self.assertEqual(
+            _get_s3_object_hash(
+                self.s3_bucket, [ConstantValue("A"), ResolvedReference("aws_lambda_function.arn", "module.m1")]
+            ),
+            _get_s3_object_hash(
+                self.s3_bucket, [ResolvedReference("aws_lambda_function.arn", "module.m1"), ConstantValue("A")]
+            ),
+        )
+        self.assertEqual(
+            _get_s3_object_hash(
+                [ConstantValue("B"), ResolvedReference("aws_s3_bucket.id", "module.m2")],
+                [ConstantValue("A"), ResolvedReference("aws_lambda_function.arn", "module.m1")],
+            ),
+            _get_s3_object_hash(
+                [ResolvedReference("aws_s3_bucket.id", "module.m2"), ConstantValue("B")],
+                [ResolvedReference("aws_lambda_function.arn", "module.m1"), ConstantValue("A")],
+            ),
+        )
+        self.assertNotEqual(
+            _get_s3_object_hash(
+                [ConstantValue("B"), ConstantValue("C"), ResolvedReference("aws_s3_bucket.id", "module.m2")],
+                [ConstantValue("A"), ResolvedReference("aws_lambda_function.arn", "module.m1")],
+            ),
+            _get_s3_object_hash(
+                [ResolvedReference("aws_s3_bucket.id", "module.m2"), ConstantValue("B")],
+                [ResolvedReference("aws_lambda_function.arn", "module.m1"), ConstantValue("A")],
+            ),
+        )
+        self.assertNotEqual(
+            _get_s3_object_hash([ConstantValue("B"), ResolvedReference("aws_s3_bucket.id", "module.m2")], self.s3_key),
+            _get_s3_object_hash(
+                [ResolvedReference("aws_s3_bucket.id", "module.m2"), ConstantValue("B")], self.s3_key_2
+            ),
+        )
+        self.assertNotEqual(
+            _get_s3_object_hash(
+                self.s3_bucket, [ConstantValue("A"), ResolvedReference("aws_lambda_function.arn", "module.m1")]
+            ),
+            _get_s3_object_hash(
+                self.s3_bucket_2, [ResolvedReference("aws_lambda_function.arn", "module.m1"), ConstantValue("A")]
+            ),
+        )
+        self.assertNotEqual(
+            _get_s3_object_hash(self.s3_bucket, self.s3_key), _get_s3_object_hash(self.s3_bucket_2, self.s3_key_2)
+        )
+        self.assertNotEqual(
+            _get_s3_object_hash(self.s3_bucket, self.s3_key), _get_s3_object_hash(self.s3_bucket_2, self.s3_key)
+        )
+        self.assertNotEqual(
+            _get_s3_object_hash(self.s3_bucket, self.s3_key), _get_s3_object_hash(self.s3_bucket, self.s3_key_2)
         )
