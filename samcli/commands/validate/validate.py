949307d75a244a6517f56fee13c78d19eed10ff5
David Xu <cdavidxu@amazon.com>
2022-12-02 18:58:43 +0000

    Add Click error handling and allow config params

--- samcli/commands/validate/validate.py
+++ samcli/commands/validate/validate.py
@@ -3,14 +3,13 @@ CLI Command for Validating a SAM Template
 """
 import os
 
-import logging
 import boto3
 from botocore.exceptions import NoCredentialsError
 import click
-import cfnlint.core # type: ignore
 
 from samtranslator.translator.arn_generator import NoRegionFound
 
+from samcli.cli.context import Context
 from samcli.cli.main import pass_context, common_options as cli_framework_options, aws_creds_options, print_cmdline_args
 from samcli.commands._utils.cdk_support_decorators import unsupported_command_cdk
 from samcli.commands._utils.options import template_option_without_build
@@ -19,8 +18,6 @@ from samcli.cli.cli_config_file import configuration_option, TomlProvider
 from samcli.lib.utils.version_checker import check_newer_version
 from samcli.commands._utils.click_mutex import ClickMutex
 
-LOGGER = logging.getLogger("cfnlint")
-LOGGER.propagate = False
 
 @click.command("validate", short_help="Validate an AWS SAM template.")
 @configuration_option(provider=TomlProvider(section="parameters"))
@@ -30,24 +27,17 @@ LOGGER.propagate = False
 @click.option(
     "--lint",
     is_flag=True,
-    is_eager=True,
     help="Run linting validation on template through cfn-lint. "
-     "For more information, see: https://github.com/aws-cloudformation/cfn-lint",
+    "For more information, see: https://github.com/aws-cloudformation/cfn-lint",
     cls=ClickMutex,
-    incompatible_params=["config-env", "config-file", "profile", "region"]
+    incompatible_params=["profile"],
 )
 @pass_context
 @track_command
 @check_newer_version
 @print_cmdline_args
 @unsupported_command_cdk(alternative_command="cdk doctor")
-def cli(
-    ctx,
-    template_file,
-    config_file,
-    config_env,
-    lint
-):
+def cli(ctx, template_file, config_file, config_env, lint):
 
     # All logic must be implemented in the ``do_cli`` method. This helps with easy unit testing
 
@@ -90,8 +80,11 @@ def do_cli(ctx, template, lint):
                 "AWS Credentials are required. Please configure your credentials.", wrapped_from=e.__class__.__name__
             ) from e
 
-        click.secho("{} can be transformed to a Cloudformation template. "
-         "Please run \"sam validate --lint -t template.yaml\" for additional validation".format(template), fg="green")
+        click.secho(
+            "{} can be transformed to a Cloudformation template. "
+            'Please run "sam validate --lint -t template.yaml" for additional validation'.format(template),
+            fg="green",
+        )
 
 
 def _read_sam_file(template):
@@ -115,9 +108,15 @@ def _read_sam_file(template):
 
     return sam_template
 
-def _lint(ctx, template):
+
+def _lint(ctx: Context, template: str):
     """
     Parses provided SAM template and maps errors from CloudFormation template back to SAM template.
+
+    Cfn-lint loggers are added to the SAM cli logging hierarchy which at the root logger
+    configures with INFO level logging and a different formatting. This exposes and duplicates
+    some cfn-lint logs that are not typically shown to customers. Explicitly setting the level to
+    WARNING and propagate to be False remediates these issues.
 
     Parameters
     -----------
@@ -128,11 +127,26 @@ def _lint(ctx, template):
 
     """
 
+    import cfnlint.core  # type: ignore
+    import logging
+    from samcli.commands.exceptions import UserException
+    from botocore.utils import validate_region_name
+    from botocore.exceptions import InvalidRegionError
+
+    LOGGER = logging.getLogger("cfnlint")
+    LOGGER.propagate = False
+
     try:
+        validate_region_name(ctx.region)
+
+        lint_args = [template]
         if ctx.debug:
-            (args, filenames, formatter) = cfnlint.core.get_args_filenames([template, "--debug"])
-        else:
-            (args, filenames, formatter) = cfnlint.core.get_args_filenames([template])
+            lint_args.append("--debug")
+        if ctx.region:
+            lint_args.append("--region")
+            lint_args.append(ctx.region)
+
+        (args, filenames, formatter) = cfnlint.core.get_args_filenames(lint_args)
         LOGGER.setLevel(logging.WARNING)
         matches = list(cfnlint.core.get_matches(filenames, args))
         if not matches:
@@ -141,13 +155,20 @@ def _lint(ctx, template):
         matches_output = formatter.print_matches(matches, rules, filenames)
 
         if matches_output:
-            if args.output_file:
-                with open(args.output_file, 'w', encoding='utf-8') as output_file:
-                    output_file.write(matches_output)
-            else:
-                print(matches_output)
+            click.secho(matches_output)
 
-        return cfnlint.core.get_exit_code(matches)
-    except cfnlint.core.CfnLintExitException as e:
-        LOGGER.error(str(e))
-        return e.exit_code
+    except cfnlint.core.InvalidRegionException as e:
+        raise UserException(
+            "AWS Region was not found. Please configure your region through the --region option",
+            wrapped_from=e.__class__.__name__,
+        ) from e
+    except cfnlint.core.CfnLintExitException as lint_error:
+        raise UserException(
+            lint_error,
+            wrapped_from=lint_error.__class__.__name__,
+        ) from lint_error
+    except InvalidRegionError as no_region_found_e:
+        raise UserException(
+            "AWS Region was not found. Please configure your region through the --region option",
+            wrapped_from=no_region_found_e.__class__.__name__,
+        ) from no_region_found_e
