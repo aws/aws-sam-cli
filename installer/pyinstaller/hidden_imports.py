48f13cbeba2a207a49c38295a8634448633c9f9d
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2022-12-01 17:31:21 -0800

    Update pyinstaller script to include modules under samcli directory as hidden imports

--- installer/pyinstaller/hidden_imports.py
+++ installer/pyinstaller/hidden_imports.py
@@ -1,8 +1,38 @@
-from samcli.cli.command import _SAM_CLI_COMMAND_PACKAGES
+import os
+import importlib
+from pathlib import Path
+from typing import List
 
-SAM_CLI_HIDDEN_IMPORTS = _SAM_CLI_COMMAND_PACKAGES + [
-    # terraform hook
-    "samcli.hook_packages.terraform",
+
+def _can_import(module: str) -> bool:
+    """Checks if a given module str is import-able"""
+    try:
+        importlib.import_module(module)
+        return True
+    except ImportError:
+        return False
+
+
+def get_samcli_modules() -> List[str]:
+    """
+    Walks a directory and returns a list of modules
+    """
+    samcli_root = Path(__file__).parent.parent.parent
+    samcli_dir = samcli_root / "samcli"
+    modules = []
+
+    for path, _, files in os.walk(samcli_dir):
+        module = os.path.relpath(path, samcli_root).replace("/", ".")
+        for f in files:
+            if f == "__init__.py" and _can_import(module):
+                modules.append(module)
+            elif f.endswith(".py") and _can_import(module + "." + f[:-3]):
+                modules.append(module + "." + f[:-3])
+
+    return modules
+
+
+SAM_CLI_HIDDEN_IMPORTS = get_samcli_modules() + [
     "cookiecutter.extensions",
     "jinja2_time",
     "text_unidecode",
--- tests/unit/cli/test_pyinstaller_imports.py
+++ tests/unit/cli/test_pyinstaller_imports.py
@@ -1,11 +1,12 @@
-import click
-
+from pathlib import Path
 from unittest import TestCase
+
 from installer.pyinstaller import hidden_imports
 from samcli.cli.command import BaseCommand
+from samcli.lib.hook.hook_wrapper import get_available_hook_packages_ids
 
 
-class TestPyinstallerImportCommands(TestCase):
+class TestPyinstallerHiddenImports(TestCase):
     def setUp(self):
         pass
 
@@ -22,3 +23,37 @@ class TestPyinstallerImportCommands(TestCase):
 
         for name in command_package_names:
             self.assertNotIn(name, hidden_imports.SAM_CLI_HIDDEN_IMPORTS)
+
+    def test_hook_contain_all_samcli_hook_packages(self):
+        hook_package_modules = [f"samcli.hook_packages.{hook_name}" for hook_name in get_available_hook_packages_ids()]
+        for module in hook_package_modules:
+            self.assertIn(module, hidden_imports.SAM_CLI_HIDDEN_IMPORTS)
+
+
+class TestGetSamcliModules(TestCase):
+    SAMCLI_ROOT = Path(__file__).parent.parent.parent.parent / "samcli"
+
+    @classmethod
+    def setUpClass(cls):
+        samcli_root = cls.SAMCLI_ROOT
+        my_test_module = samcli_root / "my_test_module"
+        my_test_module.mkdir(exist_ok=True)
+        (my_test_module / "__init__.py").touch(exist_ok=True)
+        (my_test_module / "my_submodule.py").touch(exist_ok=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        samcli_root = cls.SAMCLI_ROOT
+        my_test_module = samcli_root / "my_test_module"
+        (my_test_module / "__init__.py").unlink()
+        (my_test_module / "my_submodule.py").unlink()
+        my_test_module.rmdir()
+
+    def test_samcli_modules_contain_dir_module(self):
+        self.assertIn("samcli.my_test_module", hidden_imports.get_samcli_modules())
+
+    def test_samcli_modules_contain_file_module(self):
+        self.assertIn("samcli.my_test_module.my_submodule", hidden_imports.get_samcli_modules())
+
+    def test_samcli_modules_not_contain_nonexistant_module(self):
+        self.assertNotIn("my_non_existant_module", hidden_imports.get_samcli_modules())
