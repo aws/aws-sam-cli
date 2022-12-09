5a0a9ac41972359fad612bf7dbbf747b4df4a96a
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2022-12-08 17:50:30 -0800

    add tests and documentation

--- samcli/cli/import_module_proxy.py
+++ samcli/cli/import_module_proxy.py
@@ -14,19 +14,44 @@ LOG = logging.getLogger(__name__)
 _original_import = importlib.import_module
 
 
+# This constant keeps all hidden imports + the 'tests' packages which is dynamically imported during unit tests
+SAFE_IMPORTS = ["tests", *hidden_imports.SAM_CLI_HIDDEN_IMPORTS]
+
+
+class MissingDynamicImportError(ImportError):
+    """
+    Thrown when a dynamic import is used without adding it into hidden imports constant
+    """
+
+
 def _dynamic_import(name, package=None):
-    if name in hidden_imports.SAM_CLI_HIDDEN_IMPORTS:
-        LOG.debug(
-            "Importing a package which was already defined in hidden imports name: %s, package: %s", name, package
-        )
-        return _original_import(name, package)
-    if not name.startswith("samcli."):
-        LOG.debug("Importing a package from 'samcli.' module name: %s, package: %s", name, package)
-        return _original_import(name, package)
+    """
+    Replaces original import_module function and then analyzes all the imports going through this call.
+    If the package is not defined in hidden imports, then it will raise an error
+    """
+    for hidden_import in SAFE_IMPORTS:
+        # An import should either match the exact hidden import string or should start with it following a "." (dot)
+        # For instance if there is a hidden import definition like 'samtranslator', importing 'samtranslator' or
+        # 'samtranslator.sub_module' should succeed. But if there is an import like 'samtranslator_side_module' it
+        # should fail.
+        if name == hidden_import or name.startswith(f"{hidden_import}."):
+            LOG.debug(
+                "Importing a package which was already defined in hidden imports name: %s, package: %s", name, package
+            )
+            return _original_import(name, package)
 
-    LOG.info("Dynamic import name: %s package: %s, which is not defined in hidden imports", name, package)
-    raise ValueError(f"Dynamic import not allowed for {name} {package}")
+    LOG.error(
+        "Dynamic import name: %s package: %s, which is not defined in hidden imports: %s",
+        name,
+        package,
+        hidden_imports.SAM_CLI_HIDDEN_IMPORTS,
+    )
+    raise MissingDynamicImportError(f"Dynamic import not allowed for name: {name} package: {package}")
 
 
-def attach_module_import_proxy():
+def attach_import_module_proxy():
+    """
+    Attaches import_module proxy which will analyze every dynamic import and raise an error if it is not defined in
+    hidden imports configuration
+    """
     importlib.import_module = _dynamic_import
