c374c98bb698beb6fa9aa81a9f30ffc85429f0d7
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2022-12-15 09:48:51 -0800

    add build folder and executable name to macos

--- installer/pyinstaller/build-mac.sh
+++ installer/pyinstaller/build-mac.sh
@@ -21,6 +21,11 @@ else
     exit 1
 fi
 
+if [ "$CI_OVERRIDE" = "1" ]; then
+  build_folder="aws-sam-cli-dev"
+  build_binary_name="sam-dev"
+fi
+
 if [ "$python_library_zip_filename" = "" ]; then
     python_library_zip_filename="python-libraries.zip";
 fi
