312f5f7df151e9958bbda658c013c30d8f319f50
Sriram Madapusi Vasudevan <srirammv@amazon.com>
2023-01-11 09:33:59 -0800

    fix: `test_lint_invalid_template` on windows

--- tests/integration/validate/test_validate_command.py
+++ tests/integration/validate/test_validate_command.py
@@ -151,6 +151,8 @@ class TestValidate(TestCase):
         template_path = test_data_path.joinpath(template_file)
         command_result = run_command(self.command_list(lint=True, template_file=template_path))
         output = command_result.stdout.decode("utf-8")
+        # Remove Windows Line Endings for comparison.
+        output.replace("\r", "")
 
         warning_message = (
             'E0000 Duplicate found "HelloWorldFunction" (line 5)\n'
