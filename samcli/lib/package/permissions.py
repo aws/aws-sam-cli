f7a830c30bbf72ab3c678231d60a26d16922929b
Sriram Madapusi Vasudevan <srirammv@amazon.com>
2023-02-02 13:34:57 -0800

    doc: add clear docstrings to `permissions.py`

--- samcli/lib/package/permissions.py
+++ samcli/lib/package/permissions.py
@@ -6,10 +6,36 @@ import zipfile
 
 
 class WindowsFilePermissionMapper:
+    """
+    Windows specific Permission Mapper onto a zipfile.ZipInfo object to
+    set explicit permissions onto `external_attr` attribute if the given object
+    is of type `file`
+    """
+
     def __init__(self, permissions: int):
+        """
+
+        Parameters
+        ----------
+        permissions: int
+            Base permissions.
+        """
         self.permissions = permissions
 
     def apply(self, zip_info: zipfile.ZipInfo):
+        """
+
+        Parameters
+        ----------
+        zip_info: zipfile.ZipInfo
+            object of zipfile.ZipInfo
+
+        Returns
+        -------
+        zipfile.ZipInfo
+            modified object with `external_attr` set to member permissions.
+
+        """
         if platform.system().lower() != "windows":
             return zip_info
         zip_info.external_attr = self.permissions << 16 if not zip_info.is_dir() else zip_info.external_attr
@@ -17,10 +43,36 @@ class WindowsFilePermissionMapper:
 
 
 class WindowsDirPermissionMapper:
+    """
+    Windows specific Permission Mapper onto a zipfile.ZipInfo object to
+    set explicit permissions onto `external_attr` attribute if the given object
+    is of type `directory`
+    """
+
     def __init__(self, permissions: int):
+        """
+
+        Parameters
+        ----------
+        permissions: int
+            Base permissions.
+        """
         self.permissions = permissions
 
     def apply(self, zip_info: zipfile.ZipInfo):
+        """
+
+        Parameters
+        ----------
+        zip_info: zipfile.ZipInfo
+            object of zipfile.ZipInfo
+
+        Returns
+        -------
+        zipfile.ZipInfo
+            modified object with `external_attr` set to member permissions.
+
+        """
         if platform.system().lower() != "windows":
             return zip_info
         zip_info.external_attr = self.permissions << 16 if zip_info.is_dir() else zip_info.external_attr
@@ -28,20 +80,74 @@ class WindowsDirPermissionMapper:
 
 
 class AdditiveDirPermissionMapper:
+    """
+    Additive Permission Mapper onto a zipfile.ZipInfo object to
+    set additive permissions onto `external_attr` attribute if the given object
+    is of type `directory`
+    """
+
     def __init__(self, permissions: int):
+        """
+
+        Parameters
+        ----------
+        permissions: int
+            Base permissions.
+        """
         self.permissions = permissions
 
     def apply(self, zip_info: zipfile.ZipInfo):
+        """
+
+        Parameters
+        ----------
+        zip_info: zipfile.ZipInfo
+            object of zipfile.ZipInfo
+
+        Returns
+        -------
+        zipfile.ZipInfo
+            modified object with `external_attr` added with member permissions
+            if conditions are satisfied.
+
+        """
         if zip_info.is_dir():
             zip_info.external_attr |= self.permissions << 16
         return zip_info
 
 
 class AdditiveFilePermissionMapper:
+    """
+    Additive Permission Mapper onto a zipfile.ZipInfo object to
+    set additive permissions onto `external_attr` attribute if the given object
+    is of type `file`
+    """
+
     def __init__(self, permissions: int):
+        """
+
+        Parameters
+        ----------
+        permissions: int
+            Base permissions.
+        """
         self.permissions = permissions
 
     def apply(self, zip_info: zipfile.ZipInfo):
+        """
+
+        Parameters
+        ----------
+        zip_info: zipfile.ZipInfo
+            object of zipfile.ZipInfo
+
+        Returns
+        -------
+        zipfile.ZipInfo
+            modified object with `external_attr` added with member permissions
+            if conditions are satisfied.
+
+        """
         if not zip_info.is_dir():
             zip_info.external_attr |= self.permissions << 16
         return zip_info
