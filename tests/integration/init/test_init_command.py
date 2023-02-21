888000f4c8488bf1a26804cb1baa40af5d38705a
Sriram Madapusi Vasudevan <srirammv@amazon.com>
2023-02-20 16:16:52 -0800

    fix: telemetry for Abort
    - Add Abort as an exit code that's no longer classified as 255
    - Add OSError to the catch blocks for `sam init` such that a non
      existent zip location does not cause the cli to crash.
      ```
      sam init --location /path/to/template.zip
      Error: An error occurred while generating this project : [Errno 2] No such file or directory: '/path/to/template.zip'
      ```

--- tests/integration/init/test_init_command.py
+++ tests/integration/init/test_init_command.py
@@ -1,3 +1,6 @@
+import time
+import signal
+
 from click.testing import CliRunner
 
 from samcli.commands.init import cli as init_cmd
@@ -14,6 +17,7 @@ from samcli.lib.utils.packagetype import IMAGE, ZIP
 
 from pathlib import Path
 
+from tests.integration.init.test_init_base import InitIntegBase
 from tests.testing_utils import get_sam_command
 
 TIMEOUT = 300
@@ -940,3 +944,20 @@ class TestInitProducesSamconfigFile(TestCase):
         with open(Path(project_path, "samconfig.toml"), "r") as f:
             text = f.readlines()
         return text
+
+
+class TestInitCommand(InitIntegBase):
+    def test_graceful_exit(self):
+        # Run the Base Command
+        command_list = self.get_command()
+        process_execute = Popen(command_list, stdout=PIPE, stderr=PIPE)
+
+        # Wait for binary to be ready before sending interrupts.
+        time.sleep(self.BINARY_READY_WAIT_TIME)
+
+        # Send SIGINT signal
+        process_execute.send_signal(signal.SIGINT)
+        process_execute.wait()
+        # Process should exit gracefully with an exit code of 1.
+        self.assertEqual(process_execute.returncode, 1)
+        self.assertIn("Aborted!", process_execute.stderr.read().decode("utf-8"))
