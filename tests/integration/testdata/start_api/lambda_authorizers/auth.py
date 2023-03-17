e8331328535a0e8adf32006a70c319eed098424a
Lucas <12496191+lucashuy@users.noreply.github.com>
2023-03-17 11:18:31 -0700

    feat: Integration testing of local Lambda authorizers in serverless properties (#4872)
    * Added initial invocation logic for Lambda authorizer
    
    * Added response validation
    
    * Removed unused method
    
    * Added tests
    
    * Updated principalId get logic
    
    * Changed typing to be correct
    
    * Addressed comments
    
    * Format
    
    * Moved lambda auth invocation to its own method
    
    * Addressed comments
    
    * Initial integration testing setup and work
    
    * Change test class name and add comment to make it more clear that this is a bad test case
    
    * make black

---
+++ tests/integration/testdata/start_api/lambda_authorizers/auth.py
@@ -0,0 +1,12 @@
+# https://github.com/awslabs/aws-apigateway-lambda-authorizer-blueprints/blob/master/blueprints/python/api-gateway-authorizer-python.py
+
+"""
+Copyright 2015-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+     http://aws.amazon.com/apache2.0/
+
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+"""
+from __future__ import print_function
