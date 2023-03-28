b8efd164721b41b02851e9b27cd2a4d0def9eed0
Lucas <12496191+lucashuy@users.noreply.github.com>
2023-03-28 15:05:27 -0700

    Added template validation for swagger

--- tests/integration/local/start_api/lambda_authorizers/test_swagger_authorizer_definitions.py
+++ tests/integration/local/start_api/lambda_authorizers/test_swagger_authorizer_definitions.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 from tests.integration.local.start_api.start_api_integ_base import (
     StartApiIntegBaseClass,
+    WritableStartApiIntegBaseClass,
 )
 from parameterized import parameterized_class
 
@@ -10,7 +11,11 @@ from parameterized import parameterized_class
     ("template_path", "endpoint", "parameter_overrides"),
     [
         ("/testdata/start_api/lambda_authorizers/swagger-api.yaml", "requestauthorizerswaggertoken", {}),
-        ("/testdata/start_api/lambda_authorizers/swagger-api.yaml", "requestauthorizerswaggertoken", {"ValidationString": "^myheader$"}),
+        (
+            "/testdata/start_api/lambda_authorizers/swagger-api.yaml",
+            "requestauthorizerswaggertoken",
+            {"ValidationString": "^myheader$"},
+        ),
         ("/testdata/start_api/lambda_authorizers/swagger-api.yaml", "requestauthorizerswaggerrequest", {}),
         ("/testdata/start_api/lambda_authorizers/swagger-api.yaml", "requestauthorizeropenapi", {}),
         ("/testdata/start_api/lambda_authorizers/swagger-http.yaml", "requestauthorizer", {}),
@@ -122,3 +127,204 @@ class TestSwaggerLambdaAuthorizer500(StartApiIntegBaseClass):
 
         self.assertEqual(response.status_code, 502)
         self.assertEqual(response_json, {"message": "Internal server error"})
+
+
+class TestInvalidSwaggerTemplateUsingUnsupportedType(WritableStartApiIntegBaseClass):
+    """
+    Test using an invalid Lambda authorizer type
+    """
+
+    do_collect_cmd_init_output = True
+
+    template_content = """AWSTemplateFormatVersion: '2010-09-09'
+Transform: AWS::Serverless-2016-10-31
+
+Resources:
+  HttpApiOpenApi:
+    Type: AWS::ApiGatewayV2::Api
+    Properties:
+      Body:
+        openapi: "3.0"
+        info:
+          title: HttpApiOpenApi
+        components:
+          securitySchemes:
+            Authorizer:
+              type: apiKey
+              in: header
+              name: notused
+              "x-amazon-apigateway-authorizer":
+                authorizerPayloadFormatVersion: "2.0"
+                type: "bad type"
+                identitySource: "$request.header.header, $request.querystring.query"
+                authorizerUri: arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:123456789012:function:AuthorizerFunction/invocations
+"""
+
+    @pytest.mark.flaky(reruns=3)
+    @pytest.mark.timeout(timeout=10, method="thread")
+    def test_invalid_template(self):
+        self.assertIn(
+            "Lambda authorizer 'Authorizer' type 'bad type' is unsupported, skipping",
+            self.start_api_process_output,
+        )
+
+
+class TestInvalidSwaggerTemplateUsingSimpleResponseWithPayloadV1(WritableStartApiIntegBaseClass):
+    """
+    Test using simple response with wrong payload version
+    """
+
+    do_collect_cmd_init_output = True
+
+    template_content = """AWSTemplateFormatVersion: '2010-09-09'
+Transform: AWS::Serverless-2016-10-31
+
+Resources:
+  HttpApiOpenApi:
+    Type: AWS::ApiGatewayV2::Api
+    Properties:
+      Body:
+        openapi: "3.0"
+        info:
+          title: HttpApiOpenApi
+        components:
+          securitySchemes:
+            Authorizer:
+              type: apiKey
+              in: header
+              name: notused
+              "x-amazon-apigateway-authorizer":
+                authorizerPayloadFormatVersion: "1.0"
+                type: "request"
+                enableSimpleResponses: True
+                identitySource: "$request.header.header, $request.querystring.query"
+                authorizerUri: arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:123456789012:function:AuthorizerFunction/invocations
+"""
+
+    @pytest.mark.flaky(reruns=3)
+    @pytest.mark.timeout(timeout=10, method="thread")
+    def test_invalid_template(self):
+        self.assertIn(
+            "Simple responses are only available on HTTP APIs with "
+            "payload version 2.0, ignoring for Lambda authorizer 'Authorizer'",
+            self.start_api_process_output,
+        )
+
+
+class TestInvalidSwaggerTemplateUsingUnsupportedPayloadVersion(WritableStartApiIntegBaseClass):
+    """
+    Test using an incorrect payload format version
+    """
+
+    do_collect_cmd_init_output = True
+
+    template_content = """AWSTemplateFormatVersion: '2010-09-09'
+Transform: AWS::Serverless-2016-10-31
+
+Resources:
+  HttpApiOpenApi:
+    Type: AWS::ApiGatewayV2::Api
+    Properties:
+      Body:
+        openapi: "3.0"
+        info:
+          title: HttpApiOpenApi
+        components:
+          securitySchemes:
+            Authorizer:
+              type: apiKey
+              in: header
+              name: notused
+              "x-amazon-apigateway-authorizer":
+                authorizerPayloadFormatVersion: "1.2.3"
+                type: "request"
+                identitySource: "$request.header.header, $request.querystring.query"
+                authorizerUri: arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:123456789012:function:AuthorizerFunction/invocations
+"""
+
+    @pytest.mark.flaky(reruns=3)
+    @pytest.mark.timeout(timeout=10, method="thread")
+    def test_invalid_template(self):
+        self.assertIn(
+            "Error: Authorizer 'Authorizer' contains an invalid payload version",
+            self.start_api_process_output,
+        )
+
+class TestInvalidSwaggerTemplateUsingInvalidIdentitySources(WritableStartApiIntegBaseClass):
+    """
+    Test using an invalid identity source (a.b.c.d.e)
+    """
+
+    do_collect_cmd_init_output = True
+
+    template_content = """AWSTemplateFormatVersion: '2010-09-09'
+Transform: AWS::Serverless-2016-10-31
+
+Resources:
+  HttpApiOpenApi:
+    Type: AWS::ApiGatewayV2::Api
+    Properties:
+      Body:
+        openapi: "3.0"
+        info:
+          title: HttpApiOpenApi
+        components:
+          securitySchemes:
+            Authorizer:
+              type: apiKey
+              in: header
+              name: notused
+              "x-amazon-apigateway-authorizer":
+                authorizerPayloadFormatVersion: "2.0"
+                type: "request"
+                identitySource: "a.b.c.d.e"
+                authorizerUri: arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:123456789012:function:AuthorizerFunction/invocations
+"""
+
+    @pytest.mark.flaky(reruns=3)
+    @pytest.mark.timeout(timeout=10, method="thread")
+    def test_invalid_template(self):
+        self.assertIn(
+            "Error: Identity source 'a.b.c.d.e' for Lambda Authorizer "
+            "'Authorizer' is not a valid identity source, check the spelling/format.",
+            self.start_api_process_output,
+        )
+
+class TestInvalidSwaggerTemplateUsingTokenWithHttpApi(WritableStartApiIntegBaseClass):
+    """
+    Test using token authorizer with HTTP API
+    """
+
+    do_collect_cmd_init_output = True
+
+    template_content = """AWSTemplateFormatVersion: '2010-09-09'
+Transform: AWS::Serverless-2016-10-31
+
+Resources:
+  HttpApiOpenApi:
+    Type: AWS::ApiGatewayV2::Api
+    Properties:
+      Body:
+        openapi: "3.0"
+        info:
+          title: HttpApiOpenApi
+        components:
+          securitySchemes:
+            Authorizer:
+              type: apiKey
+              in: header
+              name: notused
+              "x-amazon-apigateway-authorizer":
+                authorizerPayloadFormatVersion: "2.0"
+                type: "token"
+                identitySource: "$request.header.header"
+                authorizerUri: arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:123456789012:function:AuthorizerFunction/invocations
+"""
+
+    @pytest.mark.flaky(reruns=3)
+    @pytest.mark.timeout(timeout=10, method="thread")
+    def test_invalid_template(self):
+        self.assertIn(
+            "Type 'token' for Lambda Authorizer 'Authorizer' is unsupported",
+            self.start_api_process_output,
+        )
