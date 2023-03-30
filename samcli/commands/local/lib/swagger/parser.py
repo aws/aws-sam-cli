41b36b54a9b481b1477f4899f90022d8a7d2fe5d
Lucas <12496191+lucashuy@users.noreply.github.com>
2023-03-30 15:46:45 -0700

    fix: Added missing checks for Swagger parsing of Lambda authorizers and fixed some existing ones (#4938)
    * Added missing check for simple responses and fixed validation string check
    
    * Added missing check for simple responses
    
    * Added checks for payload version
    
    * make format
    
    * Addressed comments

--- samcli/commands/local/lib/swagger/parser.py
+++ samcli/commands/local/lib/swagger/parser.py
@@ -40,6 +40,7 @@ class SwaggerParser:
     _AUTHORIZER_NAME = "name"
     _AUTHORIZER_IN = "in"
     _AUTHORIZER_IDENTITY_SOURCE = "identitySource"
+    _AUTHORIZER_SIMPLE_RESPONSES = "enableSimpleResponses"
 
     def __init__(self, stack_path: str, swagger):
         """
@@ -103,9 +104,13 @@ class SwaggerParser:
                 continue
 
             authorizer_type = authorizer_object.get(SwaggerParser._AUTHORIZER_TYPE, "").lower()
-            payload_version = authorizer_object.get(
-                SwaggerParser._AUTHORIZER_PAYLOAD_VERSION, LambdaAuthorizer.PAYLOAD_V1
-            )
+            payload_version = authorizer_object.get(SwaggerParser._AUTHORIZER_PAYLOAD_VERSION)
+
+            if event_type == Route.HTTP and payload_version not in LambdaAuthorizer.PAYLOAD_VERSIONS:
+                raise InvalidSecurityDefinition(f"Authorizer '{auth_name}' contains an invalid payload version")
+
+            if event_type == Route.API:
+                payload_version = LambdaAuthorizer.PAYLOAD_V1
 
             lambda_name = LambdaUri.get_function_name(authorizer_object.get(SwaggerParser._AUTHORIZER_LAMBDA_URI))
 
@@ -123,11 +128,21 @@ class SwaggerParser:
             )
 
             validation_expression = authorizer_object.get(SwaggerParser._AUTHORIZER_LAMBDA_VALIDATION)
-            if event_type != Route.HTTP:
+            if event_type == Route.HTTP and validation_expression:
                 validation_expression = None
 
                 LOG.warning(
-                    "Validation expressions is only available on Rest APIs, ignoring for Lambda authorizer '%s'",
+                    "Validation expressions is only available on REST APIs, ignoring for Lambda authorizer '%s'",
+                    auth_name,
+                )
+
+            enable_simple_response = authorizer_object.get(SwaggerParser._AUTHORIZER_SIMPLE_RESPONSES, False)
+            if event_type != Route.HTTP or payload_version != LambdaAuthorizer.PAYLOAD_V2 and enable_simple_response:
+                enable_simple_response = False
+
+                LOG.warning(
+                    "Simple responses are only available on HTTP APIs with payload version "
+                    "2.0, ignoring for Lambda authorizer '%s'",
                     auth_name,
                 )
 
@@ -145,6 +160,7 @@ class SwaggerParser:
                 lambda_name=lambda_name,
                 identity_sources=identity_sources,
                 validation_string=validation_expression,
+                use_simple_response=enable_simple_response,
             )
 
             authorizers[auth_name] = lambda_authorizer
@@ -257,7 +273,7 @@ class SwaggerParser:
             # make sure that authorizer actually has keys
             if len(authorizer_object) != 1:
                 raise InvalidSecurityDefinition(
-                    "Invalid default security definition found, there must " "be an authorizer defined."
+                    "Invalid default security definition found, there must be an authorizer defined."
                 )
 
             authorizer_name = str(authorizer_object[0])
