7b3f8d745e132029c789af31748c9c5ed165a95e
SleepWithCoffee <duytran.developer@gmail.com>
2023-03-30 21:40:56 +0800

    add unit tests for is_ecr() function

--- samcli/lib/package/regexpr.py
+++ samcli/lib/package/regexpr.py
@@ -2,10 +2,10 @@
 Regular Expressions for Resources.
 """
 # Regex derived from https://docs.aws.amazon.com/AmazonECR/latest/APIReference/API_Repository.html
-HOSTNAME_ECR_AWS = r"([a-zA-Z0-9][a-zA-Z0-9-_]*)\.dkr\.ecr\.([a-zA-Z0-9][a-zA-Z0-9-_]*)\.amazonaws\.com(\.cn)?"
-HOSTNAME_LOCALHOST = r"localhost(:\d{1,5})?"
-HOSTNAME_127_0_0_1 = r"127\.0\.0\.1(:\d{1,5})?"
+HOSTNAME_ECR_AWS = r"(?:[a-zA-Z0-9][\w-]*)\.dkr\.ecr\.(?:[a-zA-Z0-9][\w-]*)\.amazonaws\.com(\.cn)?"
+HOSTNAME_LOCALHOST = r"localhost(?::\d{1,5})?"
+HOSTNAME_127_0_0_1 = r"127\.0\.0\.1(?::\d{1,5})?"
 ECR_URL = (
-    rf"^({HOSTNAME_ECR_AWS}|{HOSTNAME_LOCALHOST}|{HOSTNAME_127_0_0_1})\/"
+    rf"^(?:{HOSTNAME_ECR_AWS}|{HOSTNAME_LOCALHOST}|{HOSTNAME_127_0_0_1})\/"
     r"(?:[a-z0-9]+(?:[._-][a-z0-9]+)*\/)*[a-z0-9]+(?:[._-][a-z0-9]+)*"
 )
