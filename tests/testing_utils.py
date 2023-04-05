0928ed310f5ce89a2815cf1088e1dc2711adb4a9
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2023-04-04 17:17:29 -0700

    update app insights template modifier to comply with cfn-lint rules

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -56,6 +56,9 @@ def method_to_stack_name(method_name):
 
 def run_command(command_list, cwd=None, env=None, timeout=TIMEOUT) -> CommandResult:
     LOG.info("Running command: %s", " ".join(command_list))
+    LOG.info("cwd:             %s", cwd)
+    LOG.info("env:             %s", env)
+    LOG.info("timeout:         %s", timeout)
     process_execute = Popen(command_list, cwd=cwd, env=env, stdout=PIPE, stderr=PIPE)
     try:
         stdout_data, stderr_data = process_execute.communicate(timeout=timeout)
