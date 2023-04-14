f92a3a452a53670194d798ad34625a9ac98eafb8
Sriram Madapusi Vasudevan <srirammv@amazon.com>
2023-04-14 16:40:46 -0700

    feat: help text for all local suite.

---
+++ samcli/commands/local/start_api/core/options.py
@@ -0,0 +1,80 @@
+"""
+Invoke Start API Command Options related Datastructures for formatting.
+"""
+from typing import Dict, List
+
+from samcli.cli.row_modifiers import RowDefinition
+
+# NOTE(sriram-mv): The ordering of the option lists matter, they are the order
+# in which options will be displayed.
+
+REQUIRED_OPTIONS: List[str] = ["template_file"]
+
+AWS_CREDENTIAL_OPTION_NAMES: List[str] = ["region", "profile"]
+
+TEMPLATE_OPTIONS: List[str] = [
+    "parameter_overrides",
+]
+
+CONTAINER_OPTION_NAMES: List[str] = [
+    "event",
+    "host",
+    "port",
+    "env_vars",
+    "debug_port",
+    "debugger_path",
+    "debug_function",
+    "docker_volume_basedir",
+    "skip_pull_image",
+    "docker_network",
+    "force_image_build",
+    "warm_containers",
+    "debug_function",
+    "shutdown",
+    "container_host",
+    "container_host_interface",
+    "invoke_image",
+]
+
+CONFIGURATION_OPTION_NAMES: List[str] = ["config_env", "config_file"]
+
+ARTIFACT_LOCATION_OPTIONS: List[str] = [
+    "log_file",
+    "layer_cache_basedir",
+    "static_dir",
+]
+
+OTHER_OPTIONS: List[str] = ["debug", "help"]
+
+ALL_OPTIONS: List[str] = (
+    REQUIRED_OPTIONS
+    + TEMPLATE_OPTIONS
+    + AWS_CREDENTIAL_OPTION_NAMES
+    + CONTAINER_OPTION_NAMES
+    + ARTIFACT_LOCATION_OPTIONS
+    + CONFIGURATION_OPTION_NAMES
+    + OTHER_OPTIONS
+)
+
+OPTIONS_INFO: Dict[str, Dict] = {
+    "Required Options": {"option_names": {opt: {"rank": idx} for idx, opt in enumerate(REQUIRED_OPTIONS)}},
+    "Template Options": {"option_names": {opt: {"rank": idx} for idx, opt in enumerate(TEMPLATE_OPTIONS)}},
+    "AWS Credential Options": {
+        "option_names": {opt: {"rank": idx} for idx, opt in enumerate(AWS_CREDENTIAL_OPTION_NAMES)}
+    },
+    "Container Options": {"option_names": {opt: {"rank": idx} for idx, opt in enumerate(CONTAINER_OPTION_NAMES)}},
+    "Artifact Location Options": {
+        "option_names": {opt: {"rank": idx} for idx, opt in enumerate(ARTIFACT_LOCATION_OPTIONS)}
+    },
+    "Configuration Options": {
+        "option_names": {opt: {"rank": idx} for idx, opt in enumerate(CONFIGURATION_OPTION_NAMES)},
+        "extras": [
+            RowDefinition(name="Learn more about configuration files at:"),
+            RowDefinition(
+                name="https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli"
+                "-config.html. "
+            ),
+        ],
+    },
+    "Other Options": {"option_names": {opt: {"rank": idx} for idx, opt in enumerate(OTHER_OPTIONS)}},
+}
---
+++ samcli/commands/validate/core/options.py
@@ -0,0 +1,44 @@
+"""
+Validate Command Options related Datastructures for formatting.
+"""
+from typing import Dict, List
+
+from samcli.cli.row_modifiers import RowDefinition
+
+# NOTE(sriram-mv): The ordering of the option lists matter, they are the order
+# in which options will be displayed.
+
+REQUIRED_OPTIONS: List[str] = ["template_file"]
+
+AWS_CREDENTIAL_OPTION_NAMES: List[str] = ["region", "profile"]
+
+LINT_OPTION_NAMES: List[str] = [
+    "lint",
+]
+
+CONFIGURATION_OPTION_NAMES: List[str] = ["config_env", "config_file"]
+
+OTHER_OPTIONS: List[str] = ["debug", "help"]
+
+ALL_OPTIONS: List[str] = (
+    REQUIRED_OPTIONS + LINT_OPTION_NAMES + AWS_CREDENTIAL_OPTION_NAMES + CONFIGURATION_OPTION_NAMES + OTHER_OPTIONS
+)
+
+OPTIONS_INFO: Dict[str, Dict] = {
+    "Required Options": {"option_names": {opt: {"rank": idx} for idx, opt in enumerate(REQUIRED_OPTIONS)}},
+    "Lint Options": {"option_names": {opt: {"rank": idx} for idx, opt in enumerate(LINT_OPTION_NAMES)}},
+    "AWS Credential Options": {
+        "option_names": {opt: {"rank": idx} for idx, opt in enumerate(AWS_CREDENTIAL_OPTION_NAMES)}
+    },
+    "Configuration Options": {
+        "option_names": {opt: {"rank": idx} for idx, opt in enumerate(CONFIGURATION_OPTION_NAMES)},
+        "extras": [
+            RowDefinition(name="Learn more about configuration files at:"),
+            RowDefinition(
+                name="https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli"
+                "-config.html. "
+            ),
+        ],
+    },
+    "Other Options": {"option_names": {opt: {"rank": idx} for idx, opt in enumerate(OTHER_OPTIONS)}},
+}
