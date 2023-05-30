076e4af825f1a411f338b212611fd4d4ab7f31ce
Sriram Madapusi Vasudevan <srirammv@amazon.com>
2023-05-30 13:19:31 -0700

    fix: add 3.11 to classifiers
    - update dependencies, need to nail down the versions.

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Internet",
         "Topic :: Software Development :: Build Tools",
         "Topic :: Utilities",
