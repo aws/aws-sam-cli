6a25c54973d2f573f9db728e8b562224324beb9d
hnnasit <84355507+hnnasit@users.noreply.github.com>
2023-06-05 11:20:50 -0700

    feat: Add click command for cloud invoke command (#5238)
    * Add custom click option for cloud invoke called parameter
    
    * Added more error handling to executors and updated output-format enum to use auto
    
    * Add new CLI command for cloud invoke
    
    * Update samcli/commands/remote_invoke/invoke/cli.py
    
    Co-authored-by: Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
    
    * Update samcli/commands/remote_invoke/invoke/cli.py
    
    Co-authored-by: Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
    
    * Update samcli/commands/remote_invoke/cloud.py
    
    Co-authored-by: Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
    
    * Update samcli/cli/types.py
    
    Co-authored-by: Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
    
    * Address feedback
    
    * Moved all command options to be handled by click configuration
    
    * Updated validation function doc-string
    
    * Updated debug logs in types.py
    
    * Changed remote_invoke dir to cloud and updated log level for validation
    
    * Address feedback
    
    ---------
    
    Co-authored-by: Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>

--- tests/unit/commands/cloud/test_remote_invoke_context.py
+++ tests/unit/commands/cloud/test_remote_invoke_context.py
@@ -2,14 +2,14 @@ from unittest import TestCase
 from unittest.mock import Mock, patch
 from uuid import uuid4
 
-from samcli.commands.remote_invoke.exceptions import (
+from samcli.commands.cloud.exceptions import (
     InvalidRemoteInvokeParameters,
     AmbiguousResourceForRemoteInvoke,
     NoResourceFoundForRemoteInvoke,
     UnsupportedServiceForRemoteInvoke,
     NoExecutorFoundForRemoteInvoke,
 )
-from samcli.commands.remote_invoke.remote_invoke_context import RemoteInvokeContext, SUPPORTED_SERVICES
+from samcli.commands.cloud.remote_invoke_context import RemoteInvokeContext, SUPPORTED_SERVICES
 from samcli.lib.utils.cloudformation import CloudFormationResourceSummary
 
 
@@ -32,7 +32,7 @@ class TestRemoteInvokeContext(TestCase):
             with self._get_remote_invoke_context():
                 pass
 
-    @patch("samcli.commands.remote_invoke.remote_invoke_context.get_resource_summaries")
+    @patch("samcli.commands.cloud.remote_invoke_context.get_resource_summaries")
     def test_only_stack_name_with_no_resource_should_fail(self, patched_resource_summaries):
         self.resource_id = None
         patched_resource_summaries.return_value = {}
@@ -40,7 +40,7 @@ class TestRemoteInvokeContext(TestCase):
             with self._get_remote_invoke_context():
                 pass
 
-    @patch("samcli.commands.remote_invoke.remote_invoke_context.get_resource_summaries")
+    @patch("samcli.commands.cloud.remote_invoke_context.get_resource_summaries")
     def test_only_stack_name_with_multiple_resource_should_fail(self, patched_resource_summaries):
         self.resource_id = None
         patched_resource_summaries.return_value = {"resource1": Mock(), "resource2": Mock()}
@@ -48,7 +48,7 @@ class TestRemoteInvokeContext(TestCase):
             with self._get_remote_invoke_context():
                 pass
 
-    @patch("samcli.commands.remote_invoke.remote_invoke_context.get_resource_summaries")
+    @patch("samcli.commands.cloud.remote_invoke_context.get_resource_summaries")
     def test_only_stack_name_with_single_resource_should_be_valid(self, patched_resource_summaries):
         self.resource_id = None
         resource_summary = Mock(logical_resource_id=self.resource_id)
@@ -75,7 +75,7 @@ class TestRemoteInvokeContext(TestCase):
                 ),
             )
 
-    @patch("samcli.commands.remote_invoke.remote_invoke_context.get_resource_summary_from_physical_id")
+    @patch("samcli.commands.cloud.remote_invoke_context.get_resource_summary_from_physical_id")
     def test_only_resource_id_as_invalid_physical_id_should_fail(self, patched_resource_summary_from_physical_id):
         self.stack_name = None
         patched_resource_summary_from_physical_id.return_value = None
@@ -83,14 +83,14 @@ class TestRemoteInvokeContext(TestCase):
             with self._get_remote_invoke_context():
                 pass
 
-    @patch("samcli.commands.remote_invoke.remote_invoke_context.get_resource_summary")
+    @patch("samcli.commands.cloud.remote_invoke_context.get_resource_summary")
     def test_if_no_resource_found_with_given_stack_and_resource_id_should_fail(self, patched_get_resource_summary):
         patched_get_resource_summary.return_value = None
         with self.assertRaises(AmbiguousResourceForRemoteInvoke):
             with self._get_remote_invoke_context() as remote_invoke_context:
                 remote_invoke_context.run(Mock())
 
-    @patch("samcli.commands.remote_invoke.remote_invoke_context.get_resource_summary_from_physical_id")
+    @patch("samcli.commands.cloud.remote_invoke_context.get_resource_summary_from_physical_id")
     def test_only_resource_id_as_valid_physical_id_should_be_valid(self, patched_resource_summary_from_physical_id):
         self.stack_name = None
         resource_summary = Mock()
@@ -98,22 +98,22 @@ class TestRemoteInvokeContext(TestCase):
         with self._get_remote_invoke_context() as remote_invoke_context:
             self.assertEqual(remote_invoke_context._resource_summary, resource_summary)
 
-    @patch("samcli.commands.remote_invoke.remote_invoke_context.get_resource_summary")
+    @patch("samcli.commands.cloud.remote_invoke_context.get_resource_summary")
     def test_running_without_resource_summary_should_raise_exception(self, patched_get_resource_summary):
         patched_get_resource_summary.return_value = None
         with self._get_remote_invoke_context() as remote_invoke_context:
             with self.assertRaises(AmbiguousResourceForRemoteInvoke):
                 remote_invoke_context.run(Mock())
 
-    @patch("samcli.commands.remote_invoke.remote_invoke_context.get_resource_summary")
+    @patch("samcli.commands.cloud.remote_invoke_context.get_resource_summary")
     def test_running_with_unsupported_resource_should_raise_exception(self, patched_get_resource_summary):
         patched_get_resource_summary.return_value = Mock(resource_type="UnSupportedResource")
         with self._get_remote_invoke_context() as remote_invoke_context:
             with self.assertRaises(NoExecutorFoundForRemoteInvoke):
                 remote_invoke_context.run(Mock())
 
-    @patch("samcli.commands.remote_invoke.remote_invoke_context.RemoteInvokeExecutorFactory")
-    @patch("samcli.commands.remote_invoke.remote_invoke_context.get_resource_summary")
+    @patch("samcli.commands.cloud.remote_invoke_context.RemoteInvokeExecutorFactory")
+    @patch("samcli.commands.cloud.remote_invoke_context.get_resource_summary")
     def test_running_should_execute_remote_invoke_executor_instance(
         self, patched_get_resource_summary, patched_remote_invoke_executor_factory
     ):
