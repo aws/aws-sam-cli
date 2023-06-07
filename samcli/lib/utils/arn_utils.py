d8473abe5c2fab8464487b1e4332375241c1d1c8
Haresh Nasit <hnnasit@amazon.com>
2023-06-07 16:47:37 -0700

    Address feedback

--- samcli/lib/utils/arn_utils.py
+++ samcli/lib/utils/arn_utils.py
@@ -36,43 +36,39 @@ class ARNParts:
     resource_id: str
 
     def __init__(self, arn: str) -> None:
-        try:
-            # Regex pattern formed based on the 3 ARN general formats found here:
-            # https://docs.aws.amazon.com/IAM/latest/UserGuide/reference-arns.html
-            arn_pattern = (
-                r"arn:([a-zA-Z0-9_-]+):"  # Pattern for partition
-                r"([a-zA-Z0-9_-]+):"  # Pattern for service
-                r"([a-zA-Z0-9_-]*):"  # Pattern for region
-                r"([a-zA-Z0-9_-]*)"  # Pattern for account_id
-                r"(?::([a-zA-Z0-9_-]+))?"  # Pattern for resource_type if it exists
-                r"(?::(.+))"  # Pattern for resource_id if it exists
-            )
-            print(arn_pattern)
-            matched_arn = re.match(arn_pattern, arn)
-            if not matched_arn:
-                raise ValueError()
+        # Regex pattern formed based on the 3 ARN general formats found here:
+        # https://docs.aws.amazon.com/IAM/latest/UserGuide/reference-arns.html
+        arn_pattern = (
+            r"arn:([a-zA-Z0-9_-]+):"  # Pattern for partition
+            r"([a-zA-Z0-9_-]+):"  # Pattern for service
+            r"([a-zA-Z0-9_-]*):"  # Pattern for region
+            r"([a-zA-Z0-9_-]*)"  # Pattern for account_id
+            r"(?::([a-zA-Z0-9_-]+))?"  # Pattern for resource_type if it exists
+            r"(?::(.+))"  # Pattern for resource_id if it exists
+        )
 
-            self.partition = matched_arn.group(1)
-            self.service = matched_arn.group(2)
-            self.region = matched_arn.group(3) if matched_arn.group(3) else ""
-            self.account_id = matched_arn.group(4) if matched_arn.group(4) else ""
-            self.resource_type = matched_arn.group(5) if matched_arn.group(5) else ""
-            if matched_arn.group(5):
-                # This handles the Arns of services with the format:
-                # arn:partition:service:region:account-id:resource-type:resource-id
-                self.resource_type = matched_arn.group(5)
-                self.resource_id = matched_arn.group(6) if matched_arn.group(6) else ""
-            elif "/" in matched_arn.group(6):
-                # This handles the Arns of services with the format:
-                # arn:partition:service:region:account-id:resource-type/resource-id
-                split_resource_type_and_id = matched_arn.group(6).split("/", 1)
-                self.resource_type = split_resource_type_and_id[0]
-                self.resource_id = split_resource_type_and_id[1]
-            else:
-                # This handles the Arns of services with the format:
-                # arn:partition:service:region:account-id:resource-id
-                self.resource_type = ""
-                self.resource_id = matched_arn.group(6) if matched_arn.group(6) else ""
+        matched_arn = re.match(arn_pattern, arn)
+        if not matched_arn:
+            raise InvalidArnValue(f"Invalid ARN ({arn}) provided")
 
-        except ValueError as ex:
-            raise InvalidArnValue(f"Invalid ARN ({arn})") from ex
+        self.partition = matched_arn.group(1)
+        self.service = matched_arn.group(2)
+        self.region = matched_arn.group(3) if matched_arn.group(3) else ""
+        self.account_id = matched_arn.group(4) if matched_arn.group(4) else ""
+        self.resource_type = matched_arn.group(5) if matched_arn.group(5) else ""
+        if matched_arn.group(5):
+            # This handles the Arns of services with the format:
+            # arn:partition:service:region:account-id:resource-type:resource-id
+            self.resource_type = matched_arn.group(5)
+            self.resource_id = matched_arn.group(6) if matched_arn.group(6) else ""
+        elif "/" in matched_arn.group(6):
+            # This handles the Arns of services with the format:
+            # arn:partition:service:region:account-id:resource-type/resource-id
+            split_resource_type_and_id = matched_arn.group(6).split("/", 1)
+            self.resource_type = split_resource_type_and_id[0]
+            self.resource_id = split_resource_type_and_id[1]
+        else:
+            # This handles the Arns of services with the format:
+            # arn:partition:service:region:account-id:resource-id
+            self.resource_type = ""
+            self.resource_id = matched_arn.group(6) if matched_arn.group(6) else ""
