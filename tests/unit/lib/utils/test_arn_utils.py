f11678ba3e8720e0039b0bb03a1929d267831555
hnnasit <84355507+hnnasit@users.noreply.github.com>
2023-06-09 16:56:41 -0700

    fix: Update Arn parsing logic and fix some edge cases/bug fixes for remote invoke (#5295)
    * Fix some edge cases and bug fixes for remote invoke and update Arn parsing logic
    
    * Address feedback
    
    * Add unit test for s3 with no region/accoint_id provided
    
    * Renamed command to sam remote invoke

--- tests/unit/lib/utils/test_arn_utils.py
+++ tests/unit/lib/utils/test_arn_utils.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from parameterized import parameterized
 
 from samcli.lib.utils.arn_utils import InvalidArnValue, ARNParts
 
@@ -8,18 +9,126 @@ class TestArnUtils(TestCase):
         with self.assertRaises(InvalidArnValue):
             _ = ARNParts("invalid_arn")
 
-    def test_valid_arn(self):
-        partition = "aws"
-        service = "lambda"
-        region = "us-east-1"
-        account_id = "0123456789"
-        resource_id = "resource_id"
-        arn_value = f"arn:{partition}:{service}:{region}:{account_id}:{resource_id}"
+    @parameterized.expand(
+        [
+            (
+                "arn:aws:service:region:account-id:resource-id",
+                {
+                    "partition": "aws",
+                    "service": "service",
+                    "region": "region",
+                    "account_id": "account-id",
+                    "resource_type": "",
+                    "resource_id": "resource-id",
+                },
+            ),
+            (
+                "arn:aws:service:region:account-id:resource-type/resource-id",
+                {
+                    "partition": "aws",
+                    "service": "service",
+                    "region": "region",
+                    "account_id": "account-id",
+                    "resource_type": "resource-type",
+                    "resource_id": "resource-id",
+                },
+            ),
+            (
+                "arn:aws:service:region:account-id:resource-type:resource-id",
+                {
+                    "partition": "aws",
+                    "service": "service",
+                    "region": "region",
+                    "account_id": "account-id",
+                    "resource_type": "resource-type",
+                    "resource_id": "resource-id",
+                },
+            ),
+            (
+                "arn:partition:service:region:account-id:repository/repository-name",
+                {
+                    "partition": "partition",
+                    "service": "service",
+                    "region": "region",
+                    "account_id": "account-id",
+                    "resource_type": "repository",
+                    "resource_id": "repository-name",
+                },
+            ),
+            (
+                "arn:partition:service:region:account-id:s3-bucket-name",
+                {
+                    "partition": "partition",
+                    "service": "service",
+                    "region": "region",
+                    "account_id": "account-id",
+                    "resource_type": "",
+                    "resource_id": "s3-bucket-name",
+                },
+            ),
+            (
+                "arn:partition:service:::s3-bucket-name",
+                {
+                    "partition": "partition",
+                    "service": "service",
+                    "region": "",
+                    "account_id": "",
+                    "resource_type": "",
+                    "resource_id": "s3-bucket-name",
+                },
+            ),
+            (
+                "arn:aws:lambda:us-west-2:123456789012:function:my-function",
+                {
+                    "partition": "aws",
+                    "service": "lambda",
+                    "region": "us-west-2",
+                    "account_id": "123456789012",
+                    "resource_type": "function",
+                    "resource_id": "my-function",
+                },
+            ),
+            (
+                "arn:aws:states:us-east-1:111122223333:stateMachine:HelloWorld-StateMachine",
+                {
+                    "partition": "aws",
+                    "service": "states",
+                    "region": "us-east-1",
+                    "account_id": "111122223333",
+                    "resource_type": "stateMachine",
+                    "resource_id": "HelloWorld-StateMachine",
+                },
+            ),
+            (
+                "arn:aws:sqs:region:account_id:queue_name",
+                {
+                    "partition": "aws",
+                    "service": "sqs",
+                    "region": "region",
+                    "account_id": "account_id",
+                    "resource_type": "",
+                    "resource_id": "queue_name",
+                },
+            ),
+            (
+                "arn:aws:kinesis:us-east-2:123456789012:stream/mystream",
+                {
+                    "partition": "aws",
+                    "service": "kinesis",
+                    "region": "us-east-2",
+                    "account_id": "123456789012",
+                    "resource_type": "stream",
+                    "resource_id": "mystream",
+                },
+            ),
+        ]
+    )
+    def test_valid_arn(self, given_arn, expected_result):
+        arn_parts = ARNParts(given_arn)
 
-        arn_parts = ARNParts(arn_value)
-
-        self.assertEqual(arn_parts.partition, partition)
-        self.assertEqual(arn_parts.service, service)
-        self.assertEqual(arn_parts.region, region)
-        self.assertEqual(arn_parts.account_id, account_id)
-        self.assertEqual(arn_parts.resource_id, resource_id)
+        self.assertEqual(arn_parts.partition, expected_result["partition"])
+        self.assertEqual(arn_parts.service, expected_result["service"])
+        self.assertEqual(arn_parts.region, expected_result["region"])
+        self.assertEqual(arn_parts.account_id, expected_result["account_id"])
+        self.assertEqual(arn_parts.resource_type, expected_result["resource_type"])
+        self.assertEqual(arn_parts.resource_id, expected_result["resource_id"])
