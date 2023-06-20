efc6b9bdb2394a3ae8889e21a48da4e004cb130c
Leonardo Gama <leogama@amazon.com>
2023-06-20 10:17:34 -0700

    Fix bug comparing ParameterSource enum

--- samcli/cli/cli_config_file.py
+++ samcli/cli/cli_config_file.py
@@ -13,6 +13,7 @@ from pathlib import Path
 from typing import Any, Callable, Dict, List, Optional
 
 import click
+from click.core import ParameterSource
 
 from samcli.cli.context import get_cmd_names
 from samcli.commands.exceptions import ConfigException
@@ -169,7 +170,7 @@ def configuration_callback(
     config_dir = getattr(ctx, "samconfig_dir", None) or os.getcwd()
     config_file = (  # If given by default, check for other `samconfig` extensions first. Else use user-provided value
         SamConfig.get_default_file(config_dir=config_dir)
-        if getattr(ctx.get_parameter_source("config_file"), "name", "") == click.core.ParameterSource.DEFAULT
+        if getattr(ctx.get_parameter_source("config_file"), "name", "") == ParameterSource.DEFAULT.name
         else ctx.params.get("config_file") or SamConfig.get_default_file(config_dir=config_dir)
     )
     # If --config-file is an absolute path, use it, if not, start from config_dir
