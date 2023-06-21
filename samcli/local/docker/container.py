7878ae8218f82eef5260d2ed062929dacb6a45e5
Jacob Fuss <jfuss@users.noreply.github.com>
2023-06-21 12:43:33 -0500

    Fix when we flush to match pervious behavior and output

--- samcli/local/docker/container.py
+++ samcli/local/docker/container.py
@@ -326,6 +326,7 @@ class Container:
             timeout=(self.RAPID_CONNECTION_TIMEOUT, None),
         )
         stdout.write(json.dumps(json.loads(resp.content), ensure_ascii=False), write_to_buffer=False)
+        stdout.flush()
 
     def wait_for_result(self, full_path, event, stdout, stderr, start_timer=None):
         # NOTE(sriram-mv): Let logging happen in its own thread, so that a http request can be sent.
@@ -432,11 +433,9 @@ class Container:
             for stdout_data, stderr_data in output_itr:
                 if stdout_data and stdout:
                     stdout.write(stdout_data)
-                    stdout.flush()
 
                 if stderr_data and stderr:
                     stderr.write(stderr_data)
-                    stderr.flush()
 
         except Exception as ex:
             LOG.debug("Failed to get the logs from the container", exc_info=ex)
