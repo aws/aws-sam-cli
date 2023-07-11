bba84368f1fe5b8caa3a79ed3da535d9da3646be
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2023-07-11 13:58:11 -0700

    use env variables to override some of the values for interactive init

--- samcli/commands/init/init_templates.py
+++ samcli/commands/init/init_templates.py
@@ -34,7 +34,8 @@ from samcli.local.common.runtime_template import (
 )
 
 LOG = logging.getLogger(__name__)
-APP_TEMPLATES_REPO_COMMIT = configuration.get_app_template_repo_commit()
+APP_TEMPLATES_REPO_COMMIT = os.environ.get("SAM_CLI_DEV_INIT_COMMIT", configuration.get_app_template_repo_commit())
+
 MANIFEST_URL = (
     f"https://raw.githubusercontent.com/aws/aws-sam-cli-app-templates/{APP_TEMPLATES_REPO_COMMIT}/manifest-v2.json"
 )
