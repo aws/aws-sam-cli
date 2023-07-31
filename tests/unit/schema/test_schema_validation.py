aad8932f20413177f98f23bae6d11017c7ff67bb
Leonardo Gama <leogama@amazon.com>
2023-07-31 10:06:21 -0700

    Add validation tests for JSON schema

---
+++ tests/unit/schema/test_schema_validation.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+import jsonschema
+from parameterized import parameterized
+from unittest import TestCase
+
+from samcli.lib.config.file_manager import FILE_MANAGER_MAPPER
+from schema.make_schema import generate_schema
+
+
+class TestSchemaValidation(TestCase):
+    schema = None
+    testdata_dir = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.schema = generate_schema()
+        testing_dir = Path(__name__).resolve().parents[0]
+        cls.testdata_dir = Path(testing_dir, "tests", "unit", "schema", "testdata")
+
+    def test_samconfig_validates_against_schema(self):
+        self.assertIsNotNone(self.schema, "Schema was not set")
+
+        passing_tests_dir = Path(self.testdata_dir, "passing_tests")
+        tests_not_passing = []
+
+        # Read in and assert all files in passing_tests pass
+        for config_file_path in passing_tests_dir.iterdir():
+            config_file = FILE_MANAGER_MAPPER[config_file_path.suffix].read(Path(passing_tests_dir, config_file_path))
+            self.assertNotEqual(config_file, {}, f"Config file {config_file_path} should be read correctly")
+
+            try:
+                jsonschema.validate(config_file, self.schema)
+            except jsonschema.ValidationError as e:
+                tests_not_passing.append(f"<{config_file_path}: {e.message}>")
+
+        if tests_not_passing:
+            self.assertTrue(False, f"Some config files are not validating against schema: {tests_not_passing}")
+
+    def test_samconfig_doesnt_validate_against_schema(self):
+        self.assertIsNotNone(self.schema, "Schema was not set")
+
+        failing_tests_dir = Path(self.testdata_dir, "failing_tests")
+        tests_not_failing = []
+
+        # Read in and assert all files in failing_tests fail
+        for config_file_path in failing_tests_dir.iterdir():
+            config_file = FILE_MANAGER_MAPPER[config_file_path.suffix].read(Path(failing_tests_dir, config_file_path))
+            self.assertNotEqual(config_file, {}, f"Config file {config_file_path} should be read correctly")
+
+            with self.assertRaises(
+                jsonschema.ValidationError, msg=f"Config file {config_file_path} should not validate against schema"
+            ):
+                jsonschema.validate(config_file, self.schema)
+                tests_not_failing.append(config_file_path)
+                raise jsonschema.ValidationError("Test should have failed but didn't")
+
+        if tests_not_failing:
+            self.assertTrue(
+                False, f"Some config files are validating against schema when they shouldn't: {tests_not_failing}"
+            )
