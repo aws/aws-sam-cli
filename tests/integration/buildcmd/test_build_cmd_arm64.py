4d86347f17b2a4ea0a129f9a7e35a25d78ef0520
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2023-09-27 14:47:20 -0700

    add arm64 specific ruby function & gemfile

--- tests/integration/buildcmd/test_build_cmd_arm64.py
+++ tests/integration/buildcmd/test_build_cmd_arm64.py
@@ -132,12 +132,12 @@ class TestBuildCommand_RubyFunctions_With_Architecture_arm64(BuildIntegRubyBase)
     #@pytest.mark.flaky(reruns=3)
     @skipIf(SKIP_DOCKER_TESTS or SKIP_DOCKER_BUILD, SKIP_DOCKER_MESSAGE)
     def test_building_ruby_in_container_with_specified_architecture(self, runtime, architecture):
-        self._test_with_default_gemfile(runtime, "use_container", "Ruby", self.test_data_path, architecture)
+        self._test_with_default_gemfile(runtime, "use_container", "Ruby32", self.test_data_path, architecture)
 
     @parameterized.expand([("ruby2.7", "arm64"), ("ruby3.2", "arm64")])
     #@pytest.mark.flaky(reruns=3)
     def test_building_ruby_in_process_with_specified_architecture(self, runtime, architecture):
-        self._test_with_default_gemfile(runtime, False, "Ruby", self.test_data_path, architecture)
+        self._test_with_default_gemfile(runtime, False, "Ruby32", self.test_data_path, architecture)
 
 
 @skipIf(
