cde0555a7c393ba23fc69a6858c0ea00da21750c
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2023-10-05 17:17:50 -0700

    print any errors during container ping

--- tests/integration/local/start_lambda/test_start_lambda.py
+++ tests/integration/local/start_lambda/test_start_lambda.py
@@ -326,9 +326,10 @@ class TestWarmContainersBaseClass(StartLambdaIntegBaseClass):
                 _, output = container.exec_run(["bash", "-c", "'printenv'"])
                 if f"MODE={self.mode_env_variable}" in str(output):
                     running_containers += 1
-            except (docker.errors.NotFound, docker.errors.APIError):
+            except (docker.errors.NotFound, docker.errors.APIError) as ex:
                 # running tests in parallel might might cause this issue since this container in the loop
                 # might be created by other tests and might be at the removal step now
+                LOG.error("Failed to ping container %s for test %s", container.id, self.id(), exc_info=ex)
                 pass
         return running_containers
 
