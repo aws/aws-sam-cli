45c8487a9ee6665491af9da65f414638a1de8783
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2023-10-10 16:58:27 -0700

    Provided.al2023 runtime (#516) (#6061)
    * Provided.al2023 runtime (#516)
    
    * Provided.al2023 implementation
    
    * Provided.al2023 implementation
    
    * Added provided.al2023 to PREVIEW_RUNTIMES
    
    * fix test
    
    * fix set
    
    ---------
    
    Co-authored-by: Anton Stepanov <110172761+anton-stepanof@users.noreply.github.com>

--- tests/integration/validate/test_validate_command.py
+++ tests/integration/validate/test_validate_command.py
@@ -142,6 +142,7 @@ class TestValidate(TestCase):
             "nodejs18.x",
             "provided",
             "provided.al2",
+            "provided.al2023",
             "python3.7",
             "python3.8",
             "python3.9",
