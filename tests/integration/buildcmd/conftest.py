dbc04fe8792e4973780eb7fbd58756e9be0cfae7
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2023-10-12 14:56:37 -0700

    only pull them before running arm64 tests

--- tests/integration/buildcmd/conftest.py
+++ tests/integration/buildcmd/conftest.py
@@ -1,3 +1,7 @@
+"""
+Pre-pulls lambda base images for arm64 runtimes to fix timeout issues during first local invoke
+"""
+
 import logging
 
 from samcli.lib.utils.architecture import ARM64, SUPPORTED_RUNTIMES
@@ -7,6 +11,17 @@ from tests.testing_utils import run_command
 LOG = logging.getLogger(__name__)
 
 def pytest_sessionstart(session):
+    pre_pull_arm64_images = False
+    for pytest_arg in session.config.args:
+        if "arm64.py" in pytest_arg:
+            LOG.info("Running for arm64 build tests...")
+            pre_pull_arm64_images = True
+            break
+
+    if not pre_pull_arm64_images:
+        LOG.info("Not running for arm64 build tests, skipping pre pulling base images")
+        return
+
     LOG.info("Pulling all arm64 lambda base images for fixing timeout issue during sam local invoke")
     for runtime in [e.value for e in Runtime if ARM64 in SUPPORTED_RUNTIMES[e.value]]:
         image_tag = Runtime.get_image_name_tag(runtime, ARM64)
