c39d6a4f414dd11861ad26a0cd13d37e627ffcc7
squizlee <ethanscavia777@gmail.com>
2023-10-18 21:59:12 +1100

    clean up cfn base provider unit test

--- tests/unit/commands/local/lib/test_cfn_base_provider.py
+++ tests/unit/commands/local/lib/test_cfn_base_provider.py
@@ -1,3 +1,4 @@
+from parameterized import parameterized
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
@@ -6,23 +7,21 @@ from samcli.lib.providers.cfn_base_api_provider import CfnBaseApiProvider
 
 
 class TestDisableAuthorizerFlagInExtractSwaggerRoute(TestCase):
-
-    @patch("samcli.lib.providers.cfn_base_api_provider.LOG.debug")
-    @patch("samcli.lib.providers.cfn_base_api_provider.SwaggerParser")
-    @patch("samcli.lib.providers.cfn_base_api_provider.SwaggerReader")
-    def test_disable_authorizer_flag_when_enabled_skips_authorizer_extraction(self, mock_reader: Mock, mock_parser: Mock, mock_debug_log: Mock):
-        mock_collector = Mock()
-
-        CfnBaseApiProvider.extract_swagger_route(stack_path="", logical_id="", body={}, uri="", binary_media=None, collector=mock_collector, cwd="", event_type="HttpApi", disable_authorizer=True)
-        assert "Found '%s' authorizers in resource" not in mock_debug_log.call_args[0]
-
+    @parameterized.expand([
+        ("when enabled will not extract authorizers", True),
+        ("when disabled extracts authorizers", False)
+    ])
     @patch.object(SwaggerParser, "get_authorizers")
     @patch("samcli.lib.providers.cfn_base_api_provider.LOG.debug")
     @patch("samcli.lib.providers.cfn_base_api_provider.SwaggerParser")
     @patch("samcli.lib.providers.cfn_base_api_provider.SwaggerReader")
-    def test_disable_authorizer_flag_when_disabled_extracts_authorizers(self, mock_reader: Mock, mock_parser: Mock, mock_debug_log: Mock, mock_get_authorizers: Mock):
+    def test_disabled_authorizer_flag(self, _, disable_authorizer, mock_reader: Mock, mock_parser: Mock, mock_debug_log: Mock, mock_get_authorizers: Mock):
         mock_collector = Mock()
+        CfnBaseApiProvider.extract_swagger_route(stack_path="", logical_id="", body={}, uri="", binary_media=None, collector=mock_collector, cwd="", event_type="HttpApi", disable_authorizer=disable_authorizer)
         mock_get_authorizers.return_value = [{}, {}, {}]
 
-        CfnBaseApiProvider.extract_swagger_route(stack_path="", logical_id="", body={}, uri="", binary_media=None, collector=mock_collector, cwd="", event_type="HttpApi", disable_authorizer=False)
-        mock_debug_log.assert_any_call("Found '%s' authorizers in resource '%s'", 0, '')
+        if disable_authorizer:
+            mock_collector.add_authorizers.assert_not_called()
+        
+        if not disable_authorizer:
+            mock_collector.add_authorizers.assert_called_once()
