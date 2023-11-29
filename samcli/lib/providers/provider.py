0ae85690d3929f8b28443ff0826de3ad059af1c4
Jared Bentvelsen <jaredbentvelsen@gmail.com>
2023-11-28 17:11:55 -0800

    Add architecture validations for SAM build LambdaLayer (#6322)
    * Add integ test for invalid arch on Function (draft)
    
    * Draft: Add validation to utils and checks in build strategy
    
    * Add integ tests for makefile warning
    
    * Fix integ test for function invalid arch
    
    * Formatting and documentation
    
    * Add a couple None checks and formatting
    
    * Add default value to LayerBuildArchitecture
    
    * publicize and use valid_architecture function
    
    * Include the Layer Id in the warning for invalid arch
    
    * Fix expected message in tests
    
    * Remove check & test for buildarch in compatiblearchs (in another pr)
    
    * Fix log string format, remove unnecessary warn_on_invalid_architecture call
    
    * Add unit tests
    
    * Fix LOG warnings to use %s
    
    ---------
    
    Co-authored-by: Jared Bentvelsen <bentvels@amazon.com>

--- samcli/lib/providers/provider.py
+++ samcli/lib/providers/provider.py
@@ -246,8 +246,8 @@ class LayerVersion:
 
         if "BuildArchitecture" not in metadata:
             LOG.warning(
-                f"WARNING: No BuildArchitecture specifed in Layer `{self._custom_layer_id}`"
-                + " Metadata. Defaulting to x86_64."
+                "WARNING: No BuildArchitecture specifed in Layer `%s`" + " Metadata. Defaulting to x86_64.",
+                self._custom_layer_id,
             )
 
         self._build_architecture = cast(str, metadata.get("BuildArchitecture", X86_64))
@@ -255,8 +255,9 @@ class LayerVersion:
 
         if self._compatible_architectures and self._build_architecture not in self._compatible_architectures:
             LOG.warning(
-                f"WARNING: Layer `{self._custom_layer_id}` has BuildArchitecture `{self._build_architecture}`,"
-                + " which is not listed in CompatibleArchitectures."
+                "WARNING: Layer `%s` has BuildArchitecture `%s`," + " which is not listed in CompatibleArchitectures.",
+                self._custom_layer_id,
+                self._build_architecture,
             )
 
         self._skip_build = bool(metadata.get(SAM_METADATA_SKIP_BUILD_KEY, False))
