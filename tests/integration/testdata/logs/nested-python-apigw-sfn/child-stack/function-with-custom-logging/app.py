39fe8cda5b41846cbdcbc4fa57155377e4adbc0c
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2024-01-02 13:41:31 -0800

    fix: getting logging information from lambda (#6477)
    * fix: getting logging information from lambda
    
    * update formatting
    
    * add unit tests
    
    * add integration tests
    
    * bump python versions that is used in the test templates

---
+++ tests/integration/testdata/logs/nested-python-apigw-sfn/child-stack/function-with-custom-logging/app.py
@@ -0,0 +1,5 @@
+
+def handler(event, context):
+    print("Hello world from ChildStack/FunctionWithCustomLoggingConfig function")
+    print("this should be filtered ChildStackFunctionWithCustomLoggingConfig")
+    return {}
