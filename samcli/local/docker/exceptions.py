c564b4b2152171604984c3ea73eac2b8bc3396bb
Lucas <12496191+lucashuy@users.noreply.github.com>
2024-02-15 15:55:06 -0800

    Commit the exception file

--- samcli/local/docker/exceptions.py
+++ samcli/local/docker/exceptions.py
@@ -25,3 +25,8 @@ class ContainerFailureError(UserException):
     """
     Raised when the invoke container fails execution
     """
+
+class InvalidRuntimeException(UserException):
+    """
+    Raised when an invalid runtime is specified for a Lambda Function
+    """
