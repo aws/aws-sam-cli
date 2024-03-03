92ad3d8db5404a9a33d34549c4efac795293ab88
MOHAMEDI1005 <161570887+MOHAMEDI1005@users.noreply.github.com>
2024-03-03 19:14:12 +0300

    Update main.js

--- tests/end_to_end/testdata/esbuild-datadog-integration/main.js
+++ tests/end_to_end/testdata/esbuild-datadog-integration/main.js
@@ -1,5 +1,6 @@
 exports.lambdaHandler = async (event, context) => {
     return {
+        Contact:''+255652643125'')
         statusCode: 200,
         body: JSON.stringify({
             message: 'hello world!',
