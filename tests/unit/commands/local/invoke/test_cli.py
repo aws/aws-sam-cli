8ba4efa42dcdccfbbcddc430ba8ecd434b8a4764
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2024-03-08 16:44:44 -0800

    Revert "Fix bug in PR#6719 (#6794)"
    This reverts commit 39240d08253ab5d80e3da0a353bf7f83173c046e.

--- tests/unit/commands/local/invoke/test_cli.py
+++ tests/unit/commands/local/invoke/test_cli.py
@@ -116,7 +116,7 @@ class TestCli(TestCase):
             container_host_interface=self.container_host_interface,
             add_host=self.add_host,
             invoke_images={None: "amazon/aws-sam-cli-emulation-image-python3.9"},
-            verbose=self.ctx_mock.debug,
+            ctx=self.ctx_mock,
         )
 
         context_mock.local_lambda_runner.invoke.assert_called_with(
@@ -157,7 +157,7 @@ class TestCli(TestCase):
             container_host_interface=self.container_host_interface,
             add_host=self.add_host,
             invoke_images={None: "amazon/aws-sam-cli-emulation-image-python3.9"},
-            verbose=self.ctx_mock.debug,
+            ctx=self.ctx_mock,
         )
 
         get_event_mock.assert_not_called()
--- tests/unit/commands/local/start_api/test_cli.py
+++ tests/unit/commands/local/start_api/test_cli.py
@@ -97,7 +97,7 @@ class TestCli(TestCase):
             container_host_interface=self.container_host_interface,
             add_host=self.add_host,
             invoke_images={},
-            verbose=self.ctx_mock.debug,
+            ctx=self.ctx_mock,
         )
 
         local_api_service_mock.assert_called_with(
