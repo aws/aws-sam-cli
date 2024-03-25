8ddb95e76e9d51390e4cfb5a7aab71606e4967bf
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2024-03-25 16:01:09 -0700

    feat: add ruby3.3 support

--- samcli/local/docker/lambda_image.py
+++ samcli/local/docker/lambda_image.py
@@ -42,6 +42,7 @@ class Runtime(Enum):
     python311 = "python3.11"
     python312 = "python3.12"
     ruby32 = "ruby3.2"
+    ruby33 = "ruby3.3"
     java8al2 = "java8.al2"
     java11 = "java11"
     java17 = "java17"
