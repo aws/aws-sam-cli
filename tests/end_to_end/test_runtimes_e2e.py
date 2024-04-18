da87b6c5745f8aa1b8e5b8b5af256d814925fc29
Haresh Nasit <84355507+hnnasit@users.noreply.github.com>
2024-04-18 10:23:26 -0700

    replace java17 with dotnet6 for end2end tests

--- tests/end_to_end/test_runtimes_e2e.py
+++ tests/end_to_end/test_runtimes_e2e.py
@@ -69,7 +69,7 @@ class StackOutputsValidator(BaseValidator):
 @parameterized_class(
     ("runtime", "dependency_manager"),
     [
-        ("java17", "maven"),
+        ("dotnet6", "cli-package"),
         ("python3.11", "pip"),
     ],
 )
@@ -103,7 +103,7 @@ class TestHelloWorldDefaultEndToEnd(EndToEndBase):
 @parameterized_class(
     ("runtime", "dependency_manager"),
     [
-        ("java17", "maven"),
+        ("dotnet6", "cli-package"),
         ("python3.11", "pip"),
     ],
 )
@@ -141,7 +141,7 @@ class TestHelloWorldZipPackagePermissionsEndToEnd(EndToEndBase):
 @parameterized_class(
     ("runtime", "dependency_manager"),
     [
-        ("java17", "maven"),
+        ("dotnet6", "cli-package"),
         ("python3.11", "pip"),
     ],
 )
