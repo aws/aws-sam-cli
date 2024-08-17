89a29b806b73f2a583bd97dc9cfea8bee830e5c7
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2024-08-16 19:07:44 -0700

    chore: update log (#7371)
    * chore: update log
    
    * fix log
    
    * black reformat
    
    * update test

--- tests/integration/buildcmd/build_integ_base.py
+++ tests/integration/buildcmd/build_integ_base.py
@@ -921,7 +921,7 @@ class DedupBuildIntegBase(BuildIntegBase):
         # check HelloWorld and HelloMars functions are built in the same build
         self.assertRegex(
             command_result.stderr.decode("utf-8"),
-            "Building codeuri: .* runtime: .* metadata: .* functions: " "HelloWorldFunction, HelloMarsFunction",
+            "Building codeuri: .* runtime: .* functions: " "HelloWorldFunction, HelloMarsFunction",
         )
 
 
@@ -959,7 +959,7 @@ class NestedBuildIntegBase(BuildIntegBase):
         for function_full_path in function_full_paths:
             self.assertRegex(
                 command_result.stderr.decode("utf-8"),
-                f"Building codeuri: .* runtime: .* metadata: .* functions: .*{function_full_path}.*",
+                f"Building codeuri: .* runtime: .* functions: .*{function_full_path}.*",
             )
 
     def _verify_invoke_built_functions(self, template_path, overrides, function_and_expected):
@@ -1004,7 +1004,7 @@ class IntrinsicIntegBase(BuildIntegBase):
         for function_full_path in function_full_paths:
             self.assertRegex(
                 command_result.stderr.decode("utf-8"),
-                f"Building codeuri: .* runtime: .* metadata: .* functions:.*{function_full_path}.*",
+                f"Building codeuri: .* runtime: .* functions:.*{function_full_path}.*",
             )
         self.assertIn(
             f"Building layer '{layer_full_path}'",
