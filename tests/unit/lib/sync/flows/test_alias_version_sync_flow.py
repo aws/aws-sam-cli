26d1e4705663162b30c793071e8b77cd644916e2
Luís Duarte <lduarte@luisduarte.net>
2024-08-27 09:16:10 +0000

    Adds support to delete old versions when successful deployment

--- tests/unit/lib/sync/flows/test_alias_version_sync_flow.py
+++ tests/unit/lib/sync/flows/test_alias_version_sync_flow.py
@@ -1,19 +1,17 @@
-import os
 import hashlib
-
-from samcli.lib.sync.sync_flow import SyncFlow
 from unittest import TestCase
-from unittest.mock import ANY, MagicMock, call, mock_open, patch
+from unittest.mock import MagicMock, patch
 
 from samcli.lib.sync.flows.alias_version_sync_flow import AliasVersionSyncFlow
 from samcli.lib.utils.hash import str_checksum
 
 
 class TestAliasVersionSyncFlow(TestCase):
-    def create_sync_flow(self):
+    def create_sync_flow(self, delete_old_alias = False):
         sync_flow = AliasVersionSyncFlow(
             "Function1",
             "Alias1",
+            delete_old_alias,
             build_context=MagicMock(),
             deploy_context=MagicMock(),
             sync_context=MagicMock(),
@@ -76,3 +74,25 @@ class TestAliasVersionSyncFlow(TestCase):
 
         sync_flow.sync()
         self.assertEqual(sync_flow._local_sha, str_checksum("2", hashlib.sha256()))
+
+    @patch("samcli.lib.sync.sync_flow.Session")
+    def test_delete_old_alias(self, session_mock):
+        sync_flow = self.create_sync_flow(True)
+
+        sync_flow.get_physical_id = MagicMock()
+        sync_flow.get_physical_id.return_value = "PhysicalFunction1"
+
+        sync_flow.set_up()
+
+        sync_flow._lambda_client.get_alias.return_value = {"FunctionVersion": "1"}
+        sync_flow._lambda_client.publish_version.return_value = {"Version": "2"}
+
+        sync_flow.sync()
+
+        sync_flow._lambda_client.publish_version.assert_called_once_with(FunctionName="PhysicalFunction1")
+        sync_flow._lambda_client.update_alias.assert_called_once_with(
+            FunctionName="PhysicalFunction1", Name="Alias1", FunctionVersion="2"
+        )
+        sync_flow._lambda_client.delete_function.assert_called_once_with(
+            FunctionName="{}:{}".format("PhysicalFunction1", "1")
+        )
