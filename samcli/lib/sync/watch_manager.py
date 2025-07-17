c3a971d0ebf4d3be2ead5e216be2ed6dd4b34fa3
adagradschool <adagradschool@gmail.com>
2025-07-17 16:39:35 -0400

    handle missing function from watch trigger

--- samcli/lib/sync/watch_manager.py
+++ samcli/lib/sync/watch_manager.py
@@ -22,7 +22,7 @@ from samcli.lib.utils.code_trigger_factory import CodeTriggerFactory
 from samcli.lib.utils.colors import Colored, Colors
 from samcli.lib.utils.path_observer import HandlerObserver
 from samcli.lib.utils.resource_trigger import OnChangeCallback, TemplateTrigger
-from samcli.local.lambdafn.exceptions import ResourceNotFound
+from samcli.local.lambdafn.exceptions import ResourceNotFound, FunctionNotFound
 
 if TYPE_CHECKING:  # pragma: no cover
     from samcli.commands.build.build_context import BuildContext
@@ -152,7 +152,7 @@ class WatchManager:
                     extra=dict(markup=True),
                 )
                 continue
-            except ResourceNotFound:
+            except (ResourceNotFound, FunctionNotFound):
                 LOG.warning(
                     self._color.color_log(
                         msg="CodeTrigger not created as %s is not found or is with a S3 Location.",
