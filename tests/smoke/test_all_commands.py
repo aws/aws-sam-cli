8429973140aac38cbba4ea93b06188e57971f367
Roger Zhang <ruojiazh@amazon.com>
2025-10-08 18:22:16 -0700

    lint

--- tests/smoke/test_all_commands.py
+++ tests/smoke/test_all_commands.py
@@ -14,10 +14,10 @@ ALL_TEMPLATE_FILE_NAMES = sorted([v for v in os.listdir(TEMPLATE_FOLDER) if "yam
 # Check environment variable to determine which subset to use
 SMOKE_TEST_SUBSET = os.environ.get("SMOKE_TEST_SUBSET", "").lower()
 
-mid_point = len(ALL_TEMPLATE_FILE_NAMES) // 2 
+mid_point = len(ALL_TEMPLATE_FILE_NAMES) // 2
 if mid_point > 5:
     # second half will also run function test
-    mid_point = mid_point - 4
+    mid_point = mid_point - 5
 if SMOKE_TEST_SUBSET == "first-half":
     # Select first half of templates
     TEMPLATE_FILE_NAMES = ALL_TEMPLATE_FILE_NAMES[:mid_point]
