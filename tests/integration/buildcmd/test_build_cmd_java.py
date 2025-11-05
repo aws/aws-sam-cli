e9313ff6f07b535a0fb0ce0b4fdd407f4e1878a5
Harold Sun <sunhua@amazon.com>
2025-11-05 21:38:28 +0000

    fix: update appveyor integration tests

--- tests/integration/buildcmd/test_build_cmd_java.py
+++ tests/integration/buildcmd/test_build_cmd_java.py
@@ -156,34 +156,35 @@ class TestBuildCommand_Java(BuildIntegJavaBase):
                 BuildIntegJavaBase.EXPECTED_FILES_PROJECT_MANIFEST_MAVEN,
                 BuildIntegJavaBase.EXPECTED_MAVEN_DEPENDENCIES,
             ),
-            (
-                "java25",
-                "25",
-                BuildIntegJavaBase.USING_GRADLE_PATH,
-                BuildIntegJavaBase.EXPECTED_FILES_PROJECT_MANIFEST_GRADLE,
-                BuildIntegJavaBase.EXPECTED_GRADLE_DEPENDENCIES,
-            ),
-            (
-                "java25",
-                "25",
-                BuildIntegJavaBase.USING_GRADLEW_PATH,
-                BuildIntegJavaBase.EXPECTED_FILES_PROJECT_MANIFEST_GRADLE,
-                BuildIntegJavaBase.EXPECTED_GRADLE_DEPENDENCIES,
-            ),
-            (
-                "java25",
-                "25",
-                BuildIntegJavaBase.USING_GRADLE_KOTLIN_PATH,
-                BuildIntegJavaBase.EXPECTED_FILES_PROJECT_MANIFEST_GRADLE,
-                BuildIntegJavaBase.EXPECTED_GRADLE_DEPENDENCIES,
-            ),
-            (
-                "java25",
-                "25",
-                BuildIntegJavaBase.USING_MAVEN_PATH,
-                BuildIntegJavaBase.EXPECTED_FILES_PROJECT_MANIFEST_MAVEN,
-                BuildIntegJavaBase.EXPECTED_MAVEN_DEPENDENCIES,
-            ),
+            # skip these tests until build images for Java 25 are released
+            # (
+            #     "java25",
+            #     "25",
+            #     BuildIntegJavaBase.USING_GRADLE_PATH,
+            #     BuildIntegJavaBase.EXPECTED_FILES_PROJECT_MANIFEST_GRADLE,
+            #     BuildIntegJavaBase.EXPECTED_GRADLE_DEPENDENCIES,
+            # ),
+            # (
+            #     "java25",
+            #     "25",
+            #     BuildIntegJavaBase.USING_GRADLEW_PATH,
+            #     BuildIntegJavaBase.EXPECTED_FILES_PROJECT_MANIFEST_GRADLE,
+            #     BuildIntegJavaBase.EXPECTED_GRADLE_DEPENDENCIES,
+            # ),
+            # (
+            #     "java25",
+            #     "25",
+            #     BuildIntegJavaBase.USING_GRADLE_KOTLIN_PATH,
+            #     BuildIntegJavaBase.EXPECTED_FILES_PROJECT_MANIFEST_GRADLE,
+            #     BuildIntegJavaBase.EXPECTED_GRADLE_DEPENDENCIES,
+            # ),
+            # (
+            #     "java25",
+            #     "25",
+            #     BuildIntegJavaBase.USING_MAVEN_PATH,
+            #     BuildIntegJavaBase.EXPECTED_FILES_PROJECT_MANIFEST_MAVEN,
+            #     BuildIntegJavaBase.EXPECTED_MAVEN_DEPENDENCIES,
+            # ),
         ]
     )
     @skipIf(SKIP_DOCKER_TESTS or SKIP_DOCKER_BUILD, SKIP_DOCKER_MESSAGE)
@@ -209,13 +210,14 @@ class TestBuildCommand_Java(BuildIntegJavaBase):
                 BuildIntegJavaBase.EXPECTED_FILES_PROJECT_MANIFEST_GRADLE,
                 BuildIntegJavaBase.EXPECTED_GRADLE_DEPENDENCIES,
             ),
-            (
-                "java8.al2",
-                "8",
-                BuildIntegJavaBase.USING_GRADLEW_PATH,
-                BuildIntegJavaBase.EXPECTED_FILES_PROJECT_MANIFEST_GRADLE,
-                BuildIntegJavaBase.EXPECTED_GRADLE_DEPENDENCIES,
-            ),
+            # Skip in process Java 8 test when using Java 25 with gradlew
+            # (
+            #     "java8.al2",
+            #     "8",
+            #     BuildIntegJavaBase.USING_GRADLEW_PATH,
+            #     BuildIntegJavaBase.EXPECTED_FILES_PROJECT_MANIFEST_GRADLE,
+            #     BuildIntegJavaBase.EXPECTED_GRADLE_DEPENDENCIES,
+            # ),
             (
                 "java8.al2",
                 "8",
