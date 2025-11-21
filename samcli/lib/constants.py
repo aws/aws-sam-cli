7603b3c5b34254da2d28a51646f693805c6d261b
Roger Zhang <ruojiazh@amazon.com>
2025-11-20 16:22:33 -0800

    use SAM_DOCKER_API_VERSION to control docer api version

--- samcli/lib/constants.py
+++ samcli/lib/constants.py
@@ -1,2 +1,5 @@
-DOCKER_MIN_API_VERSION = "1.35"
+import os
+from samcli.cli.global_config import GlobalConfig
+
+DOCKER_MIN_API_VERSION = os.environ.get(GlobalConfig.DOCKER_API_ENV_VAR, "1.35")
 DOCKER_MIN_API_VERSION_FALLBACK = "1.44"
