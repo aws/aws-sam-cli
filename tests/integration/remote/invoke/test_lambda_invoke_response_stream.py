b7c3692f3d1a7c0d8213dc1fcdf8ee3cbb110189
vicheey <181402101+vicheey@users.noreply.github.com>
2025-12-16 15:31:35 -0800

    test(lmi): remove operator role from test (#8507)
    we will remove the pre-created operator role; this is to depend on sam default role creation.

--- tests/integration/remote/invoke/test_lambda_invoke_response_stream.py
+++ tests/integration/remote/invoke/test_lambda_invoke_response_stream.py
@@ -122,7 +122,6 @@ class TestInvokeResponseStreamingCapacityProvider(RemoteInvokeIntegBase):
         cls.parameter_overrides = {
             "SubnetId": os.environ.get("LMI_SUBNET_ID", ""),
             "SecurityGroupId": os.environ.get("LMI_SECURITY_GROUP_ID", ""),
-            "OperatorRoleArn": os.environ.get("LMI_OPERATOR_ROLE_ARN", ""),
         }
         cls.create_resources_and_boto_clients()
 
