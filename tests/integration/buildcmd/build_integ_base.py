3113e40454f0382311cf23516b1d57de93ba473e
Seshu Brahma <seshub@amazon.com>
2026-02-03 16:19:55 -0800

    add mount with write for java21

--- tests/integration/buildcmd/build_integ_base.py
+++ tests/integration/buildcmd/build_integ_base.py
@@ -664,7 +664,7 @@ class BuildIntegJavaBase(BuildIntegBase):
         overrides = self.get_override(runtime, code_path, architecture, "aws.example.Hello::myHandler")
         mount_with = (
             MountMode.WRITE
-            if use_container and str(runtime).lower() == "java25" and self.USING_MAVEN_PATH not in code_path
+            if use_container and str(runtime).lower() in ["java21", "java25"] and self.USING_MAVEN_PATH not in code_path
             else None
         )
         cmdlist = self.get_command_list(
