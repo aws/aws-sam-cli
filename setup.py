65991badc42ce045ea4cfa11ba8f2f1947948e94
Roger Zhang <ruojiazh@amazon.com>
2026-03-06 16:59:32 -0800

    black, samdev

--- setup.py
+++ setup.py
@@ -4,16 +4,11 @@ import os
 from setuptools import setup
 
 # When SAM_CLI_DEV is set, register "samdev" as the console script instead of "sam".
-# The default "sam" entry point is declared in pyproject.toml [project.scripts].
 # All other metadata (name, version, dependencies, etc.) is defined in pyproject.toml.
-cmd_name = "sam"
-if os.getenv("SAM_CLI_DEV"):
-    cmd_name = "samdev"
+cmd_name = "samdev" if os.getenv("SAM_CLI_DEV") else "sam"
 
-setup_kwargs = {}
-if os.getenv("SAM_CLI_DEV"):
-    setup_kwargs["entry_points"] = {
+setup(
+    entry_points={
         "console_scripts": [f"{cmd_name}=samcli.cli.main:cli"],
-    }
-
-setup(**setup_kwargs)
+    },
+)
