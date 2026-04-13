77a3e2ee6ec7846748604929f399b067ae691561
Reed Hamilton <reedham@amazon.com>
2026-04-13 14:18:41 -0700

    test: add unit tests for docker build extra params

--- tests/unit/lib/build_module/test_app_builder.py
+++ tests/unit/lib/build_module/test_app_builder.py
@@ -1774,6 +1774,24 @@ class TestApplicationBuilder_build_lambda_image_function(TestCase):
         with self.assertRaises(DockerBuildFailed):
             self.builder._build_lambda_image("Name", metadata, X86_64)
 
+    def test_can_build_image_function_with_extra_params(self):
+        metadata = {
+            "Dockerfile": "Dockerfile",
+            "DockerContext": "context",
+            "DockerTag": "Tag",
+            "DockerBuildArgs": {"a": "b"},
+            "DockerBuildExtraParams": ["--ssh", "default"],
+        }
+
+        mock_build_client = Mock()
+        mock_build_client.build_image.return_value = iter([])
+        self.builder._image_build_client = mock_build_client
+
+        self.builder._build_lambda_image("Name", metadata, X86_64)
+
+        call_kwargs = mock_build_client.build_image.call_args[1]
+        self.assertEqual(call_kwargs["extra_params"], ["--ssh", "default"])
+
     def test_build_lambda_image_uses_latest_tag_when_not_specified(self):
         metadata = {
             "Dockerfile": "Dockerfile",
