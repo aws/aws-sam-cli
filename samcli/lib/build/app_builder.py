31cf82cdcc7d00e5a49c69225cd1489a2b94f934
Reed Hamilton <reedham@amazon.com>
2026-04-17 10:46:40 -0700

    fix: typecheck DockerExtraBuildParams

--- samcli/lib/build/app_builder.py
+++ samcli/lib/build/app_builder.py
@@ -442,6 +442,9 @@ class ApplicationBuilder:
         if not isinstance(docker_build_args, dict):
             raise DockerBuildFailed("DockerBuildArgs needs to be a dictionary!")
 
+        if docker_build_extra_params is not None and not isinstance(docker_build_extra_params, list):
+            raise DockerBuildFailed("DockerBuildExtraParams needs to be a list!")
+
         docker_context_dir = pathlib.Path(self._base_dir, docker_context).resolve()
 
         if os.environ.get("SAM_BUILD_MODE") and isinstance(docker_build_args, dict):
