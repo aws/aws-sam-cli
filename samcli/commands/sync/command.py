559a64ad1445a412bb78fe7f9e217b363db798c2
Reed Hamilton <reedham@amazon.com>
2026-09-23 17:04:19 -0700

    feat: make sam sync use express mode by default (#9284)
    * feat: make sam sync use express mode by default
    
    * fix: update tests and function param default value

--- samcli/commands/sync/command.py
+++ samcli/commands/sync/command.py
@@ -163,10 +163,9 @@ DEFAULT_CAPABILITIES = ("CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND")
 )
 @click.option(
     "--express/--no-express",
-    default=False,
+    default=True,
     required=False,
     is_flag=True,
-    # TODO: Flip default to True after public announcement of express mode support
     help="Use CloudFormation Express mode to speed up infrastructure deployments by completing once resource "
     "configuration is applied, without waiting for full stabilization.",
 )
@@ -308,7 +307,7 @@ def do_cli(
     build_in_source: Optional[bool],
     watch_exclude: Optional[Dict[str, List[str]]],
     language_extensions: Optional[bool],
-    express: bool = False,
+    express: bool = True,
 ) -> None:
     """
     Implementation of the ``cli`` method
